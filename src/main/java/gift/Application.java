6ef0a037b86d5f712f6b239eec21ef85090b8855
Awhn <ahnshwan@outlook.com>
2024-06-24 15:38:27 +0900

    feat : SpringApplication run

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,11 +1,12 @@
 package gift;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 }
