8d7ccdf842e2a91c58849f835d71a97650359c4f
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-06-24 16:06:21 +0900

    feat : get method 구현

--- src/main/java/gift/GiftController.java
+++ src/main/java/gift/GiftController.java
@@ -1,2 +1,20 @@
-package gift;public class GiftController {
+package gift;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/api")
+public class GiftController {
+    private final Map<Long, ProductDTO> producsts = new HashMap<>();
+
+    @GetMapping("/products")
+    public ProductDTO getProduct(@RequestParam Long id){
+        return producsts.get(id);
+    }
 }
