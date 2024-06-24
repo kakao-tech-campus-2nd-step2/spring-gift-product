921fe83edf0b4f37b45eef84bda7abb716f0e810
JJY <ddglackrp@gmail.com>
2024-06-24 18:02:43 +0900

    feat "Product Service 클래스 추가"
    핵심 비지니스 로직 구현
    - addProduct : 상품추가
    - findProductById : 상품 하나 조회
    - findAllProducts : 상품 전체 조회
    - deleteProduct : 상품 하나 삭제

---
+++ src/main/java/gift/service/ProductService.java
@@ -0,0 +1,48 @@
+package gift.service;
+
+import gift.domain.Product;
+import gift.dto.ProductDto;
+import gift.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Long addProduct(ProductDto productDto){
+        Product product = new Product(productDto.getName(),
+                productDto.getPrice(),
+                productDto.getImageUrl());
+
+        productRepository.save(product);
+
+        return product.getId();
+    }
+
+    public ProductDto findProductById(Long id){
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("해당 상품은 존재하지 않습니다."));
+        return ProductDto.toDto(product);
+    }
+
+    public List<ProductDto> findAllProducts(){
+        return productRepository.findAll().stream()
+                .map(ProductDto::toDto)
+                .collect(Collectors.toList());
+    }
+
+    public Long deleteProduct(Long id){
+        Product findProduct = productRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("해당 상품은 존재하지 않습니다."));
+        return productRepository.delete(findProduct.getId());
+    }
+}
