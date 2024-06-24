6b5d9e3410b80154ec0bbf1e6ee3321fc4b8984f
canyos <4581974@naver.com>
2024-06-24 14:57:25 +0900

    product controller 추가

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,7 +1,6 @@
 package gift;
 
 public class Product {
-    int id;
     String name;
     int price;
     String imageUrl;
---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,7 @@
+package gift;
+
+import java.util.*;
+
+public class ProductController {
+    private final Map<Long, Product> producsts = new HashMap<>();
+}
