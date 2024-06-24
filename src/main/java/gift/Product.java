7dd1e91c838f1c827ba2eaad201263a3ef17cdc4
wvssm <neoramiregi@gmail.com>
2024-06-24 16:15:37 +0900

    feat: Product Entity 생성

---
+++ src/main/java/gift/Product.java
@@ -0,0 +1,15 @@
+package gift;
+
+public class Product {
+    private int id;
+    private String name;
+    private int price;
+    private String imageUrl;
+
+    Product(int id, String name, int price, String imageUrl){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,10 @@
+package gift;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductsController {
+    @GetMapping("/api/products")
+    public
+}
