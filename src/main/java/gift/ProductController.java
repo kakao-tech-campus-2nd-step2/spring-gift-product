7bd4cfef3f0caa63ecca56c17eb4a34a931ada8e
canyos <4581974@naver.com>
2024-06-24 16:17:17 +0900

    상품추가

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -2,14 +2,15 @@ package gift;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.*;
 
 
 @RestController
 public class ProductController {
     private Map<Long, Product> products = new HashMap<>();
+    private static Long count = 1L;
 
     @GetMapping("/api/products")
     public String getProducts(){
@@ -22,7 +23,12 @@ public class ProductController {
         }
         return allProducts;
     }
-
+    @PostMapping("/api/products")
+    public void addProduct(@RequestParam("id")int id,@RequestParam("name")String name,@RequestParam("price")int price, @RequestParam("imageUrl") String imageUrl){
+        Product product = new Product(id,name,price,imageUrl);
+        products.put(count,product);
+        count++;
+    }
     public void setProducts(Map<Long, Product> products) {
         this.products = products;
     }
