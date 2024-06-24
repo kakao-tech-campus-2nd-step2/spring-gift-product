359708345822749b7e4cfafa005518d811007f8f
Geon Kim <kky8586696@naver.com>
2024-06-24 17:42:41 +0900

    feat : 모든 상품 정보를 받아오는 getAllgifts 메서드 추가

--- src/main/java/gift/GiftController.java
+++ src/main/java/gift/GiftController.java
@@ -1,8 +1,26 @@
 package gift;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/api/products")
 public class GiftController {
+
+    private final Map<Long,Gift> gifts = new HashMap<>();
+    private final AtomicLong idGenerator = new AtomicLong();
+
+    @GetMapping
+    public Collection<Gift> getAllgifts(){
+        return gifts.values();
+    }
 
 
 }
