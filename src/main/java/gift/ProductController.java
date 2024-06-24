cf34ee4b727c32e720bdf16c06a09eeaeac3c96b
SeongHun <tjdgns5506@gmail.com>
2024-06-24 21:54:48 +0900

    style: 주석 추가

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -9,11 +9,13 @@ public class ProductController {
     private final Map<Long, Product> products = new HashMap<>();
 
     //Create
+    //상품 추가
     @PostMapping("api/products")
     public void addProduct(@RequestBody Product product) {
         products.put(product.getId(), product);
     }
     //Read
+    //등록된 상품들의 리스트 반환
     @GetMapping("api/products")
     public List<Product> getProducts() {
         List<Product> productList = new ArrayList<>();
@@ -25,11 +27,13 @@ public class ProductController {
         return productList;
     }
     //Update
+    //해당 id를 가진 상품 정보를 수정
     @PutMapping("api/products/{id}")
     public void updateProduct(@PathVariable("id") Long id, @RequestBody Product product) {
         products.put(id, product);
     }
     //Delete
+    //해당 id를 가진 상품을 삭제
     @DeleteMapping("api/products/{id}")
     public void deleteProduct(@PathVariable("id") Long id) {
         products.remove(id);
