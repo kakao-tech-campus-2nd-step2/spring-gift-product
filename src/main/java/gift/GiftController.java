aeecfe8e456611374d748eb7289eddee004a903d
Geon Kim <kky8586696@naver.com>
2024-06-24 22:26:00 +0900

    refactor : 중복되는 코드 makeGiftComponent 메서드로 간소화

--- src/main/java/gift/GiftController.java
+++ src/main/java/gift/GiftController.java
@@ -11,7 +11,6 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 @RequestMapping("/api/products")
 public class GiftController {
-
     private final Map<Long,Gift> gifts = new HashMap<>();
     private final AtomicLong idGenerator = new AtomicLong();
 
@@ -23,17 +22,16 @@ public class GiftController {
     @PostMapping
     public Gift addGift(@RequestBody Gift giftreq){
         long id = idGenerator.incrementAndGet();
-        Gift gift = new Gift(id,giftreq.name(),giftreq.price(),giftreq.imageUrl());
+        Gift gift = makeGiftComponent(id,giftreq);
         gifts.put(id,gift);
         return gift;
     }
-
     @PutMapping("/{id}")
     public Gift updateGift(@PathVariable Long id,@RequestBody Gift giftreq){
         if(!gifts.containsKey(id)){
             throw new IllegalArgumentException("찾는 상품이 없습니다!");
         }
-        Gift gift = new Gift(id,giftreq.name(),giftreq.price(),giftreq.imageUrl());
+        Gift gift = makeGiftComponent(id,giftreq);
         gifts.put(id,gift);
         return gift;
     }
@@ -45,6 +43,9 @@ public class GiftController {
     public String age(@RequestParam String name) {
         System.out.println("결과 " + name);
         return "result";
+    }
+    private Gift makeGiftComponent(Long id,Gift giftreq){
+        return new Gift(id, giftreq.name(), giftreq.price(), giftreq.imageUrl());
     }
 
 }
