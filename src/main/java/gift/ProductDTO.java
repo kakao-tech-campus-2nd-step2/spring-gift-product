a4d8abcc3bd459b4c24a57c0d94d7497b23a4cef
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-06-24 15:51:30 +0900

    feat : ProductDTO 만들기

---
+++ src/main/java/gift/ProductDTO.java
@@ -0,0 +1,14 @@
+package gift;
+
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private double price;
+    private String imageUrl;
+    public ProductDTO(Long id, String name, double price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+}
