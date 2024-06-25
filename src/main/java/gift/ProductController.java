b7edc2de006ea50d989e03c31ce4046294093f46
UiChan Lee <junglkls1234@gmail.com>
2024-06-25 16:54:08 +0900

    Fix: 상품 조회 메서드 수정

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,28 +1,31 @@
 package gift;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+@RequestMapping("/api/products")
 @RestController
 public class ProductController {
     private final Map<Long, Product> products = new HashMap<>();
-    @PutMapping("/products/{id}")
+    @PutMapping("/{id}")
     public Product setProduct(@PathVariable Long id, @RequestBody Product product) {
         products.put(id, product);
-        return product;
+        return  product;
     }
+
     // 특정 상품 조회를 위한 GET 메서드
-    @GetMapping("/products/{id}")
+    @GetMapping("/{id}")
     public Product getProduct(@PathVariable Long id) {
         return products.get(id);
     }
 
     // 전체 상품 리스트 조회를 위한 GET 메서드
-    @GetMapping("/products")
-    public Map<Long, Product> getAllProducts() {
-        return products;
+    @GetMapping
+    public Collection<Product> getAllProducts() {
+        return products.values();
     }
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping("/{id}")
     public void DeleteProduct(@PathVariable Long id){
         products.remove(id);
     }
