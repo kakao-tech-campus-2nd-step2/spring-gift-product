aadb47f568a6906ff34ed952597d5aa6e11ea691
kanguk <lm0828@naver.com>
2024-06-25 14:58:02 +0900

    test : 상품 조회 테스트 작성
    test : 상품 조회 테스트 작성
    
    상품을 조회하는 API를 위한 테스트를 작성한다. setUp을 통해 임시 데이터를 설정하고, 테스트를 통해 조회된 데이터가 일치하는지 확인한다.

--- src/test/java/gift/ProductControllerTest.java
+++ src/test/java/gift/ProductControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +16,13 @@ public class ProductControllerTest {
     @BeforeEach
     public void setUp() {
         productController = new ProductController();
+
+        Map<String, Object> productData = new HashMap<>();
+        productData.put("name", "오둥이 입니다만");
+        productData.put("price", 29800);
+        productData.put("imageUrl", "https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20240405092925_4b920eaeef6a4f0eb2a5c2a434da7ec7.jpg");
+
+        productController.addProduct(productData);
     }
 
     @Test
@@ -27,9 +35,21 @@ public class ProductControllerTest {
         Product addedProduct = productController.addProduct(productData);
 
         assertNotNull(addedProduct);
-        assertEquals(1L, addedProduct.id);
         assertEquals("아이스 카페 아메리카노 T", addedProduct.name);
         assertEquals(4500, addedProduct.price);
         assertEquals("https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg", addedProduct.imageUrl);
     }
+
+    @Test
+    public void 상품_조회() {
+        List<Product> productList = productController.getAllProducts();
+
+        assertNotNull(productList);
+        assertEquals(1, productList.size());
+        Product product = productList.get(0);
+        assertEquals("오둥이 입니다만", product.name);
+        assertEquals(29800, product.price);
+        assertEquals("https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20240405092925_4b920eaeef6a4f0eb2a5c2a434da7ec7.jpg", product.imageUrl);
+    }
+
 }
