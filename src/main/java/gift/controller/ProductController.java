a986b5b52af609e07589ce8b8e80e42814218611
anacnoda77 <sm1108shin@naver.com>
2024-06-25 15:34:12 +0900

    feat:상품 조회 기능 구현

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -3,11 +3,18 @@ package gift.controller;
 import gift.model.Product;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
 
     private final Map<Long, Product> products = new HashMap<>();
+
+    @GetMapping("/api/products")
+    public Product getProduct(@RequestParam("id") Long id) {
+        return products.get(id);
+    }
 
 }
