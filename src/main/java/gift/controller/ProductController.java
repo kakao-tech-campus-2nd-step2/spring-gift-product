c5d88d2f3321afae0f611987b980282738359b7a
sunandrabbit <sunandrabbit@gamil.com>
2024-06-25 19:36:36 +0900

    feat: feature delete product

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +24,7 @@ public class ProductController {
     @PostMapping
     public Product addProduct(@RequestBody Product product) {
         product.setId(idCounter);
-        products.put(idCounter++,product);
+        products.put(idCounter++, product);
         return product;
     }
 
@@ -33,9 +34,21 @@ public class ProductController {
         return new ArrayList<>(products.values());
     }
 
+    //상품 단일 조회 기능
     @GetMapping("/{id}")
     public Product selectProductById(@PathVariable Long id) {
         return products.get(id);
     }
 
+    //상품 삭제 기능
+    @DeleteMapping("/{id}")
+    public Long deleteProduct(@PathVariable Long id) {
+        Product existingProduct = products.get(id);
+        if (existingProduct != null) {
+            products.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("No Exists Product By Id");
+        }
+    }
 }
