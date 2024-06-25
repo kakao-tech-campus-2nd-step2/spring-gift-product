16c88c81963d4580cb8a70d40128bea4ea4c091a
hyyyh0x <hyyyh0x@gmail.com>
2024-06-25 19:29:10 +0900

    feat: adding product

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -5,7 +5,9 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -20,5 +22,19 @@ public class ProductController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product Not Found");
         }
         return products.get(id);
+    }
+
+    //추가
+    @PostMapping()
+    public String postProduct(@RequestParam("id") Long id,
+        @RequestParam("name") String name,
+        @RequestParam("price") int price,
+        @RequestParam("imageUrl") String imageUrl){
+        if(products.containsKey(id)){
+            return "존재하는 id 입니다.";
+        }
+        var product=new Product(id,name,price,imageUrl);
+        products.put(id,product);
+        return "추가되었습니다.";
     }
 }
