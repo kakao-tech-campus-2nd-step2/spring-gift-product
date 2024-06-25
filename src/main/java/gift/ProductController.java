98364e4bdad3ace10d4c59f3e7b9e91fdeba0580
kuma4188 <psb4188@gmail.com>
2024-06-25 11:32:34 +0900

    feat() : create ProductController class

---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,15 @@
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final Map<Long, Product> products = new HashMap<>();
+    private Long nextId = 1L;
+
+
+}
