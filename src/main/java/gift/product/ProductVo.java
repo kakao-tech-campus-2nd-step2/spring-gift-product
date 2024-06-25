bf3eb573144565bad911a7d69ffc189e87b26142
Seunghyun Ko <chris00825@naver.com>
2024-06-25 17:40:01 +0900

    chore: modify ProductVo.setId() param type

--- src/main/java/gift/product/ProductVo.java
+++ src/main/java/gift/product/ProductVo.java
@@ -10,7 +10,7 @@ public class ProductVo {
     public long getId() {
         return id;
     }
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
     public String getName() {
