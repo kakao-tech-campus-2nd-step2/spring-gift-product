022d94ca3c91b551f9d9d1fce362a772f39695df
hyunseo Jeong <wjdgustj2877@naver.com>
2024-06-26 01:48:59 +0900

    feat: 전체 상품 조회 기능 구현

--- src/main/java/gift/domain/product/ProductController.java
+++ src/main/java/gift/domain/product/ProductController.java
@@ -1,10 +1,13 @@
 package gift.domain.product;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +23,14 @@ public class ProductController {
     public ResponseEntity<Product> create(@RequestBody Product productDTO) {
         Product product = new Product(key.getAndIncrement(), productDTO.getName(),
                                         productDTO.getPrice(), productDTO.getImageUrl());
-
         productRepository.put(product.getId(), product);
 
         return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> readAll() {
+        List<Product> productList = new ArrayList<>(productRepository.values());
+        return new ResponseEntity<>(productList, HttpStatus.OK);
     }
 }
