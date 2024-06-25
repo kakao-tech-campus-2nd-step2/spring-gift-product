18d59392d32b3831444f523672a9875ff8bf0714
hyunseo Jeong <wjdgustj2877@naver.com>
2024-06-26 02:13:10 +0900

    feat: 상품 수정 기능 구현

--- src/main/java/gift/domain/product/ProductController.java
+++ src/main/java/gift/domain/product/ProductController.java
@@ -8,8 +8,10 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +44,21 @@ public class ProductController {
         if (product == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<Product> update(@PathVariable long productId, @RequestBody Product productDTO) {
+        Product product = productRepository.get(productId);
+
+        if (product == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        product.setName(productDTO.getName());
+        product.setPrice(productDTO.getPrice());
+        product.setImageUrl(productDTO.getImageUrl());
+        productRepository.put(productId, product);
+
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
 }
