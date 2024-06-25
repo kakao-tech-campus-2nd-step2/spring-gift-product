a4429f7513517cc93487eb8d5e715e70f232d5e1
HSHwan <tnghks2866@gmail.com>
2024-06-25 15:35:37 +0900

    feat: 상품 레포지토리 인터페이스 구현

---
+++ src/main/java/gift/dao/ProductRepository.java
@@ -0,0 +1,15 @@
+package gift.dao;
+
+import gift.dto.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+
+    void save(Long id, Product product);
+    Optional<Product> findById(Long id);
+    List<Product> findAll();
+    void deleteById(Long id);
+    void deleteAll();
+}
