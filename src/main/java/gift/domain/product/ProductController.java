16cabcb67c438bc881be3743e0134ba143c0b655
hyunseo Jeong <wjdgustj2877@naver.com>
2024-06-26 01:37:55 +0900

    feat: 상품 추가 기능 구현

---
+++ src/main/java/gift/domain/product/ProductController.java
@@ -0,0 +1,28 @@
+package gift.domain.product;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final Map<Long, Product> productRepository = new ConcurrentHashMap<>();
+    private final AtomicLong key = new AtomicLong(1);
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product productDTO) {
+        Product product = new Product(key.getAndIncrement(), productDTO.getName(),
+                                        productDTO.getPrice(), productDTO.getImageUrl());
+
+        productRepository.put(product.getId(), product);
+
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+}
