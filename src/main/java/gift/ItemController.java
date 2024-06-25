3ae0e4330369ec3e735fed25a08e7421e9f4a729
dkswoals <a4445656@gmail.com>
2024-06-25 18:09:03 +0900

    feat: 상품 수정 기능 구현
    PUT "/product/{id}" 요청 시 해당 id의 상품을 전달 받은 item으로 대체

--- src/main/java/gift/ItemController.java
+++ src/main/java/gift/ItemController.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,4 +32,9 @@ public class ItemController {
         list.put(item.getId(), item);
         return "Ok";
     }
+    @PutMapping("/{id}")
+    public void updateItem(@PathVariable Long id,@RequestBody Item item){
+        list.replace(id,item);
+    }
+
 }
