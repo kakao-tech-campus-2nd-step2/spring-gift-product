c079fd5767de71db29c257cf3328b4c45aa0c607
dong_yxxn <g0326kdy@gmail.com>
2024-06-25 15:09:00 +0900

    feat(PdController): getProduct 기능 추가 - 저장소 이름 오타 수정

--- src/main/java/gift/PdController.java
+++ src/main/java/gift/PdController.java
@@ -2,9 +2,16 @@ package gift;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PdController {
-    private final Map<Long, Product> producsts = new HashMap<>();
+    private final Map<Long, Product> products = new HashMap<>();
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable Long id) {
+        return products.get(id);
+    }
+
 }
