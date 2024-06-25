57525842a171431796649332db6589fc20f820cf
anacnoda77 <sm1108shin@naver.com>
2024-06-25 15:35:08 +0900

    feat:상품 수정 기능 구현

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,10 @@ public class ProductController {
     public void addProduct(@RequestBody Product product) {
         products.put(1L, product);
     }
-    
+
+    @PutMapping("/api/products")
+    public void updateProduct(@RequestBody Product product) {
+        products.put(product.id(), product);
+    }
 
 }
