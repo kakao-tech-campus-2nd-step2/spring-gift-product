31a8f3c0bd9ce7102ff7b2a46a421aeeab9616ff
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-06-25 14:38:24 +0900

    feat : 상품 전체 조회 기능 추가

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,2 +1,29 @@
-package gift.controller;public class ProductController {
+package gift.controller;
+
+import gift.domain.Product;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final Map<Long, Product> products = new HashMap<>();
+
+    // 상품 조회 기능(전체 조회)
+    @GetMapping("/get")
+    public Collection<Product> getAllProduct() {
+        Collection<Product> val = products.values();
+        return val;
+    }
+
+
+
 }
