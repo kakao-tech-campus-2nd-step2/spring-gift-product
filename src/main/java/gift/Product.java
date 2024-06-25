6037b09b217131120c64b65cefb2e0484922995f
Giheon Do <dgh0001@naver.com>
2024-06-25 15:28:25 +0900

    feat: Add Product class

---
+++ src/main/java/gift/Product.java
@@ -0,0 +1,42 @@
+package gift;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private int price;
+    private String imageUrl;
+
+    public Product(String name, int price, String imageUrl) {
+        this.id = 0L;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setProduct(Product product) {
+        this.name = product.getName();
+        this.price = product.getPrice();
+        this.imageUrl = product.imageUrl;
+    }
+}
