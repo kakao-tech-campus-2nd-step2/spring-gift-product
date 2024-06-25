11a283fe78a501deb4ef8244cb28371907804892
noojmogstel <goaudgo@naver.com>
2024-06-25 20:08:17 +0900

    refactor(products): 삭제/수정시 존재하지 않는 id값에 대해 refactor

--- src/main/java/gift/domain/Products.java
+++ src/main/java/gift/domain/Products.java
@@ -15,13 +15,20 @@ public class Products {
         products.put(product.getId(), product);
     }
 
-    public void delete(Long id) {
-        products.remove(id);
+    public boolean delete(Long id) {
+        //간단한 유효성 검증. 삭제하려는 id값이 존재하지 않으면 false, 삭제를 올바르게 마쳤다면 true를 리턴한다.
+        return products.remove(id) != null;
     }
 
-    public void edit(Long id, Product product) {
-        products.remove(id);
-        products.put(product.getId(), product);
+    public boolean edit(Long id, Product product) {
+
+        //간단한 유효성 검증. id값이 존재하지 않으면 false를 리턴한다
+        if (products.remove(id) != null) {
+            products.put(product.getId(), product);
+            return true;
+        }
+
+        return false;
     }
 
     public Products() {
