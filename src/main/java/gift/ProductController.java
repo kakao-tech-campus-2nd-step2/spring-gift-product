0a5204ab55187ccc7eaafaa01a0068e9fd524603
kuma4188 <psb4188@gmail.com>
2024-06-25 12:12:43 +0900

    fix : remove unimportant code

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -2,36 +2,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
+import gift.*;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/hello")
 public class ProductController {
-
     private final Map<Long, Product> products = new HashMap<>();
-    private Long nextId = 1L;
 
     // get all product
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
         return new ResponseEntity<>(new ArrayList<>(products.values()), HttpStatus.OK);
     }
-
-    // get product by ID
-    @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
-        Product product = products.get(id);
-        return new ResponseEntity<>(product, HttpStatus.OK);
-    }
-
-    // add product
-    @PostMapping
-    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
-        product.setId(nextId++);
-        products.put(product.getId(), product);
-        return new ResponseEntity<>(product, HttpStatus.CREATED);
-    }
-
-
 
 
 }
