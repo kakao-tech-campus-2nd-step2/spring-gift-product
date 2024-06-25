96872b5559ee9fbf2cbb37edbb09d21e0a575d81
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-06-25 20:07:37 +0900

    feat : 조회 기능 추가

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final Map<Long, Product> products = new HashMap<>();
-    private final AtomicLong counter = new AtomicLong();
 
     public ProductController(){
-        products.put(counter.incrementAndGet(), new Product(counter.get(), "아이스 카페 아메리카노 T", 4500, "url1"));
+        products.put(814607L, new Product(814607, "아이스 카페 아메리카노 T", 4500, "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
     }
 
     @GetMapping("/products")
     public List<Product> getProducts() {
         return new ArrayList<>(products.values());
     }
+
 }
