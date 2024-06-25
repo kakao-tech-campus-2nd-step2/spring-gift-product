529dcee5d489dc55696b9344606ccf24e2210703
Kim Sanghae <pelikan@pusan.ac.kr>
2024-06-25 16:34:50 +0900

    feat: update product controller

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,17 +1,19 @@
-package controller;
+package gift.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import gift.Product;
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,22 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final Map<Long, Product> products = new HashMap<>();
-    private final AtomicLong listId = new AtomicLong(1);
+    private final AtomicLong orderId = new AtomicLong(1);
 
     @PostMapping("/api/products")
-    public void CreateProducts(@RequestParam("id") Long id, @RequestParam("name") String name,
-        @RequestParam("price") int price, @RequestParam("imageUrl") String imgUrl) {
-        var product = new Product(id, name, price, imgUrl);
-        products.put(listId.getAndIncrement(), product);
+    public ResponseEntity<Product> AddProduct(@RequestBody Product product) {
+        var prdct = new Product(product.id(), product.name(), product.price(), product.imageUrl());
+        products.put(orderId.getAndIncrement(), prdct);
+        return ResponseEntity.status(HttpStatus.OK).body(prdct);
     }
 
     @GetMapping("/api/products")
-    public String ReadProducts() throws JsonProcessingException {
+    public ResponseEntity<Collection<Product>> GetAllProducts() throws JsonProcessingException {
         if (products.isEmpty()) {
-            return "Empty";
+            return ResponseEntity.noContent().build();
         }
-        var objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(new ArrayList<Product>(products.values()));
+        return ResponseEntity.ok().body(products.values());
     }
 
     @PutMapping("/api/products")
@@ -46,20 +47,24 @@ public class ProductController {
             return;
         }
         var sampleProduct = products.get(productList.getFirst());
-        productList.stream().forEach(l -> {
-            products.remove(l);
-            products.put(l, new Product(sampleProduct.id(), name, sampleProduct.price(),
-                sampleProduct.imageUrl()));
-        });
+        productList.forEach(l -> updateElement(name, l, sampleProduct));
+    }
+
+    private void updateElement(String name, Long l, Product sampleProduct) {
+        products.remove(l);
+        products.put(l, new Product(sampleProduct.id(), name, sampleProduct.price(),
+            sampleProduct.imageUrl()));
     }
 
     @DeleteMapping("/api/products")
-    public void DeletePRoduct(@RequestParam(name = "id") Long productId) {
+    public void DeleteProduct(@RequestParam(name = "id") Long productId) {
         var keyList = products.entrySet().stream().filter(x -> x.getValue().id().equals(productId))
             .map(Entry::getKey).toList();
         if (keyList.isEmpty()) {
             return;
         }
-        keyList.forEach(products::remove); // 왜 stream이 필요가 없어지는지? forEach가 stream을 포함하고 있는 관계?
+        keyList.forEach(products::remove);
     }
 }
+
+
