1a50c554eaa2e7eed6f732f54811a9fbfb52d80a
Kim Sanghae <pelikan@pusan.ac.kr>
2024-06-25 14:40:21 +0900

    feat: init Update product method

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +32,14 @@ public class ProductController {
         }
         var objectMapper = new ObjectMapper();
         return objectMapper.writeValueAsString(new ArrayList<Product>(products.values()));
+    }
+
+    @PutMapping("/api/products")
+    public void UpdateProductsName(@RequestParam(name = "id") Long id, @RequestParam(name = "name") String name) {
+        var product = products.get(id);
+        var newProduct = new Product(product.id(), name, product.price(), product.imageUrl());
+        products.remove(id);
+        products.put(listId.getAndIncrement(), newProduct);
     }
 
 }
