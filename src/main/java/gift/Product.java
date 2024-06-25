7fe78e47db81643d69d8f6ac694bb02ee57b3e00
Dalsungmin <cnww123@naver.com>
2024-06-25 18:34:49 +0900

    fix: 문제 조건대로 자료형 수정

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,17 +1,17 @@
-package src.main.java.gift;
+package gift;
 
 public class Product {
-    private int id;
+    private long id;
     private String name;
     private int price;
     private String imageUrl;
 
     // Getters, Setters
-    public int getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
