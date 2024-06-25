9e9ba4233b0eab1dfb0765c33a508842ce0e4f07
wvssm <neoramiregi@gmail.com>
2024-06-25 11:37:36 +0900

    feat(ProductController): add Product 등록 Controller

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,29 +1,41 @@
 package gift.controller;
 
-import gift.entity.Product;
+import gift.dto.ProductRequestDto;
+import gift.dto.ProductResponseDto;
+import gift.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class ProductController {
-    private final Map<Long, Product> products = new HashMap<>();
+    private final ProductService productService;
 
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/api/products")
+    public ResponseEntity<ProductResponseDto> addProduct(@RequestBody ProductRequestDto request) {
+        ProductResponseDto createdProduct = productService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+    }
+
+    /*
     @GetMapping("/api/products")
     public Product getProducts() {
     }
 
-    @PostMapping("/api/product")
-    public void addProduct() {
-    }
-
     @DeleteMapping("api/product/{id}")
     public void deleteProduct(@PathVariable int id) {
+
     }
 
     @PutMapping("api/product/{id}")
     public void editProduct(@PathVariable int id){
 
     }
+     */
 }
