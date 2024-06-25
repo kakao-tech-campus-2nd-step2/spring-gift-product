08d8075ee2a7a5d5518db796628b76393786e59e
kuma4188 <psb4188@gmail.com>
2024-06-25 15:20:19 +0900

    feat : 삼품조회 만들기

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -18,4 +18,16 @@ public class ProductController {
         products.add(new Product(8146027L, "아이스 카페 아메리카노 T", 4500, "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
+
+    // 상품 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = products.get(id);
+        if (product == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+
 }
