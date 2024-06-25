934c8b3b90cb47522389b76565b46e6e28ebc069
noojmogstel <goaudgo@naver.com>
2024-06-25 20:10:22 +0900

    feat(controller): ADD HTTP GET Method

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,28 @@
+package gift.controller;
+
+
+import gift.domain.Product;
+import gift.domain.Products;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    Products products = new Products();
+
+    //전체 product 목록 조회
+    @GetMapping
+    public Products getProduct(){
+        return products;
+    }
+
+
+}
