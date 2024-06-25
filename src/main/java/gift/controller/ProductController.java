d5a5fed4cd542572d47a396914e3fd196b836236
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-06-25 17:36:16 +0900

    feat: 전체 상품 조회 기능 구현

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -2,8 +2,11 @@ package gift.controller;
 
 import gift.Product;
 import gift.ProductDTO;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +18,11 @@ public class ProductController {
 
     @PostMapping("/api/product")
     public void registerProduct(@RequestBody ProductDTO productDTO) {
-        System.out.println(productDTO);
         products.put(productDTO.id(), productDTO.toEntity());
+    }
+
+    @GetMapping("/api/products")
+    public List<Product> getAllProducts() {
+        return new ArrayList<>(products.values());
     }
 }
