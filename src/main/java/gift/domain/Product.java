08fc6042faa3168819ee84411771e0214f1b4309
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-06-25 14:17:23 +0900

    feat : Product domain 작성

---
+++ src/main/java/gift/domain/Product.java
@@ -0,0 +1,47 @@
+package gift.domain;
+
+public class Product {
+    int id;
+    String name;
+    int price;
+    String imageUrl;
+
+    public Product(int id, String name, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
