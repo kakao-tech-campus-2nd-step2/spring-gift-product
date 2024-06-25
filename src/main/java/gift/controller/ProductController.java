5cffc7985946cfc50f51756f9cae310bf1e76ca3
hjinshin <gudwls818@gmail.com>
2024-06-26 00:39:09 +0900

    feat: 전체 상품 조회 API 구현
    GET방식으로 전체 상품 조회

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,10 +1,12 @@
 package gift.controller;
 
 import gift.model.Product;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -12,5 +14,10 @@ import java.util.Map;
 public class ProductController {
     private final Map<Long, Product> products = new HashMap<>();
 
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProducts() {
+        List<Product> productsRes = new ArrayList<>(products.values());
+        return ResponseEntity.ok().body(productsRes);
+    }
 
 }
