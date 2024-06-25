600e019f9f7c0e6bfc06ed3b080461b7c07534f3
sseooh <sseooh@gmail.com>
2024-06-25 20:15:11 +0900

    test: 상품 추가 기능 테스트

---
+++ src/test/java/gift/ProductControllerTest.java
@@ -0,0 +1,52 @@
+package gift;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ProductController.class)
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Product sampleProduct;
+
+    @BeforeEach
+    void setUp() {
+        sampleProduct = new Product(
+                null,
+                "아이스 카페 아메리카노 T",
+                4500L,
+                "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"
+        );
+    }
+
+    @Test
+    @DisplayName("상품 추가 테스트")
+    public void addProductTest() throws Exception {
+
+        String productJson = objectMapper.writeValueAsString(sampleProduct);
+
+        mockMvc.perform(post("/api/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(productJson))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.name").value(sampleProduct.getName()))
+                .andExpect(jsonPath("$.price").value(sampleProduct.getPrice()))
+                .andExpect(jsonPath("$.imageUrl").value(sampleProduct.getImageUrl()));
+    }
+}
