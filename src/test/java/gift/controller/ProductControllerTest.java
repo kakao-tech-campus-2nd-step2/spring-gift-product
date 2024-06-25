d2176dc3c5106e8b03f1c4a8d0979456ba46bd38
sunandrabbit <sunandrabbit@gamil.com>
2024-06-25 19:08:27 +0900

    refactor : using Product Constructor

--- src/test/java/gift/controller/ProductControllerTest.java
+++ src/test/java/gift/controller/ProductControllerTest.java
@@ -14,7 +14,6 @@ public class ProductControllerTest {
 
     @BeforeEach
     public void setUp() {
-        tempProduct = new Product();
         productController = new ProductController();
     }
 
@@ -22,16 +21,17 @@ public class ProductControllerTest {
     @DisplayName("상품 추가 기능 확인")
     void checkAddProductMethod() {
         //given
-        tempProduct.setName("아이스 카페 아메리카노 T");
-        tempProduct.setPrice(4500L);
-        tempProduct.setImageUrl("https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg");
+        tempProduct = new Product("아이스 카페 아메리카노 T", 4500L,
+            "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg");
         //when
         Product addedProduct = productController.addProduct(tempProduct);
-
-
+        //then
         assertEquals(1L, addedProduct.getId());
         assertEquals("아이스 카페 아메리카노 T", addedProduct.getName());
         assertEquals(4500, addedProduct.getPrice());
-        assertEquals("https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg", addedProduct.getImageUrl());
+        assertEquals(
+            "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg",
+            addedProduct.getImageUrl());
     }
+
 }
