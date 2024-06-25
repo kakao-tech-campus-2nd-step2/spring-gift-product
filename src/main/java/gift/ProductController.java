0712402cf1bff3d59e9888f3dcd5b03cf1c5bbd5
kuma4188 <psb4188@gmail.com>
2024-06-25 15:34:40 +0900

    feat : 상품수정

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -17,6 +17,9 @@ public class ProductController {
     public ResponseEntity<List<Product>> getAllProducts() {
         List<Product> products = new ArrayList<>();
         products.add(new Product(8146027L, "아이스 카페 아메리카노 T", 4500, "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
+        products.add(new Product(4565455L, "아이스 카페 라테 T", 5500, "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
+        products.add(new Product(1234567L, "뜨거운 아이스 아메리카노 T", 6500, "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
+
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
@@ -47,6 +50,18 @@ public class ProductController {
         products.remove(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+    //상품수정
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        if (!products.containsKey(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        product.setId(id);
+        products.put(id, product);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
 
 
 
