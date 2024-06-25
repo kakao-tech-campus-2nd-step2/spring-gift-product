263bbaec83a09d57965baa1f0b142045d69b3f15
pjhcsols <pjhcsols@naver.com>
2024-06-25 17:17:29 +0900

    feat: 상품 데이터 모델 작성[박한솔]

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -3,5 +3,30 @@ package gift.domain;
 
 //import lombok 사용불가
 public class Product {
+    private Long id;
+    private String name;
+    private Long price;
+    private String description;
+    //@Coullum()
+    private String imageUrl;
 
+    public Product(Long id, String name, Long price, String description, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Long getPrice() { return price; }
+    public void setPrice(Long price) { this.price = price; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public String getImageUrl() { return imageUrl; }
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
 }
