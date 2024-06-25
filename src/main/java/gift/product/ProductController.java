1323885c9f1823670253d1f8a70c96419d2ad210
Seunghyun Ko <chris00825@naver.com>
2024-06-25 17:00:14 +0900

    feat: create product control map instead of database

---
+++ src/main/java/gift/product/ProductController.java
@@ -0,0 +1,10 @@
+package gift.product;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductController {
+
+    private final Map<Long, ProductVo> products = new HashMap<>();
+
+}
