44647d6fa45cb9390a3b724e51cbe0a6c4a52afc
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-06-25 22:09:25 +0900

    feat : 상품 추가 기능 추가

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -5,7 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +27,13 @@ public class ProductController {
     public List<Product> getProducts() {
         return new ArrayList<>(products.values());
     }
+
+    @PostMapping("/products")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        long id = product.id();
+        products.put(id, product);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
 
 }
