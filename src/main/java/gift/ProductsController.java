9b1fb0c445623b13b664efa07b94f842df84bf8e
speciling <speciling@naver.com>
2024-06-25 17:06:13 +0900

    feat: 상품 추가 API 구현

--- src/main/java/gift/ProductsController.java
+++ src/main/java/gift/ProductsController.java
@@ -2,6 +2,9 @@ package gift;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductsController {
 
     private final Map<Long, Product> products = new ConcurrentHashMap<>();
+    private final AtomicLong sequence = new AtomicLong();
+
+    @PostMapping
+    public Product addNewProduct(@RequestBody Product product) {
+        Long id = sequence.getAndIncrement();
+        product.setId(id);
+        products.put(id, product);
+        return product;
+    }
+
 }
