7d53c0d0f72d08e6ab3594435106429362f129b7
dkswoals <a4445656@gmail.com>
2024-06-25 17:52:10 +0900

    feat: 단일 상품 조회 기능 구현
    "/product/{id}" 요청시 해당 id의 상품을 반환하는 findItem 메서드 작성

--- src/main/java/gift/ItemController.java
+++ src/main/java/gift/ItemController.java
@@ -2,14 +2,19 @@ package gift;
 
 import Model.Item;
 import java.util.HashMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/product")
 public class ItemController {
-    HashMap<Long, Item> list = new HashMap<Long, Item>();
+    HashMap<Long, Item> list = new HashMap<>();
 
-
+    @GetMapping("/{id}")
+    public Item findItem(@PathVariable Long id){
+        return list.get(id);
+    }
 
 }
