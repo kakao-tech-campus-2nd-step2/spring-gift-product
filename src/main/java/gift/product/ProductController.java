63b3e0ab3b3fdae0edeb199a800ead7911ac095b
Seunghyun Ko <chris00825@naver.com>
2024-06-25 19:02:31 +0900

    fix(ProductController.java): modify addProduct function
    before:
    addProduct 메서드의 파라미터가 ProductVo타입이었음
    
    after:
    addProduct 메서드의 파라미터를 HTTP 메세지의 요청으로 수정
    
    이에 따른 addProduct메서드의 바디문 수정도 하게 됨

--- src/main/java/gift/product/ProductController.java
+++ src/main/java/gift/product/ProductController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +14,13 @@ public class ProductController {
     private final AtomicLong idCounter = new AtomicLong();
 
     @PostMapping("/product")
-    public void addProduct(ProductVo product) {
+    public void addProduct(@RequestParam("name") String name, @RequestParam("price") int price, @RequestParam("imageUrl") String imageUrl) {
+        ProductVo product = new ProductVo();
+
+        product.setName(name);
+        product.setPrice(price);
+        product.setImageUrl(imageUrl);
+
         products.put(idCounter.incrementAndGet(), product);
     }
 
