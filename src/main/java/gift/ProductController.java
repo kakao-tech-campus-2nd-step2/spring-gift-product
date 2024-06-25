86b5f04f5cc076a5b5548afd3c77992904b131da
Dalsungmin <cnww123@naver.com>
2024-06-25 18:38:00 +0900

    feat: 기본 상품 추가

---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,28 @@
+package gift;
+
+import jakarta.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final Map<Long, Product> products = new HashMap<>();
+    private long nextId = 1L;
+
+    @PostConstruct
+    public void init() {
+        Product product1 = new Product();
+        product1.setId(8146027);
+        product1.setName("아이스 카페 아메리카노 T");
+        product1.setPrice(4500);
+        product1.setImageUrl("https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg");
+        products.put(product1.getId(), product1);
+
+    }
+}
