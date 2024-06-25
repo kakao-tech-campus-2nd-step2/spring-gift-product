a5c33df859b4a4829acc2fd6365a15dc4858628a
hyyyh0x <hyyyh0x@gmail.com>
2024-06-25 19:29:48 +0900

    feat: modifying product

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -2,10 +2,11 @@ package gift;
 
 import java.util.HashMap;
 import java.util.Map;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +37,31 @@ public class ProductController {
         var product=new Product(id,name,price,imageUrl);
         products.put(id,product);
         return "추가되었습니다.";
+    }
+    //수정
+    @PutMapping("/{id}")
+    public Product putProduct(@PathVariable("id") Long id,@RequestBody Map<String, Object> putData) {
+        if(!products.containsKey(id)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product Not Found");
+        }
+        String name=products.get(id).getName();
+        int price=products.get(id).getPrice();
+        String imageUrl=products.get(id).getImageUrl();
+        if(putData.containsKey("name")) {
+            name = putData.get("name").toString();
+        }
+        if (putData.containsKey("price")) {
+            try {
+                price = Integer.parseInt(putData.get("price").toString());
+            } catch (NumberFormatException e) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid price format");
+            }
+        }
+        if(putData.containsKey("imageUrl")){
+            imageUrl=putData.get("imageUrl").toString();
+        }
+        var product=new Product(id,name,price,imageUrl);
+        products.put(id,product);
+        return product;
     }
 }
