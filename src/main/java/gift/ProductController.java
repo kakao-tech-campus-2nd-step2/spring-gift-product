f7b991de3713fc7801de28d969fdfd916fa2f311
us4c0d3 <jwseok312@gmail.com>
2024-06-25 22:49:47 +0900

    feat(ProductController): Add endpoint to update product

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -3,9 +3,11 @@ package gift;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,5 +40,15 @@ public class ProductController {
     public ResponseEntity<Product> addProduct(@RequestBody Product product) {
         productService.addProduct(product);
         return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/api/products/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        try {
+            productService.updateProduct(id, product);
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(product, HttpStatus.BAD_REQUEST);
+        }
     }
 }
