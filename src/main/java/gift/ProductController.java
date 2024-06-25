2b0b99dc70a6204a5c2da32f186dcda651a6a49d
kuma4188 <psb4188@gmail.com>
2024-06-25 11:37:18 +0900

    feat() : get product

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,7 +1,6 @@
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.*;
 
 @RestController
@@ -10,6 +9,19 @@ public class ProductController {
 
     private final Map<Long, Product> products = new HashMap<>();
     private Long nextId = 1L;
+
+    // get all product
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(new ArrayList<>(products.values()), HttpStatus.OK);
+    }
+
+    // get product by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = products.get(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+
 
 
 }
