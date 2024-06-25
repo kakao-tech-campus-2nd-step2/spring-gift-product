9559cc454f0a025985e8c2957b15a7bf87cefaa6
dkswoals <a4445656@gmail.com>
2024-06-25 17:46:34 +0900

    feat: Item 객체 구현

---
+++ src/main/java/Model/Item.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class Item {
+    Long id;
+    String name;
+    Long price;
+    String imgUrl;
+
+    public Item(Long id, String name, Long price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
