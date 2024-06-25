18b27269dd2164f448a3892ed52d6cc19019ee59
hjinshin <gudwls818@gmail.com>
2024-06-26 01:36:06 +0900

    feat: 상품 생성 API 구현
    POST방식으로 저장소에 해당 상품 생성

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,6 +1,8 @@
 package gift.controller;
 
 import gift.model.Product;
+import gift.model.ProductRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,11 +10,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
     private final Map<Long, Product> products = new HashMap<>();
+    private final AtomicLong idGenerator = new AtomicLong();
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts() {
@@ -21,8 +25,16 @@ public class ProductController {
     }
 
     @GetMapping("/product")
-    public ResponseEntity<Product> getProduct(@RequestParam Long id) {
+    public ResponseEntity<Product> getProduct(@RequestParam("id") long id) {
         Product product = products.get(id);
         return ResponseEntity.ok().body(product);
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<Long> createProduct(@RequestBody ProductRequest request) {
+        long id = idGenerator.incrementAndGet();
+        Product product = new Product(id, request.name(), request.price(), request.imageUrl());
+        products.put(id, product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(id);
     }
 }
---
+++ src/main/java/gift/model/ProductRequest.java
@@ -0,0 +1,4 @@
+package gift.model;
+
+public record ProductRequest(String name, int price, String imageUrl) {
+}
