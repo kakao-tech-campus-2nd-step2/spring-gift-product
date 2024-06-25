27ef9c8154087855eb88a6cc3918a8efbf2ca7b1
sunandrabbit <sunandrabbit@gamil.com>
2024-06-25 19:09:41 +0900

    feat : feature select all product

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,8 +1,11 @@
 package gift.controller;
 
 import gift.model.Product;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +18,18 @@ public class ProductController {
     private final Map<Long, Product> products = new HashMap<>();
     private Long idCounter = 1L;
 
+    //상품 추가 기능
     @PostMapping
     public Product addProduct(@RequestBody Product product) {
         product.setId(idCounter);
         products.put(idCounter++,product);
         return product;
+    }
+
+    //상품 전체 조회 기능
+    @GetMapping
+    public List<Product> selectAllProduct() {
+        return new ArrayList<>(products.values());
     }
 
 }
