c2cca65601bb48e945b69892ef0b01c60148e1dd
HSHwan <tnghks2866@gmail.com>
2024-06-25 15:55:57 +0900

    feat: 컬렉션 기반의 상품 레포지토리 구현

---
+++ src/main/java/gift/dao/MemoryProductRepository.java
@@ -0,0 +1,41 @@
+package gift.dao;
+
+import gift.domain.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class MemoryProductRepository implements ProductRepository {
+
+    private final Map<Long, Product> products = new HashMap<>();
+
+    @Override
+    public void save(Long id, Product product) {
+        products.put(id, product);
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return Optional.ofNullable(products.get(id));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<>(products.values());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        products.remove(id);
+    }
+
+    @Override
+    public void deleteAll() {
+        products.clear();
+    }
+}
