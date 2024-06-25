e1910a048427c5c8d9506fd49feff1c42f1b31ed
Seunghyun Ko <chris00825@naver.com>
2024-06-25 16:09:44 +0900

    feat: add product getter and setter

--- src/main/java/gift/product/ProductVo.java
+++ src/main/java/gift/product/ProductVo.java
@@ -7,4 +7,28 @@ public class ProductVo {
     private int price;
     private String imageUrl;
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
