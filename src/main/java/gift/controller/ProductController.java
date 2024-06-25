77eafca29f781024601cbe56d74266afd4adf612
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-06-25 18:03:11 +0900

    feat: 상품 수정 기능 구현

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +31,16 @@ public class ProductController {
     @GetMapping("/api/product/{id}")
     public Product getProduct(@PathVariable("id") Long id) {
         return products.get(id);
+    }
+
+    @PutMapping("/api/product/{id}")
+    public void updateProduct(@PathVariable("id") Long id, @RequestBody ProductDTO productDTO) {
+        Product product = products.get(id);
+        product.setId(id);
+        product.setName(productDTO.name());
+        product.setPrice(productDTO.price());
+        product.setImageUrl(productDTO.imageUrl());
+
+        products.put(id, product);
     }
 }
