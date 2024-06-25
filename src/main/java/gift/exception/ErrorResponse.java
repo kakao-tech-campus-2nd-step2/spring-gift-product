fd30b20d3ebcbfcd0e715fc56df9f5209ba96eca
kangwlgns <kangji0615@gmail.com>
2024-06-26 04:19:01 +0900

    docs: add comments

--- src/main/java/gift/exception/ErrorResponse.java
+++ src/main/java/gift/exception/ErrorResponse.java
@@ -1,7 +1,10 @@
 package gift.exception;
 
+// JSON 형태로 에러 내용을 반환하기 위해 만든 클래스
 public class ErrorResponse {
+    // 에러 종류
     private String message;
+    // 자세한 에러 메시지
     private String details;
 
     public ErrorResponse(String message, String details) {
--- src/main/java/gift/exception/ValidationException.java
+++ src/main/java/gift/exception/ValidationException.java
@@ -1,5 +1,6 @@
 package gift.exception;
 
+// 추가한 예외
 public class ValidationException extends IllegalArgumentException {
     public ValidationException(String message) {
         super(message);
