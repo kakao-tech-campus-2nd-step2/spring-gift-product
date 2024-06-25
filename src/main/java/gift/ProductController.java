f4ae9ff75c6bde2d1b37b299d3f5dcddfa23cacd
5win <woozzoo7@gmail.com>
2024-06-25 17:19:47 +0900

    feat: Add products POST method endpoint - payload의 정보에 맞는 상품 객체를 생성하여 저장.

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,6 +1,8 @@
 package gift;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -16,5 +18,14 @@ public class ProductController {
         return productMap.values()
                 .stream()
                 .toList();
+    }
+
+    @PostMapping("/api/products")
+    public String addProduct(@RequestBody Product newProduct) {
+        if(productMap.containsKey(newProduct.id())) {
+            return "이미 상품이 등록되어있습니다.";
+        }
+        productMap.put(newProduct.id(), newProduct);
+        return "상품 등록이 완료되었습니다.";
     }
 }
