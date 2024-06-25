78e53880cfdec491592c9516aecb3b638bd5f289
alsswl <alswl4223@naver.com>
2024-06-25 17:54:36 +0900

    feat : Menu DTO 생성

---
+++ src/main/java/Menu/Menu.java
@@ -0,0 +1,16 @@
+package Menu;
+
+public class Menu {
+    private Long id;
+    private String name;
+    private int price;
+    private String imageUrl;
+
+    public Menu(Long id, String name, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+}
