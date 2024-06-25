2ced5bb58a9bf3a6e7e71ad29e6f44c6cc983228
wvssm <neoramiregi@gmail.com>
2024-06-25 11:35:58 +0900

    feat(ProductService): add Product 등록 Service

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,4 +1,44 @@
-package gift;
+package gift.service;
 
+import gift.domain.Product;
+import gift.dto.ProductRequestDto;
+import gift.dto.ProductResponseDto;
+import gift.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    public ProductResponseDto save(ProductRequestDto product){
+        return productRepository.save(product.toEntity()).toDto();
+    }
+
+    /*
+    public Product findById(int id){
+
+    }
+
+    public List<Product> findAll(){
+
+    }
+
+    public void deleteById(Long id){
+
+    }
+
+    public void updateById(Long id){
+
+    } */
 }
