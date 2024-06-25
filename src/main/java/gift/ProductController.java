3082db11164e538da8e65d224b789ecd7fbd1680
Kim Sanghae <pelikan@pusan.ac.kr>
2024-06-25 14:25:41 +0900

    feat: init ProductController

---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,36 @@
+package gift;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    private final Map<Long, Product> products = new HashMap<>();
+    private final AtomicLong listId = new AtomicLong(1);
+
+    @PostMapping("/api/products")
+    public void CreateProducts(@RequestParam("id") Long id, @RequestParam("name") String name,
+        @RequestParam("price") int price, @RequestParam("imageUrl") String imgUrl) {
+        var product = new Product(id, name, price, imgUrl);
+        products.put(listId.getAndIncrement(), product);
+    }
+
+    @GetMapping("/api/products")
+    public String ReadProducts() throws JsonProcessingException {
+        if (products.isEmpty()) {
+            return "Empty";
+        }
+        var objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(new ArrayList<Product>(products.values()));
+    }
+
+}
