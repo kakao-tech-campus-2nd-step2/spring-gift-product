2108e49a2d9691464f6d6fdc2eba0a475e17ee02
kmj452 <111856852+kmj452@users.noreply.github.com>
2024-06-25 13:16:58 +0900

    feat:PruductModel.java 추가

---
+++ src/main/java/gift/PruductModel.java
@@ -0,0 +1,47 @@
+package gift;
+
+public class PruductModel {
+    private long id;
+    private String name;
+    private int price;
+    private String imageURL;
+
+    public PruductModel(long id, String name, int price, String imageURL) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageURL = imageURL;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
