54970c1f0467e7bb3c73fc6e6f0f14d4b4e9987a
pjhcsols <pjhcsols@naver.com>
2024-06-26 21:53:57 +0900

    feat: 이미지 업로드 기능 로직 구현[박한솔]

--- src/main/java/gift/controller/admin/AdminProductController.java
+++ src/main/java/gift/controller/admin/AdminProductController.java
@@ -31,14 +31,23 @@ public class AdminProductController {
     public String addProduct(@RequestParam String name,
                              @RequestParam Long price,
                              @RequestParam String description,
-                             @RequestPart String imageUrl) {
+                             @RequestPart MultipartFile imageFile) {
+        try {
+            // 이미지 저장
+            String imagePath = ImageStorageUtil.saveImage(imageFile, null);
+
+            // 이미지 URL을 Base64로 인코딩
+            String imageUrl = ImageStorageUtil.encodeImagePathToBase64(imagePath);
 
             // 제품 정보 저장
             Product product = new Product(null, name, price, description, imageUrl);
             productService.addProduct(product);
 
             return "redirect:/admin/products";
-
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "error";
+        }
     }
 
     @PostMapping("/update/{id}")
@@ -46,9 +55,16 @@ public class AdminProductController {
                                 @RequestParam String name,
                                 @RequestParam Long price,
                                 @RequestParam String description,
-                                @RequestPart String imageUrl) {
+                                @RequestPart MultipartFile imageFile) {
+        try {
             Product product = productService.getProductById(id);
 
+            if (product.getImageUrl() != null && !product.getImageUrl().isEmpty()) {
+                ImageStorageUtil.deleteImage(ImageStorageUtil.decodeBase64ImagePath(product.getImageUrl()));
+            }
+
+            String imagePath = ImageStorageUtil.saveImage(imageFile, id);
+            String imageUrl = ImageStorageUtil.encodeImagePathToBase64(imagePath);
 
             product.setName(name);
             product.setPrice(price);
@@ -58,17 +74,20 @@ public class AdminProductController {
             productService.updateProduct(product);
 
             return "redirect:/admin/products";
-
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "error";
+        }
     }
 
     @PostMapping("/delete/{id}")
     public String deleteProduct(@PathVariable Long id) {
         Product product = productService.getProductById(id);
+        if (product != null && product.getImageUrl() != null && !product.getImageUrl().isEmpty()) {
+            ImageStorageUtil.deleteImage(ImageStorageUtil.decodeBase64ImagePath(product.getImageUrl()));
+        }
         productService.deleteProduct(id);
 
         return "redirect:/admin/products";
     }
-
-
-
 }
