b7bc16a666e3141f073b69d29544df5ed65e8add
sanghee0820 <lsh277604@gmail.com>
2024-06-26 22:38:09 +0900

    feat: 특정 Product 반환 API 추가

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -3,11 +3,10 @@ package gift.controller;
 
 import gift.model.Product;
 import gift.model.ProductRepository;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,5 +23,11 @@ public class ProductController {
     public List<Product> getAllProducts(){
 
         return productRepository.findAll();
+    }
+
+    @GetMapping("/api/products/{id}")
+    public Product getProduct(@PathVariable("id") Long id){
+
+        return productRepository.find(id);
     }
 }
