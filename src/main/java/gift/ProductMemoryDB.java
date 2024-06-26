39e9b36975370909d5ae0078378f41898cf0b1e2
SeongHun <tjdgns5506@gmail.com>
2024-06-26 18:05:27 +0900

    feat: 상품 개별,묶음 삭제 기능 구현

--- src/main/java/gift/ProductMemoryDB.java
+++ src/main/java/gift/ProductMemoryDB.java
@@ -14,8 +14,14 @@ public class ProductMemoryDB {
     // Private constructor to prevent instantiation
     private ProductMemoryDB() {
         //초기 상품
-        Product testProduct = new Product(1L, "Americano", 4500, "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg");
-        products.put(1L, testProduct);
+        Product defaultProduct1 = new Product(1L, "Americano", 4500, "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg");
+        products.put(1L, defaultProduct1);
+        Product defaultProduct2 = new Product(2L, "Latte", 5500, "https://cdn.pixabay.com/photo/2023/07/08/13/17/coffee-8114518_1280.png");
+        products.put(2L, defaultProduct2);
+        Product defaultProduct3 = new Product(3L, "Sandwich", 7700, "https://cdn.pixabay.com/photo/2023/08/12/02/58/sandwich-8184642_1280.png");
+        products.put(3L, defaultProduct3);
+        Product defaultProduct4 = new Product(4L, "Cup Cake", 10000, "https://cdn.pixabay.com/photo/2023/05/31/14/41/ai-generated-8031574_1280.png");
+        products.put(4L, defaultProduct4);
     }
 
     public static ProductMemoryDB getInstance() {
@@ -49,6 +55,11 @@ public class ProductMemoryDB {
 
     public void removeProduct(Long id) {
         products.remove(id);
+    }
+    public void removeProducts(List<Long> productIds){
+        for (Long id : productIds) {
+            products.remove(id);
+        }
     }
 
 }
