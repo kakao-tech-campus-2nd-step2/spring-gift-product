2ee412ab54bac97b5ee8a528af6885fd804ea862
Kiwoong Kim <gw150311@naver.com>
2024-06-26 23:32:47 +0900

    feat(ProductController): 상품 삭제 기능

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,5 +60,18 @@ public class ProductController {
         return new ResponseEntity<>(product, HttpStatus.CREATED); // 201 Created
     }
 
+    /**
+     * 상품 삭제
+     * @param id 삭제할 상품의 id
+     * @return 삭제에 성공하면 204 NO CONTENT, 해당 ID의 상품이 없으면 404 NOT FOUND
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        Product product = products.remove(id);
+        if (product == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
