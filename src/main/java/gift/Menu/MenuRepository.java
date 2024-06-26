6bf47bc6b430ce77111245a6cafa6518dce1cc73
alsswl <alswl4223@naver.com>
2024-06-26 12:23:48 +0900

    feat : MenuRepository에 get을 위한 메서드 추가

--- src/main/java/gift/Menu/MenuRepository.java
+++ src/main/java/gift/Menu/MenuRepository.java
@@ -3,11 +3,12 @@ package gift.Menu;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
 public class MenuRepository {
-    private final Map<Long,Menu> menus = new HashMap<>();
+    private static final Map<Long,Menu> menus = new HashMap<>();
     private Long id = 1L;
 
     public Menu save(Menu menu){
@@ -18,4 +19,10 @@ public class MenuRepository {
     public void delete(Menu menu){
         menus.remove(menu.getId());
     }
+    public static List<Menu> findAll(){
+        return menus.values()
+                .stream()
+                .toList();
+    }
+
 }
