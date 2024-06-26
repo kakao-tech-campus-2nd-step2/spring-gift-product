a1bef1a0235ea919ed28dbb48c45bce985d3199d
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-06-26 11:27:18 +0900

    feat: ProductController

---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,58 @@
+package gift;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/products")
+public class ProductController {
+    private final Map<Long, Product> products = new HashMap<>();
+    private long counter = 8146027;
+
+    @GetMapping
+    public ResponseEntity<Collection<Product>> getAllProducts() {
+        return ResponseEntity.ok(products.values());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable long id) {
+        Product product = products.get(id);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        long id = counter++;
+        product.setId((int)id);
+        products.put(id, product);
+        return ResponseEntity.ok(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updataProduct(@PathVariable long id, @RequestBody Product updatedProduct) {
+        if (!products.containsKey(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        updatedProduct.setId((int) id);
+        products.put(id, updatedProduct);
+        return ResponseEntity.ok(updatedProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable long id) {
+        Product removed = products.remove(id);
+        if (removed == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+
+}
