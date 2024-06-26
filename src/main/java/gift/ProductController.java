a82c38249e39fda3ba595afb51846a56ccfd091e
hyunseo <wndlthsk@naver.com>
2024-06-26 22:25:40 +0900

    feat : 조회 기능 구현

---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,25 @@
+package gift;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final Map<Long, Product> products = new HashMap<>();
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return new ArrayList<>(products.values());
+    }
+
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable(value = "id") long id) {
+        return products.get(id);
+    }
+
+
+
+}
