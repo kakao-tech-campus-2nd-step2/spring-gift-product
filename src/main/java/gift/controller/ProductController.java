500443bc9c7e523ccd2a0357cfe4c10c725b536c
Jeje <jjh4450jjh4450@gmail.com>
2024-06-26 15:30:35 +0900

    feat(Product.java): Product.java 생성 및 컨트롤러에 적용

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,8 @@
+package gift.controller;
+import gift.model.Product;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductController {
+    private final Map<Long, Product> producsts = new HashMap<>();
+}
