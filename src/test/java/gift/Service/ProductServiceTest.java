1b90969ba1359dfc4ed4221fb603ec02332361c5
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-06-26 23:14:06 +0900

    test: 주석 추가

--- src/test/java/gift/Service/ProductServiceTest.java
+++ src/test/java/gift/Service/ProductServiceTest.java
@@ -32,12 +32,14 @@ class ProductServiceTest {
   @Test
   @DisplayName("상품 생성")
   public void CreateProduct() {
-    productService.getProduct(3l);
+    //    가장 마지막 상품 가져오기
+    productService.getProduct((long) productService.getProductList().size() - 1);
     //  how
     Product.CreateProduct temp = new Product.CreateProduct("test4", 100, "test4");
     //  when
     productService.createProduct(temp);
     //  then
+    //    추가한 상품의 이름과 마지막에 위치한 상품의 이름 비교
     assertEquals(temp.getName(),
         productService.getProduct((long) productService.getProductList().size() - 1).getName());
   }
@@ -45,12 +47,14 @@ class ProductServiceTest {
   @Test
   @DisplayName("상품 수정")
   public void UpdateProduct() {
-    productService.getProduct(3l);
+    //    가장 마지막 상품 가져오기
+    productService.getProduct((long) productService.getProductList().size() - 1);
     //  how
     Product.UpdateProduct temp = new Product.UpdateProduct("test4-1", 100, "test4-1");
     //  when
     productService.updateProduct(temp, (long) productService.getProductList().size() - 1);
     //  then
+    //    수정한 상품의 이름과 마지막에 위치한 상품의 이름 비교
     assertEquals(temp.getName(),
         productService.getProduct((long) productService.getProductList().size() - 1).getName());
   }
@@ -58,13 +62,15 @@ class ProductServiceTest {
   @Test
   @DisplayName("상품 삭제")
   public void removeProduct() {
-    productService.getProduct(3l);
+    //    가장 마지막 상품 가져오기
+    productService.getProduct((long) productService.getProductList().size() - 1);
     //  how
     int before = productService.getProductList().size();
     Long target = (long) before - 1;
     //  when
     productService.deleteProduct(target);
     //  then
+    //    삭제 전후의 크기 비교
     assertEquals(before - 1, productService.getProductList().size());
   }
 
