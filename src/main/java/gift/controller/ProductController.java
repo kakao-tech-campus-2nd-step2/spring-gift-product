3400f9d51ccf819865d2275728dca365c486c9d0
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-06-26 14:29:50 +0900

    feat: 상품 ì¡ 조회 - 전체 기능 ê

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,10 +1,13 @@
 package gift.controller;
 
 import gift.vo.Product;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -13,4 +16,12 @@ public class ProductController {
 
     private final Map<Long, Product> products = new HashMap<>();
 
+    /**
+     * 상품 조회 - 전체
+     * @return 전체 상품 목록
+     */
+    @GetMapping()
+    public List<Product> getAllProducts() {
+        return new ArrayList<>(products.values());
+    }
 }
