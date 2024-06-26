0e290b566fc7d1858f4632089e4d173348c10fec
Yongjin An <yongjin5352@gmail.com>
2024-06-26 20:49:53 +0900

    feat: add ProductRecord class for storing product information
    and some code for testing if record class works well

---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,27 @@
+package gift;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @GetMapping("/products")
+    public ProductRecord[] getAllProducts() {
+        ProductRecord record1 = new ProductRecord(
+                "1",
+                "김밥",
+                5000,
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Gimbap_%28pixabay%29.jpg/220px-Gimbap_%28pixabay%29.jpg"
+        );
+        ProductRecord record2 = new ProductRecord(
+                "3",
+                "라면",
+                5000,
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Jin_Ramen_%28spicy%29_20210731_002.jpg/220px-Jin_Ramen_%28spicy%29_20210731_002.jpg"
+        );
+
+        ProductRecord[] products = new ProductRecord[] {record1, record2};
+
+        return products;
+    }
+}
---
+++ src/main/java/gift/ProductRecord.java
@@ -0,0 +1,4 @@
+package gift;
+
+public record ProductRecord(String id, String name, int price, String imageUrl) {
+}
