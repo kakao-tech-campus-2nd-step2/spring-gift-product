13c15384bcd407795bef4a96f53379afd1965c77
humpose <yjpx416@gamil.com>
2024-06-26 16:16:19 +0900

    어떤 설정도 하지 않는 기본 생성자 추가

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -6,6 +6,10 @@ public class Product {
     private int price;
     private String imageUrl;
 
+    // 기본 생성자
+    public Product() {
+    }
+
     public Product(long id, String name, int price, String imageUrl) {
         this.id = id;
         this.name = name;
