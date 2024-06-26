84c329591545f57ff23c6f5962d7ea7d507d64fa
Giheon Do <dgh0001@naver.com>
2024-06-26 21:36:32 +0900

    refactor: change price int to Integer

--- src/main/java/gift/model/ProductForm.java
+++ src/main/java/gift/model/ProductForm.java
@@ -3,10 +3,10 @@ package gift.model;
 public class ProductForm {
 
     private String name;
-    private int price;
+    private Integer price;
     private String imageUrl;
 
-    public ProductForm(String name, int price, String imageUrl) {
+    public ProductForm(String name, Integer price, String imageUrl) {
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
@@ -16,12 +16,24 @@ public class ProductForm {
         return name;
     }
 
-    public int getPrice() {
+    public Integer getPrice() {
         return price;
     }
 
     public String getImageUrl() {
         return imageUrl;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
     @Override
