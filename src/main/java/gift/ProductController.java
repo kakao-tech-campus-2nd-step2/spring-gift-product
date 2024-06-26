3f0e0ec806918f9493fd18d99e92d385f8b13994
amm0124 <amm012450@gmail.com>
2024-06-26 16:10:59 +0900

    FIX : GET, POST, DELETE 수정

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -20,37 +20,43 @@ public class ProductController {
         return products.values();
     }
 
-    //ID를 통한 물품 조회
+    //ID를 통한 물품 조회 - product를 return하면 JSON이 된다.
     @GetMapping("/products/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable Long id) {
-        Product product = products.get(id);
-        if (product == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+
+        for (Product product : products.values()) {
+            if(product.id() == id){
+                return ResponseEntity.ok(product);
+            }
         }
-        return ResponseEntity.ok(product);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 
     // POST - 상품 추가
     @PostMapping("/products")
-    public ResponseEntity<String> addProduct(@RequestParam("id") Long id, @RequestParam("name") String name,
-                                             @RequestParam("price") int price, @RequestParam("imageUrl") String imageUrl) {
-        if (products.containsKey(id)) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("중복된 id입니다.");
+    public ResponseEntity<String> addProduct(@RequestParam("product") Product addproduct) {
+
+        long id = addproduct.id();
+        for (Product product : products.values()) {
+            if(product.id() == id){
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("중복된 id입니다.");
+            }
         }
-        Product product = new Product(id, name, price, imageUrl);
-        products.put(++sequence, product);
+        products.put(++sequence, addproduct);
         return ResponseEntity.status(HttpStatus.CREATED).body("Product added successfully.");
     }
 
+
     // DELETE - 상품 삭제
+    @DeleteMapping("/product/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
-        if (!products.containsKey(id)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("존재하지 않는 상품입니다.");
+        for (Product product : products.values()) {
+            if(product.id() == id){
+                products.values().remove(product);
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Product deleted successfully.");
+            }
         }
-        products.remove(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Product deleted successfully.");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("존재하지 않는 id입니다.");
     }
-
-
 
 }
