77b22183c52c31313295dc5594a192a60aa841a8
mac <20j.code@gmail.com>
2024-06-26 14:51:09 +0900

    feat : 상품 데이터 추가
    id, name, price, imageUrl 추가
    
    id는 생성 후 바뀌지 않음으로 final로 설정 - 수정을 막음

---
+++ src/main/java/com/kakaotech2/j20/Product.java
@@ -0,0 +1,19 @@
+package com.kakaotech2.j20;
+
+public class Product {
+
+    private final long id;
+
+    private String name;
+
+    private int price;
+
+    private String imageUrl;
+
+    public Product(long id, String name, int priace, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+}
