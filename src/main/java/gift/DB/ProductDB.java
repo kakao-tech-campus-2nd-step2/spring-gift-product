4f4973b8c51858f203a3dfd42579ec4d146a5b92
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-06-26 23:03:18 +0900

    feat: 저장을 위한 메모리db 구현

---
+++ src/main/java/gift/DB/ProductDB.java
@@ -0,0 +1,42 @@
+package gift.DB;
+
+import gift.DTO.ProductDTO;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductDB {
+
+  private static final Map<Long, ProductDTO> products = new HashMap<>();
+
+  public List<ProductDTO> getList() {
+    return products.values().stream().toList();
+  }
+
+  public ProductDTO getProduct(Long id) {
+    return products.get(id);
+  }
+
+  public int getLastIndex() {
+    return products.size();
+  }
+
+  public void setProduct(long id, ProductDTO product) {
+    products.put(id, product);
+  }
+
+  public void updateProduct(long id, ProductDTO product) {
+    products.replace(id, product);
+  }
+
+  public void removeProduct(long id) {
+    products.remove(id);
+  }
+
+  public Boolean validateId(Long id) {
+    return products.containsKey(id);
+  }
+
+}
