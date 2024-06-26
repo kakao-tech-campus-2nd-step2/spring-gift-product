16caf8c7d87e8c090e75905395f6adb0919ab681
sanghee0820 <lsh277604@gmail.com>
2024-06-26 22:38:22 +0900

    test: 특정 Product 반환 테스트 추가

--- src/test/java/gift/controller/ProductControllerTest.java
+++ src/test/java/gift/controller/ProductControllerTest.java
@@ -29,7 +29,7 @@ class ProductControllerTest {
     @DisplayName("모든 Product 반환 테스트")
     void getAllProducts () throws Exception {
 
-        //then
+        //given
         Product product1 = new Product(8146027L, "아이스 카페 아메리카노 T", 4500,
                 "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg");
         Product product2 = new Product(8146028L, "아이스 라떼 T", 5000,
@@ -54,5 +54,25 @@ class ProductControllerTest {
                 .andExpect(jsonPath("$[1].price").value(5000))
                 .andExpect(jsonPath("$[1].imgUrl").value(
                         "https://st.kakaocdn.net/product/gift/product/20231010111814_bbbff9eccc943648797925498bdd8a3.jpg"));
+    }
+
+    @Test
+    @DisplayName("특정 Product 반환 테스트")
+    void getProduct () throws Exception {
+        //given
+        Product product = new Product(8146027L, "아이스 카페 아메리카노 T", 4500,
+                "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg");
+        //when
+        when(productRepository.find(8146027L)).thenReturn(product);
+
+        //then
+        mockMvc.perform(get("/api/products/8146027"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4))
+                .andExpect(jsonPath("id").value(8146027))
+                .andExpect(jsonPath("name").value("아이스 카페 아메리카노 T"))
+                .andExpect(jsonPath("price").value(4500))
+                .andExpect(jsonPath("imgUrl").value(
+                        "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
     }
 }
