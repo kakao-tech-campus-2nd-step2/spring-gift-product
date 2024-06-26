32bc987174bf979a4a228c9ede75a61a447fee7d
HSHwan <tnghks2866@gmail.com>
2024-06-26 11:53:28 +0900

    refactor: 상품 도메인으로 생성하도록 수정

--- src/main/java/gift/dto/ProductResponse.java
+++ src/main/java/gift/dto/ProductResponse.java
@@ -1,6 +1,12 @@
 package gift.dto;
 
+import gift.domain.Product;
+
 public record ProductResponse(Long id,
                               String name,
                               int price,
-                              String imageUrl) { }
+                              String imageUrl) {
+    public ProductResponse(Product product) {
+        this(product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
+    }
+}
