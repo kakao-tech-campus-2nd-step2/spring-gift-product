14eef739b8aa4b8bbfe29938d7dc6167c4e69322
donghyun kim <gimdonghyun5434@gmail.com>
2024-06-26 14:59:37 +0900

    refactor : rename folder from 'Controller' to 'controller'

--- src/main/java/gift/Controller/ProductController.java
+++ src/main/java/gift/Controller/ProductController.java
@@ -1,10 +1,12 @@
-package gift.Controller;
+package gift.controller;
 
 import gift.Product;
 import gift.ProductRepository;
+import gift.exception.ProductNotFoundException;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,5 +32,17 @@ public class ProductController {
         List<Product> products = productRepository.getAllProducts();
         return new ResponseEntity<>(products,HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id){
+        try{
+            Product product = productRepository.findById(id);
+            return new ResponseEntity<>(product,HttpStatus.OK);
+        }catch(ProductNotFoundException e){
+            return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+        }
+    }
+
+    
 
 }
