cc241d17de722b4fe896b7bd09b4a9ef9aa2f209
sanghee0820 <lsh277604@gmail.com>
2024-06-27 00:15:04 +0900

    feat: 새로운 Product 저장 API 추가

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,12 +1,15 @@
 package gift.controller;
 
 
+import gift.controller.res.ProductRequest;
 import gift.model.Product;
 import gift.model.ProductRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,5 +32,10 @@ public class ProductController {
     public Product getProduct(@PathVariable("id") Long id){
 
         return productRepository.find(id);
+    }
+
+    @PostMapping("/api/products")
+    public void productSave(@RequestBody ProductRequest newProduct){
+        productRepository.save(newProduct.toModel());
     }
 }
