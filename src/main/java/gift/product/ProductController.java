b3d142fab5034eb057a58d2761ef87fe209b1475
Profile-exe <kang67572346@gmail.com>
2024-06-26 15:15:25 +0900

    feat(ProductController): 상품 조회 API 구현
    - 전체 상품 목록 조회: /products
    - 단일 상품 조회: /products/{productId}

---
+++ src/main/java/gift/product/ProductController.java
@@ -0,0 +1,43 @@
+package gift.product;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class ProductController {
+
+    private final Map<Long, Product> products = new HashMap<>();
+
+    @GetMapping("/products")
+    public List<ProductResDto> getProducts() {
+        Collection<Product> productList = products.values();
+
+        return productList.stream()
+                .map(product -> new ProductResDto(
+                        product.getId(),
+                        product.getName(),
+                        product.getPrice(),
+                        product.getImageUrl()
+                )).toList();
+    }
+
+    @GetMapping("/products/{productId}")
+    public ProductResDto getProduct(@PathVariable Long productId) {
+        Product product = products.get(productId);
+
+        return new ProductResDto(
+                product.getId(),
+                product.getName(),
+                product.getPrice(),
+                product.getImageUrl()
+        );
+    }
+
+}
