e732503af3c328f8aff493173ddeff8b91076bec
HSHwan <tnghks2866@gmail.com>
2024-06-26 12:17:32 +0900

    fix: 상품 전체 조회 반환 타입 수정

--- src/main/java/gift/api/ProductController.java
+++ src/main/java/gift/api/ProductController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
@@ -23,7 +22,7 @@ public class ProductController {
     // 상품 전체 조회
     @GetMapping
     @ResponseBody
-    public List<Product> getAllProducts() {
+    public List<ProductResponse> getAllProducts() {
         return productService.getAllProducts();
     }
 
