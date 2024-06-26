4fd286225c83b757fccb5b7e1fff7e4999f5f7ee
amm0124 <amm012450@gmail.com>
2024-06-26 15:43:00 +0900

    FEAT : row를 나타내는 sequence 추가 및 addProduct 추가

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -4,27 +4,45 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
+
     private final Map<Long, Product> products = new HashMap<>();
-    
+    private long sequence = 0L;
+
     // 전체 상품 조회
     @GetMapping("/products")
     public Collection<Product> getProducts() {
         return products.values();
     }
-    
+
     //ID를 통한 물품 조회
     @GetMapping("/products/{id}")
-    public Product getProductById(@PathVariable Long id) {
-        return products.get(id);
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = products.get(id);
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(product);
     }
+
+    // POST - 상품 추가
+    @PostMapping("/products")
+    public ResponseEntity<String> addProduct(@RequestParam("id") Long id, @RequestParam("name") String name,
+                                             @RequestParam("price") int price, @RequestParam("imageUrl") String imageUrl) {
+        if (products.containsKey(id)) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("중복된 id입니다.");
+        }
+        Product product = new Product(id, name, price, imageUrl);
+        products.put(++sequence, product);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Product added successfully.");
+    }
+
+
 
 
 
