34d117162d15fcbac43ade1945433fc16569a6f9
Profile-exe <kang67572346@gmail.com>
2024-06-26 15:00:54 +0900

    feat(Product): 상품 정보를 저장하는 클래스
    - static 변수를 두어 id를 자동으로 증가시킴
    - id는 setter를 구현하지 않아 id를 변경할 수 없게 함
    - getter는 모두 열어두어 Dto를 생성할 때 필요한 값들을 제공

---
+++ src/main/java/gift/product/Product.java
@@ -0,0 +1,46 @@
+package gift.product;
+
+public class Product {
+
+    private static Long idCounter = 1L;
+
+    private Long id;
+    private String name;
+    private Integer price;
+    private String imageUrl;
+
+    public Product(String name, Integer price, String imageUrl) {
+        this.id = idCounter++;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
