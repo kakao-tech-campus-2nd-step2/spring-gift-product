f795bdee0bb91ebd45e2f2c40e1cb19f5788c95a
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-06-26 12:34:06 +0900

    feat : 상품 추가 HTTP API 구현

--- src/main/java/gift/Controller/ProductController.java
+++ src/main/java/gift/Controller/ProductController.java
@@ -2,10 +2,23 @@ package gift.Controller;
 
 import gift.Model.Product;
 import java.util.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-    private final Map<Long, Product> producsts = new HashMap<>();
+    private final Map<Long, Product> products = new HashMap<>();
 
+    @PostMapping("/api/products")
+    public Product addProduct(
+        @RequestParam(value = "id") long id,
+        @RequestParam(value = "name") String name,
+        @RequestParam(value = "price") long price,
+        @RequestParam(value = "imageUrl") String imageUrl
+    ) {
+        Product newProduct = new Product(id, name, price, imageUrl);
+        products.put(id, newProduct);
+        return newProduct;
+    }
 }
