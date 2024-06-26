1435e997b10ce1a2ffba55257460b0880250a8c8
mac <20j.code@gmail.com>
2024-06-26 14:03:24 +0900

    init

---
+++ src/test/java/com/kakaotech2/_jcode/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaotech2._jcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
