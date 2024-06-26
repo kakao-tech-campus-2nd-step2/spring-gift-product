93085b0ab06c98bdc414bc3893a65bcd12977c06
SeongHun <tjdgns5506@gmail.com>
2024-06-26 17:55:30 +0900

    feat: 상품 추가 구현

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -3,15 +3,16 @@ package gift;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 
 @Controller
 public class ProductController {
     ProductMemoryDB productDB = ProductMemoryDB.getInstance();
 
+    //상품 보여주기
     @GetMapping("/")
     public String getProducts(Model model) {
 
@@ -20,5 +21,31 @@ public class ProductController {
         return "version-SSR/index.html";
     }
 
+    //상품 추가하기
+    @GetMapping("/add")
+    public String getAddForm(Model model) {
+        model.addAttribute("product", new Product()); // Add an empty Product object for the form
+        return "version-SSR/add-form.html";
+    }
+
+    @PostMapping("/add")
+    public String addProduct(Product product) {
+        productDB.addProduct(product);
+        return "redirect:/";
+    }
+    //상품 삭제하기
+    // POST 요청으로 선택된 상품들 삭제
+    @PostMapping("/deleteSelected")
+    public String deleteSelectedProduct(@RequestParam("productIds") List<Long> productIds) {
+        System.out.println("선택");
+        productDB.removeProducts(productIds);
+        return "redirect:/";
+    }
+    @PostMapping("/delete")
+    public String deleteProduct(@RequestParam("productId") Long productId ){
+        productDB.removeProduct(productId);
+        System.out.println(productId);
+        return "redirect:/";
+    }
 
 }
