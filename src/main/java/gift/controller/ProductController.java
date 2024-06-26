4b94b215dd253405c5054db20bdc48b02c5548cf
HaegyeongKim01 <haegyeong.kim01@gmail.com>
2024-06-26 14:32:51 +0900

    feat: 상품 조회 - 한 개

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,7 +1,10 @@
 package gift.controller;
 
 import gift.vo.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +27,19 @@ public class ProductController {
     public List<Product> getAllProducts() {
         return new ArrayList<>(products.values());
     }
+
+    /**
+     * 상품 조회 - 한 개
+     * @param id 조회할 상품의 ID
+     * @return 조회한 product
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable(value = "id") Long id) {
+        Product product = products.get(id);
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(product);
+    }
+
 }
