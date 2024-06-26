0b21b9c7a56258accb90f98293309717ee08c30f
amm0124 <amm012450@gmail.com>
2024-06-26 15:05:54 +0900

    FRAT : CRUD READ - GET method 구현

---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,31 @@
+package gift;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final Map<Long, Product> products = new HashMap<>();
+    
+    // 전체 상품 조회
+    @GetMapping("/products")
+    public Collection<Product> getProducts() {
+        return products.values();
+    }
+    
+    //ID를 통한 물품 조회
+    @GetMapping("/products/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return products.get(id);
+    }
+
+
+
+}
