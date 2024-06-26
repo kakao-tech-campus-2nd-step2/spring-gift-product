9d0f0b1620c1f7101ad396f83e7b2cd146aaf07b
youcaslte03 <luckthb325@naver.com>
2024-06-27 02:15:07 +0900

    feat: products-form.html 추가

--- src/main/java/gift/Controller/AdminController.java
+++ src/main/java/gift/Controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class AdminController {
 
     private final Products products;
