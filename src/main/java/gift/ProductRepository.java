0471126f6af4d0a93b494032e090d5bbbeb08ee6
donghyun kim <gimdonghyun5434@gmail.com>
2024-06-26 15:37:54 +0900

    refactor : findById메서드 리펙토링

---
+++ src/main/java/gift/ProductRepository.java
@@ -0,0 +1,34 @@
+package gift;
+
+import gift.exception.ProductNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductRepository {
+    private final Map<Long,Product> products = new HashMap<>();
+    private Long nextId = 1L;
+
+    public void addProduct(Product product) {
+        products.put(nextId++,product);
+    }
+
+    public List<Product> getAllProducts() {
+        return new ArrayList<>(products.values());
+    }
+
+    public Product findById(Long id) throws ProductNotFoundException{
+        if(validateProduct(id)){
+            return products.get(id);
+        }
+    }
+
+    private Boolean validateProduct(Long id) throws ProductNotFoundException{
+        Product product = products.get(id);
+        if (product == null){
+            throw new ProductNotFoundException("product is not found");
+        }
+        return true;
+    }
+}
