5883e3142ef96769e30888f9e07e141f6fda1d7d
HSHwan <tnghks2866@gmail.com>
2024-06-26 11:55:49 +0900

    feat: 상품 서비스 전체 상품 기능 구현

---
+++ src/main/java/gift/application/ProductService.java
@@ -0,0 +1,24 @@
+package gift.application;
+
+import gift.dao.ProductRepository;
+import gift.domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+}
