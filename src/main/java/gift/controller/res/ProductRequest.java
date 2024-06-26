06a8fd71c0ab9d6a3335f228f8610e76396001af
sanghee0820 <lsh277604@gmail.com>
2024-06-27 01:07:07 +0900

    refactor: request에서 id값 삭제

--- src/main/java/gift/controller/res/ProductRequest.java
+++ src/main/java/gift/controller/res/ProductRequest.java
@@ -2,9 +2,13 @@ package gift.controller.res;
 
 import gift.model.Product;
 
-public record ProductRequest(Long id, String name, Integer price, String imgUrl) {
+public record ProductRequest(String name, Integer price, String imgUrl) {
 
     public Product toModel(){
-        return new Product(id(), name(), price(), imgUrl());
+        return new Product(null, name(), price(), imgUrl());
+    }
+
+    public Product toModel(final Long id){
+        return new Product(id, name(), price(), imgUrl());
     }
 }
