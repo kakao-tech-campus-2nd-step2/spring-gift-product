6a39b24978817cdbf31826937a916d81a4197f7d
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-06-26 10:35:14 +0900

    feat : 상품 삭제 기능 추가

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,5 +48,12 @@ public class ProductController {
         return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
     }
 
-
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        if (!products.containsKey(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        products.remove(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
