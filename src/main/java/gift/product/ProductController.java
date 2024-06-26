3e7cd5df07e0538281e85bc99d018f1227f84d58
Profile-exe <kang67572346@gmail.com>
2024-06-26 15:18:51 +0900

    fix(ProductController): Controller를 RestController로 변경

--- src/main/java/gift/product/ProductController.java
+++ src/main/java/gift/product/ProductController.java
@@ -4,12 +4,12 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class ProductController {
 
