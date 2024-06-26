10b4f8e66e3947eeb11d42be8f5747bd5e8904fe
suhyeon <nucc1186@gamil.com>
2024-06-26 13:55:12 +0900

    feat(ProductController.java): getProduct

--- src/main/java/Controller/ProductController.java
+++ src/main/java/Controller/ProductController.java
@@ -1,2 +1,24 @@
-package Controller;public class ProductController {
+package Controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import Model.Product;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final Map<Long, Product> products = new HashMap<>();
+
+    @GetMapping("/{id:[1-9][0-9}*")
+    public Product getProduct(@PathVariable Long id){
+        if(!products.containsKey(id)){
+            System.out.println("{%d}에 매칭되는 Product가 없습니다.".formatted(id));
+            return null;
+        }
+        return products.get(id);
+    }
 }
