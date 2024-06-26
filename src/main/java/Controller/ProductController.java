857a57922261115caef41f8bf881a055f706bbcf
suhyeon <nucc1186@gamil.com>
2024-06-26 14:19:15 +0900

    feat(ProductController.class): updateProduct

--- src/main/java/Controller/ProductController.java
+++ src/main/java/Controller/ProductController.java
@@ -2,9 +2,11 @@ package Controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +33,14 @@ public class ProductController {
             return;
         }
         System.out.println("{%d}에 매칭되는 Product가 이미 존재합니다.".formatted(product.getId()));
+    }
+
+    @PutMapping("/{id:[1-9][0-9]*}")
+    public void updateProduct(@RequestBody Product product, @PathVariable Long id){
+        if(products.containsKey(product.getId())){
+            products.put(product.getId(), product);
+            return;
+        }
+        System.out.println("{%d}에 매칭되는 Product가 이미 존재하지 않습니다.");
     }
 }
