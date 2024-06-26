02c1f155fb74e1f39729eceacdaa3f32b9a2f137
sanghee0820 <lsh277604@gmail.com>
2024-06-26 14:34:30 +0900

    feat: Product 클래스 추가

---
+++ src/main/java/gift/model/Product.java
@@ -0,0 +1,31 @@
+package gift.model;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Integer price;
+    private String imgUrl;
+
+    public Product(Long id, String name, Integer price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
