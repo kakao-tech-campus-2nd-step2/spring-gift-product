ff557474a1a147ce3dbdf4641e8944c6e5361d98
Kiwoong Kim <gw150311@naver.com>
2024-06-26 23:26:13 +0900

    feat(ProductController): ID로 상품 조회 기능

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +27,20 @@ public class ProductController {
     public ResponseEntity<Map<Long, Product>> getProducts() {
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
+
+    /**
+     * id로 특정 상품 조회
+     * @param id 조회할 상품의 id
+     * @return 조회된 상품 객체
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = products.get(id);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK); // 200 OK
+        }
+        return ResponseEntity.notFound().build();
+
+    }
+
 }
