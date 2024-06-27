b199165cc1ba257907c99f5677d55f6706bb4d1a
mingjuu <tisu79@naver.com>
2024-06-27 22:02:18 +0900

    데이터 all, one 조회 기능 작성

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,4 +1,27 @@
 package gift.repository;
 
+import gift.domain.Product;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class ProductRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Product> findAll() {
+        String sql = "SELECT * FROM product";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Product.class));
+    }
+
+    public Product findById(Long id) {
+        String sql = "SELECT * FROM product WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Product.class), id);
+    }
 }
