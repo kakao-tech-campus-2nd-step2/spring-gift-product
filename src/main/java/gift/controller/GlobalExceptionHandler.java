95bb00bc7a1df9cc3eead242b0ccf43bb179fa41
kangwlgns <kangji0615@gmail.com>
2024-06-28 05:33:50 +0900

    fix(GlobalExceptionHandler): modify the return from http response to view

--- src/main/java/gift/controller/GlobalExceptionHandler.java
+++ src/main/java/gift/controller/GlobalExceptionHandler.java
@@ -1,56 +1,62 @@
 package gift.controller;
 
-import gift.exception.ErrorResponse;
+import static gift.utility.SecurityUtility.addPasswordAttribute;
+
 import gift.exception.ValidationException;
 import gift.exception.WrongParamException;
 import java.util.NoSuchElementException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 
 // 전역 예외 핸들러.
 @ControllerAdvice
-@RestController
+@Controller
 public class GlobalExceptionHandler {
 
     // ValidationException 핸들러 함수
     @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<ErrorResponse> handler(ValidationException validationException) {
-        // 어떤 에러가 발생했는지.
-        String message = "validation error";
-        // 예외의 message를 통해 자세한 내용을 가져옴.
-        String details = validationException.getMessage();
-        // ErrorResponse 객체를 만듦
-        var errorResponse = new ErrorResponse(message, details);
-        // ErrorResponse객체를 JSON으로 바꾼 것을 반환하면서 BAD_REQUEST(400)를 반환.
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    public String handler(ValidationException validationException, Model model) {
+        // 에러 메시지 받아옴
+        String message = validationException.getMessage();
+
+        // html로 넘길 attributes를 넣기
+        addAttributesForManagerPage(message, model);
+
+        return "html/error";
     }
 
     // NoSuchElement 핸들러 함수
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<ErrorResponse> handler(NoSuchElementException noSuchElementException) {
-        // 어떤 에러가 발생했는지.
-        String message = "no such element error";
-        // 예외의 message를 통해 자세한 내용을 가져옴.
-        String details = noSuchElementException.getMessage();
-        // ErrorResponse 객체를 만듦
-        var errorResponse = new ErrorResponse(message, details);
-        // ErrorResponse객체를 JSON으로 바꾼 것을 반환하면서 NOT_FOUND(404)를 반환.
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    public String handler(NoSuchElementException noSuchElementException, Model model) {
+        // 에러 메시지 받아옴
+        String message = noSuchElementException.getMessage();
+
+        // html로 넘길 attributes를 넣기
+        addAttributesForManagerPage(message, model);
+
+        return "html/error";
     }
 
     // WrongParamException 핸들러 함수
     @ExceptionHandler(WrongParamException.class)
-    public ResponseEntity<ErrorResponse> handler(WrongParamException wrongParamException) {
-        // 어떤 에러가 발생했는지.
-        String message = "wrong parameter error";
-        // 예외의 message를 통해 자세한 내용을 가져옴.
-        String details = wrongParamException.getMessage();
-        // ErrorResponse 객체를 만듦
-        var errorResponse = new ErrorResponse(message, details);
-        // ErrorResponse객체를 JSON으로 바꾼 것을 반환하면서 BAD_REQUEST(400)를 반환.
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    public String handler(WrongParamException wrongParamException, Model model) {
+        // 에러 메시지 받아옴
+        String message = wrongParamException.getMessage();
+
+        // html로 넘길 attributes를 넣기
+        addAttributesForManagerPage(message, model);
+
+        return "html/error";
+    }
+
+    // error.html에서 보여줄 attributes를 넣는 함수
+    private void addAttributesForManagerPage(String message, Model model) {
+        // 에러 메시지 넣어줌
+        model.addAttribute("message", message);
+
+        // 비밀번호를 최초만 맞추면 여기서부터는 계속 자동으로 넣어줌
+        addPasswordAttribute(model);
     }
 }
