be35fa9bf2cd2de57f9988453767100e6c84f065
mac <20j.code@gmail.com>
2024-06-28 03:19:17 +0900

    feat : ProductService 구현
    DTO와 인터페이스 함께 추가

--- src/main/java/com/kakaotech2/j20/service/ProductService.java
+++ src/main/java/com/kakaotech2/j20/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.kakaotech2.j20.service;
 
+import com.kakaotech2.j20.DTO.ProductDTO;
 import com.kakaotech2.j20.Product;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -7,9 +8,9 @@ import org.springframework.stereotype.Service;
 @Service
 public interface ProductService {
     //상품 리스트 전체 조회
-    List<Product> readAll();
+    List<ProductDTO> readAll();
     //새 상품 생성
-    void create(Product prod);
+    void create(ProductDTO prod);
     //상품 이름 변경
     void updateName(long id,String name);
     //상품 가격 변경
