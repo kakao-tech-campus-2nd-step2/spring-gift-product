cf09c51d1904b92eb0537f9bc6bff6575c9ea517
Giheon Do <dgh0001@naver.com>
2024-06-27 22:25:29 +0900

    feat: 애플리케이션이 실행될 때 테이블 새로 구축되도록 수정

--- src/main/java/gift/SpringConfig.java
+++ src/main/java/gift/SpringConfig.java
@@ -4,10 +4,14 @@ import gift.controller.AdminController;
 import gift.repository.JdbcProductRepository;
 import gift.repository.MemoryProductRepository;
 import gift.repository.ProductRepository;
+import jakarta.annotation.PostConstruct;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 @Configuration
 public class SpringConfig {
@@ -28,5 +32,12 @@ public class SpringConfig {
     public ProductRepository productRepository() {
 //        return new MemoryProductRepository();
         return new JdbcProductRepository(dataSource);
+    }
+
+    @PostConstruct
+    private void initializeDB() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("sql/ddl.sql"));
+        DatabasePopulatorUtils.execute(populator, dataSource);
     }
 }
