604250b59aaa78f5f832081ee2c85f436f2f5f6f
kangwlgns <kangji0615@gmail.com>
2024-06-28 02:23:05 +0900

    refactor(Product): change type of id to long

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -7,35 +7,35 @@ public class Product {
 
     // 필드
     // Products의 map key로 id를 쓸 예정이지만, 여기에도 id가 있는 것이 편할 것 같다.
-    private int id;
+    private long id;
     private String name;
     private int price;
-    private String img;
+    private String image;
 
     // 기본 생성자
     public Product() {
         id = -1;
         name = null;
         price = -1;
-        img = null;
+        image = null;
     }
 
     // 생성자
-    public Product(int id, String name, int price, String img) {
+    public Product(long id, String name, int price, String image) {
         // 모든 할당은 setter를 통해서만 하기. 생성도 마찬가지
         setId(id);
         setName(name);
         setPrice(price);
-        setImg(img);
+        setImage(image);
     }
 
     // getter는 JSON으로 바꿀 수 있도록 public으로 선언
-    public int getId() {
+    public long getId() {
         return id;
     }
 
     // setter는 사용을 폭넓은 지양하기 위해 private으로 선언
-    private void setId(int id) {
+    private void setId(long id) {
         // id의 도메인 규칙 검사. setter에 넣음으로써 항상 검사하도록 함.
         verifyId(id);
         this.id = id;
@@ -59,21 +59,21 @@ public class Product {
         this.price = price;
     }
 
-    public String getImg() {
-        return img;
+    public String getImage() {
+        return image;
     }
 
-    private void setImg(String img) {
-        this.img = img;
+    private void setImage(String image) {
+        this.image = image;
     }
 
     // setter의 폭넓은 사용을 지양하기 위해 만든 구체적인 update 메서드들
     // 상품을 수정하는 경우
     public void updateAll(Product product) {
-        int id = product.getId();
+        long id = product.getId();
         String name = product.getName();
         int price = product.getPrice();
-        String img = product.getImg();
+        String img = product.getImage();
 
         updateId(id);
         updateName(name);
@@ -82,7 +82,7 @@ public class Product {
     }
 
     // 상품의 id를 수정. patch이므로 id가 -1이면 무시.
-    private void updateId(int id) {
+    private void updateId(long id) {
         if (id == -1) return;
         setId(id);
     }
@@ -102,11 +102,11 @@ public class Product {
     // 상품의 이미지 주소를 수정
     private void updateImg(String img) {
         if (img == null) return;
-        setImg(img);
+        setImage(img);
     }
 
     // id 값을 검사하는 도메인 규칙
-    private void verifyId(int id) {
+    private void verifyId(long id) {
         // 음수인 id는 불가능하다.
         if (id < 0) {
             throw new ValidationException("id는 음수일 수 없습니다.");
