f0219e650a5a3fe51af6f5b51c0de03ec0832ffe
EunKyung Lee <2dmsrud2002@naver.com>
2024-06-27 14:06:02 +0900

    Feat: 상품 조회 표 생성 및 기능 구현
    이후 수정과 삭제를 고려하여 수정 버튼, 저장 버튼, 삭제 버튼을 생성했다.
    이 또한 상품 생성과 마찬가지로 fetch api를 활용하여 api에 요청하여
    결과를 처리했다.

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -21,3 +21,27 @@ function createProducts() {
         }
     });
 }
+
+function loadProducts() {
+    fetch('/api/products')
+        .then(response => response.json())
+        .then(products => {
+            const productsTableBody = document.getElementById("productsTableBody");
+            productsTableBody.innerHTML = '';
+            products.forEach(product => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                        <td>${product.id}</td>
+                        <td><span>${product.name}</span><input type="text" value="${product.name}" style="display:none"></td>
+                        <td><span>${product.price}</span><input type="text" value="${product.price}" style="display:none"></td>
+                        <td><span>${product.imageUrl}</span><input type="text" value="${product.imageUrl}" style="display:none"></td>
+                        <td>
+                            <button class="btn btn-warning" onclick="editProduct(${product.id}, this)">수정 버튼</button>
+                            <button class="btn btn-primary" onclick="saveProduct(${product.id}, this)" style="display:none">저장 버튼</button>
+                            <button class="btn btn-danger" onclick="deleteProduct(${product.id})">삭제 버튼</button>
+                        </td>
+                    `;
+                productsTableBody.appendChild(row);
+            });
+        });
+}
