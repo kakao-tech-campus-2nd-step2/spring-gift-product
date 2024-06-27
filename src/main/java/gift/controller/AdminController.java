6ca3a81eeaebfd9eb147174ce33065f08c6e9d77
hjinshin <gudwls818@gmail.com>
2024-06-27 18:27:06 +0900

    feat: 관리자페이지 상품 입력 기능 구현

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -5,6 +5,7 @@ import gift.model.ProductDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -24,6 +25,17 @@ public class AdminController {
         List<Product> products = productDao.findAll();
         model.addAttribute("products", products);
         return "index";
+    }
+
+    @GetMapping("admin/product/new")
+    public String newProduct() {
+        return "new";
+    }
+
+    @PostMapping("admin/product/new")
+    public String newProduct(@ModelAttribute ProductRequest request) {
+        productDao.save(request);
+        return "redirect:/";
     }
 
     @PostMapping("/admin/product/delete/{id}")
