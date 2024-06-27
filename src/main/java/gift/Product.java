2fa63181496f13dedfd507c2d7af8e4b1fb5ca16
seoyoung-dev <seoyoung.dev@gmail.com>
2024-06-27 18:11:17 +0900

    feat: Product 클래스 Getter와 Setter 메서드 추가

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -14,4 +14,36 @@ public class Product {
         this.price = price;
         this.imageUrl = imageUrl;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
