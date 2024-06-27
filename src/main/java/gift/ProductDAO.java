1ad93a830cccf29d19b1bdb13d9fc2b4c66e650e
Yongjin An <yongjin5352@gmail.com>
2024-06-27 16:16:40 +0900

    feat: add feature to create new id

--- src/main/java/gift/ProductDAO.java
+++ src/main/java/gift/ProductDAO.java
@@ -5,12 +5,21 @@ import java.util.Map;
 
 public class ProductDAO {
     private Map<Long, ProductRecord> records = new HashMap<Long, ProductRecord>();
-    private long id = 1;
+    private long idTraker = 1;
 
     private boolean isRecordExist(long id) {
         if (records.containsKey(id)) {
             return true;
         }
+
         return false;
+    }
+
+    private long getNewId() {
+        if (isRecordExist(idTraker)) {
+            idTraker++;
+        }
+
+        return idTraker;
     }
 }
