32c1143de9cdb13c646a52b089f79f22da258146
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-06-27 23:15:53 +0900

    feat: 상품 등록 화면 추가

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -7,6 +7,7 @@ import gift.model.ProductResponse;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ProductController {
 
     @Autowired
@@ -23,6 +24,11 @@ public class ProductController {
 
     ProductController(ProductDao productDao) {
         this.productDao = productDao;
+    }
+
+    @GetMapping("/api/product")
+    public String registerProductForm() {
+        return "addProduct";
     }
 
     @PostMapping("/api/product")
