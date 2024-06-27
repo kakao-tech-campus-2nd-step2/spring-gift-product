19b219d78ccbe99da1ea7af7eb9c27cac683b2c2
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-06-28 01:04:12 +0900

    feat: 특정 ID를 가진 상품 조회 기능

---
+++ src/main/java/gift/ProductRepository.java
@@ -0,0 +1,32 @@
+package gift;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepository {
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+    public ProductRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+            .withTableName("product")
+            .usingGeneratedKeyColumns("id");
+    }
+
+    public Optional<Product> findById(int id) {
+        List<Product> products = jdbcTemplate.query("SELECT * FROM products WHERE id = ?",
+            new Object[]{id}, (resultSet, rowNum) ->
+            new Product(
+                resultSet.getLong("id"),
+                resultSet.getString("name"),
+                resultSet.getInt("price"),
+                resultSet.getString("imgUrl")
+            ));
+        return products.isEmpty() ? Optional.empty() : Optional.of(products.get(0));
+    }
+}
