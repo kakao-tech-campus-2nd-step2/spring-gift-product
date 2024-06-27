fc75161aa7810785b2c01880b04ac421378500c5
dkswoals <a4445656@gmail.com>
2024-06-27 20:07:49 +0900

    feat: 상품 추가 페이지 요청 헨들러 구현

--- src/main/java/gift/Controller/ItemController.java
+++ src/main/java/gift/Controller/ItemController.java
@@ -1,19 +1,12 @@
 package gift.Controller;
 
-import Model.Item;
+import gift.Model.ItemDTO;
 import gift.Service.ItemService;
-import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/product")
@@ -26,6 +19,12 @@ public class ItemController {
     public String getItemList(Model model){
         model.addAttribute("list",itemService.getList());
         return "list";
+    }
+
+    @GetMapping("/create")
+    public String getCreateForm(Model model,  ItemDTO itemDTO){
+        model.addAttribute("item",itemDTO);
+        return "create";
     }
 
 }
