903f5740b45b6d13de871b06fc5d9524b9774e7d
seoyoung-dev <seoyoung.dev@gmail.com>
2024-06-28 05:59:32 +0900

    feat: ProductViewController 구현

--- src/main/java/gift/ProductViewController.java
+++ src/main/java/gift/ProductViewController.java
@@ -1,2 +1,28 @@
-package gift;public class ProductViewController {
+package gift;
+
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+//@RequestMapping("")
+public class ProductViewController {
+    private List<Product> products; // 상품 데이터
+
+    @GetMapping("/products")
+    public String showProductList(Model model) {
+        model.addAttribute("products", products);
+        return "products";
+    }
+
+    @GetMapping("/products/new")
+    public String showAddForm() {
+        return "new";
+    }
+
+    @GetMapping("/products/edit/{}")
+    public String editProduct() {
+        return "edit";
+    }
 }
