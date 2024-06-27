825a3f0e7a0f532a19ac394ece8ce8a96204f090
canyos <4581974@naver.com>
2024-06-27 15:36:44 +0900

    jdbc test작성

--- src/test/java/gift/Test.java
+++ src/test/java/gift/Test.java
@@ -1,51 +1,51 @@
 package gift;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.JDBCType;
 import java.util.HashMap;
 import java.util.Map;
 
+@SpringBootTest
 public class Test {
+    @Autowired
+    ProductController productController;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @org.junit.jupiter.api.Test
     public void getTest() {
-        ProductController productController = new ProductController();
-        Map<Long, Product> products = new HashMap<>();
-        Product product = new Product(1,"1",1,"1","1,1");
-        products.put(1L,product);
-        Product product2 = new Product(2,"1",1,"1","1,2");
-        products.put(2L,product2);
-        productController.setProducts(products);
+        //ProductController productController = new ProductController();
+        productController.addProduct( 1,"1",1,"1","1");
+        productController.addProduct( 2,"2",2,"2","2");
         System.out.println(productController.getProducts());
     }
 
     @org.junit.jupiter.api.Test
     public void addTest(){
-        ProductController productController = new ProductController();
+        //ProductController productController = new ProductController();
         productController.addProduct(1,"1",1,"1","1");
         System.out.println(productController.getProducts());
     }
 
     @org.junit.jupiter.api.Test
     public void deleteTest() {
-        ProductController productController = new ProductController();
-        Map<Long, Product> products = new HashMap<>();
-        Product product = new Product(1,"1",1,"1","1");
-        products.put(1L,product);
-        Product product2 = new Product(2,"1",1,"1","1");
-        products.put(2L,product2);
-        productController.setProducts(products);
+        //ProductController productController = new ProductController();
+        productController.addProduct( 1,"1",1,"1","1");
+        productController.addProduct( 2,"2",2,"2","2");
         productController.deleteProduct(1);
         System.out.println(productController.getProducts());
     }
 
     @org.junit.jupiter.api.Test
     public void modifyTest(){
-        ProductController productController = new ProductController();
-        Map<Long, Product> products = new HashMap<>();
-        Product product = new Product(1,"1",1,"1","1");
-        products.put(1L,product);
-        Product product2 = new Product(2,"1",1,"1","1");
-        products.put(2L,product2);
-        productController.setProducts(products);
+        //ProductController productController = new ProductController();
+        productController.addProduct( 1,"1",1,"1","1");
+        productController.addProduct( 2,"2",2,"2","2");
         productController.modifyProduct(1,"modifyname",3,"3","3");
         System.out.println(productController.getProducts());
     }
