afc98118c61abf096babbd86dce39f64edabe8de
BaeJunho <bjh4498@knu.ac.kr>
2024-06-27 13:53:49 +0900

    feat : CRUD API Implementation
    GET, POST, PUT, DELETE HTTP 메서드를 이용한 CRUD 기능 구현

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,21 +1,44 @@
 package gift.controller;
 
 import gift.DTO.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@Controller
 public class ProductController {
     private final Map<Long, Product> products = new HashMap<>();
-
+    // Product 정보 조회를 위한 GET 요청에 대한 응답
+    // 기존에 Json 배열 형식의 반환을 UI를 입힌 View 를 반환하는 것으로 변경
     @GetMapping("/api/products")
-    public Collection<Product> findAll() {
-        Product p1 = new Product(8146027L, "아이스 카페 아메리카노 T", 4500, "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg");
-        products.put(p1.getId(), p1);
-        return products.values();
+    public String findAll(Model model) {
+        model.addAttribute("products", products.values());
+        return "products.html";
+    }
+
+    // PRG ( Post Redirect Get ) 패턴 적용, 상품 추가 이후 상품 조회로 리다이렉트
+    @PostMapping("/api/products")
+    public String addProduct(Product product){
+        products.put(product.getId(), product);
+        return "redirect:/api/products";
+    }
+
+    // 위와 같은 사유로 상품 삭제 이후 상품 조회로 리다이레트
+    @DeleteMapping("/api/products/{productId}")
+    public String deleteProduct(@PathVariable("productId") Long data){
+        products.remove(data);
+        return "redirect:/api/products";
+    }
+
+    // 상품 수정
+    @PutMapping("/api/products/{productId}")
+    public String modifyProduct(@PathVariable("productId") Long data, Product product){
+        products.remove(data);
+        products.put(data, product);
+        return "redirect:/api/products";
     }
 }
