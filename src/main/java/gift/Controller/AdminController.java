638b484f6c08e70f88c65fa640b7d25e4bcd964b
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-06-27 23:42:00 +0900

    상품 추가 메서드
    상품을 추가하는 메서드입니다. PostMapping을 사용합니다. 추가하려고 하는 id가 존재할 경우 에러 메시지가 발생하며 추가되지 않습니다. 상품이 성공적으로 추가되면 상품 목록 조회 페이지로 이동합니다.

--- src/main/java/gift/Controller/AdminController.java
+++ src/main/java/gift/Controller/AdminController.java
@@ -5,6 +5,8 @@ import java.util.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,5 +25,16 @@ public class AdminController {
     public String showAddProductForm(Model model) {
         model.addAttribute("product",new Product(0,"",0,""));
         return "add_product_form";
+    }
+
+    @PostMapping("/add")
+    public String addProduct(@ModelAttribute Product product, Model model) {
+        if(products.containsKey(product.id())){
+            model.addAttribute("error","존재하는 ID 입니다.");
+            model.addAttribute("product",product);
+            return "add_product_form";
+        }
+        products.put(product.id(), product);
+        return "redirect:/admin/products";
     }
 }
