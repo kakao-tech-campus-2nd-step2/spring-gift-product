510c605ed053cb1e4653f9395e86699e780063cc
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-06-27 14:44:53 +0900

    feat : Service 추가

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,2 +1,47 @@
-package gift.service;public class ProductService {
+package gift.service;
+
+
+import gift.domain.Product;
+import gift.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Iterable<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product productDetails) {
+        return productRepository.findById(id)
+            .map(product -> {
+                product.setName(productDetails.getName());
+                product.setPrice(productDetails.getPrice());
+                product.setImageUrl(productDetails.getImageUrl());
+                return productRepository.save(product);
+            })
+            .orElseGet(() -> {
+                productDetails.setId(id);
+                return productRepository.save(productDetails);
+            });
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
 }
