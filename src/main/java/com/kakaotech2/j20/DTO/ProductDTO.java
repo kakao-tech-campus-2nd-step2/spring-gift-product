1614bc9ee1ad6450764c119c7d024f1ed2930d32
mac <20j.code@gmail.com>
2024-06-28 03:19:17 +0900

    feat : ProductService 구현
    DTO와 인터페이스 함께 추가

--- src/main/java/com/kakaotech2/j20/DTO/ProductDTO.java
+++ src/main/java/com/kakaotech2/j20/DTO/ProductDTO.java
@@ -1,19 +1,46 @@
 package com.kakaotech2.j20.DTO;
 
+import com.kakaotech2.j20.Product;
+
 public class ProductDTO {
     //DTO는 생성 후 변화하지 않으므로 final 지정
 
-    private final long id;
+    private final Long id;
+
     private final String name;
 
-    private final int price;
+    private final Integer price;
 
     private final String imageUrl;
 
-    public ProductDTO(long id, String name, int price, String imageUrl) {
+    public ProductDTO(Long id, String name, Integer price, String imageUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
+    }
+
+    //상품 모델을 DTO로 빠르게 전환
+    public ProductDTO(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.price = product.getPrice();
+        this.imageUrl = product.getImageUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
     }
 }
---
+++ src/main/java/com/kakaotech2/j20/service/ProductServiceImpl.java
@@ -0,0 +1,77 @@
+package com.kakaotech2.j20.service;
+
+import com.kakaotech2.j20.DTO.ProductDTO;
+import com.kakaotech2.j20.Product;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductServiceImpl implements ProductService{
+
+    private final Map<Long,Product> db;
+    private int count=0;
+
+    public ProductServiceImpl() {
+        this.db = new HashMap<>();
+    }
+
+    /**
+     * 상품데이터 전체를 얻습니다.
+     * @return 데이터가 없을 경우 null반환
+     */
+    @Override
+    public List<ProductDTO> readAll() {
+
+        List<ProductDTO> requestValue = new ArrayList<>();
+
+        for (Product product : db.values()) {
+            ProductDTO dto = new ProductDTO(product);
+            requestValue.add(dto);
+        }
+        return requestValue;
+    }
+
+    @Override
+    public void create(ProductDTO dto) {
+        db.put((long) count,new Product(count,dto.getName(),dto.getPrice(),dto.getImageUrl()));
+    }
+
+    @Override
+    public void updateName(long id, String name) {
+        if(checkId(id)){
+             db.get(id).setName(name);
+        }
+    }
+
+    @Override
+    public void updatePrice(long id, int price) {
+        if(checkId(id)){
+            db.get(id).setPrice(price);
+        }
+
+    }
+
+    @Override
+    public void updateImageUrl(long id, String url) {
+        if(checkId(id)){
+            db.get(id).setImageUrl(url);
+        }
+    }
+
+    @Override
+    public void delete(long id) {
+        if(checkId(id)){
+            db.remove(id);
+        }
+    }
+
+    private boolean checkId(long id) {
+        if(!db.containsKey(id)){
+            return false;
+        }
+        return true;
+    }
+}
