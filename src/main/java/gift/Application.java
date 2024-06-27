a3e7c80e1aaf331dbcecb6b9c35353baeb54812b
eunsoni <esk12704@gmail.com>
2024-06-28 05:41:30 +0900

    add Product DTO

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,11 +1,13 @@
 package gift;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.sun.net.httpserver.HttpServer;
 
-@SpringBootApplication
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws IOException {
+
     }
 }
+
