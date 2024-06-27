7819e0af752bb1fb0248ae3f00a6be865179f5ba
Kiwoong Kim <gw150311@naver.com>
2024-06-28 07:29:08 +0900

    feat(Application): DB 초기화 - JdbcTemplate 사용 - id가 1이상이 되도록 DB레벨에서 강제

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,12 +1,27 @@
 package gift;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("Hello From Aplication");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS product (" +
+            "id BIGINT PRIMARY KEY check (id >= 1) ," +
+            "name VARCHAR(255) NOT NULL," +
+            "price INT NOT NULL," +
+            "image_url VARCHAR(255))");
     }
 }
