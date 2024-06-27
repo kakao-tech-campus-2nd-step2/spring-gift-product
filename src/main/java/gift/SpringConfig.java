dacfea0bd5d878837da6bfbcdfbc7859e5821ccf
Giheon Do <dgh0001@naver.com>
2024-06-27 21:48:32 +0900

    feat: add SpringConfig.java

--- src/main/java/gift/SpringConfig.java
+++ src/main/java/gift/SpringConfig.java
@@ -1,2 +1,32 @@
-package gift;public class SpringConfig {
+package gift;
+
+import gift.controller.AdminController;
+import gift.repository.JdbcProductRepository;
+import gift.repository.MemoryProductRepository;
+import gift.repository.ProductRepository;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public AdminController adminController() {
+        return new AdminController(productRepository());
+    }
+
+    @Bean
+    public ProductRepository productRepository() {
+//        return new MemoryProductRepository();
+        return new JdbcProductRepository(dataSource);
+    }
 }
