f7d87a6632cbe8b5e58832b6c8652626483ca4fe
5win <woozzoo7@gmail.com>
2024-06-27 14:47:57 +0900

    docs: Add comments in ProductController.java - 엔드포인트들의 주석을 추가함

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -12,6 +12,10 @@ import java.util.Map;
 public class ProductController {
     private final Map<Long, Product> productMap = new HashMap<>();
 
+    /**
+     * 상품 목록을 보여주는 products.html 을 렌더링하여 반환
+     * @return products.html
+     */
     @GetMapping("/api/products")
     public String getProducts(Model model) {
         List<Product> products = productMap.values()
@@ -21,17 +25,31 @@ public class ProductController {
         return "products";
     }
 
+    /**
+     * 상품을 추가하는 페이지인 addForm.html 반환
+     * @return addForm.html
+     */
     @GetMapping("/api/products/add")
     public String addProductForm(Model model) {
         return "addForm";
     }
 
+    /**
+     * 상품 정보를 수정하는 페이지인 editForm.html 반환 <br>
+     * 상품의 기존 정보를 model에 담아서 반환
+     * @param id 수정할 상품의 id
+     * @return editForm.html
+     */
     @GetMapping("/api/products/{id}")
     public String editProductForm(@PathVariable("id") Long id, Model model) {
         model.addAttribute("product", productMap.get(id));
         return "editForm";
     }
 
+    /**
+     * 수정된 상품 정보를 받아서 데이터를 갱신
+     * @return 상품 목록 페이지로 리다이렉션
+     */
     @PostMapping("/api/products/edit")
     public String editProduct(@RequestParam("id") Long id,
                               @RequestParam("name") String name,
@@ -43,6 +61,11 @@ public class ProductController {
         return "redirect:/api/products";
     }
 
+    /**
+     * 추가할 상품의 정보를 받아서 저장 <br>
+     * 이미 존재하는 상품 id이면 저장하지 않음
+     * @return 상품 목록 페이지로 리다이렉션
+     */
     @PostMapping("/api/products/add")
     public String addProduct(@RequestParam("id") Long id,
                              @RequestParam("name") String name,
@@ -54,6 +77,11 @@ public class ProductController {
         return "redirect:/api/products";
     }
 
+    /**
+     * 상품을 삭제
+     * @param id 삭제할 상품의 id
+     * @return 상품 목록 페이지로 리다이렉션
+     */
     @PostMapping("/api/products/{id}")
     public String deleteProduct(@PathVariable(name = "id") Long id) {
         if(productMap.containsKey(id)) {
