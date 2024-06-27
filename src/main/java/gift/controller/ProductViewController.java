38291d4b5cf2d51f417f3a9b16f8a751debe2903
anacnoda77 <sm1108shin@naver.com>
2024-06-27 15:16:27 +0900

    feat: 상품 목록 조회 메서드 구현

--- src/main/java/gift/controller/ProductViewController.java
+++ src/main/java/gift/controller/ProductViewController.java
@@ -1,14 +1,24 @@
 package gift.controller;
 
 import gift.model.Product;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductViewController {
     private final Map<Long, Product> products = new HashMap<>();
     private static Long sequence = 0L;
 
+    @GetMapping("/step2/products")
+    public String getAllProducts(Model model) {
+        List<Product> productsList = products.values().stream().toList();
+        model.addAttribute("products", productsList);
+        return "products";
+    }
 
 }
