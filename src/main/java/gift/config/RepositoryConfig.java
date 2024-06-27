10d8c23159c54a5f01e2e4240a405876ee985bc7
sanghee0820 <lsh277604@gmail.com>
2024-06-28 03:21:52 +0900

    refactor: 사용하지않는 Import문 제거

--- src/main/java/gift/config/RepositoryConfig.java
+++ src/main/java/gift/config/RepositoryConfig.java
@@ -1,14 +1,10 @@
 package gift.config;
 
-import gift.model.dao.JDBCProductDao;
 import gift.model.dao.JDBCTemplateProductDao;
 import gift.model.repository.ProductRepository;
-import gift.model.dao.MapProductDao;
-import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class RepositoryConfig {
