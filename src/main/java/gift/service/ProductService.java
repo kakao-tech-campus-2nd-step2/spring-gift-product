61f15ddd0629e08716e81b85cbc7ea5f8f0158b6
GamJaDo <kim0615157@gmail.com>
2024-06-27 15:16:58 +0900

    fix(ProductService)
    관리자 페이지에서 해당 Product를 수정했는데 id만 변경이 되지 않는 issues발견을 하여 수정하였습니다

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -27,17 +27,20 @@ public class ProductService {
         return product;
     }
     
-    public Product updateProduct(long id, Product product) {
-        if(!products.containsKey(id)) {
+    public Product updateProduct(long oldId, Product updatedProduct) {
+        Product existingProduct = products.get(oldId);
+        if (existingProduct == null) {
             return null;
         }
-        product.setId(id);
-        products.put(id, product);
-        return product;
+
+        products.remove(oldId);
+
+        products.put(updatedProduct.getId(), updatedProduct);
+        return updatedProduct;
     }
     
     public boolean deleteProduct(long id) {
-        if(!products.containsKey(id)) {
+        if (!products.containsKey(id)) {
             return false;
         }
         products.remove(id);
