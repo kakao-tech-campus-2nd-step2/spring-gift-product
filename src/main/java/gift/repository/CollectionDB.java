08ce48732a8d8eab851afe0930d8f3f6af7bd98a
Shsin9797 <aaa8141@naver.com>
2024-06-28 00:52:11 +0900

    feat: CollectionDB 클래스 생성시, products에 특정 Product 값을 하나 넣어주는 생성자 구현

--- src/main/java/gift/repository/CollectionDB.java
+++ src/main/java/gift/repository/CollectionDB.java
@@ -2,12 +2,16 @@ package gift.repository;
 
 import gift.entity.Product;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CollectionDB {
     private Map<Long, Product> products =new HashMap<>(); //여기에 final 붙이는게 맞나요?
 
+    public CollectionDB() {
+        products.put(8146027L,new Product("아이스 카페 아메리카노 T",4500,"https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
+    }
 
     public Map<Long, Product> getProducts() {
         return products;
