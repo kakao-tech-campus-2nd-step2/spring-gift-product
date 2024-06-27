e4896b82759bcfc1f73a48998b7e6bb28e48c60b
Lucerna00 <pjs3859811@naver.com>
2024-06-28 03:39:30 +0900

    feat(AdminController.java): add admin page to view product information

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,5 +23,11 @@ public class AdminController {
     public String getProducts(Model model) {
         model.addAttribute("products", productService.getProducts());
         return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String getProduct(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("product", productService.getProduct(id));
+        return "product";
     }
 }
