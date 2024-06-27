674486b4af1bbcf12b4853871287f31d5c34bfbb
배민수 <duecost@gmail.com>
2024-06-27 18:01:08 +0900

    feat: 상품 전체 목록 조회 기능 추가
    - admin 컨트롤러 최초 추가
    - products.html 추가

---
+++ src/main/java/gift/product/controller/AdminController.java
@@ -0,0 +1,35 @@
+package gift.product.controller;
+
+import gift.product.model.Product;
+import gift.product.repository.ProductRepository;
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/products")
+public class AdminController {
+
+    private final ProductRepository productRepository;
+
+
+    public AdminController() {
+        this.productRepository = new ProductRepository();
+    }
+
+    @GetMapping
+    public String products(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        return "admin/products";
+    }
+
+    @PostConstruct
+    public void init() {
+        productRepository.save(new Product( "아이스 카페 아메리카노 T", 4500, "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
+        productRepository.save(new Product("사과", 3000, "사과링크1"));
+    }
+}
