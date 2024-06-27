4fa282e737326733f32a453634d8d6fd83ebfa1a
dkswoals <a4445656@gmail.com>
2024-06-27 19:48:02 +0900

    test: updateItem, deleteItem Test 코드 작성

--- src/test/java/gift/Service/ItemServiceTest.java
+++ src/test/java/gift/Service/ItemServiceTest.java
@@ -28,4 +28,32 @@ public class ItemServiceTest {
             .extracting("name", "price", "imgUrl")
             .containsExactly(itemDTO.getName(), itemDTO.getPrice(), itemDTO.getImgUrl());
     }
+
+    @Test
+    @DisplayName("상품 수정 기능 검사")
+    void test2(){
+        ItemDTO itemDTO = new ItemDTO("김치", 1200L, "URL");
+        //when
+        itemService.insertItem(itemDTO);
+        itemService.updateItem(new ItemDTO("커피", 2000L, "nnn"),1L);
+        ItemDTO item = itemService.findItem(1L);
+        //then
+        assertThat(item)
+            .isNotNull()
+            .extracting("name","price","imgUrl")
+            .containsExactly("커피",2000L,"nnn");
+    }
+
+    @Test
+    @DisplayName("상품 삭제 테스트")
+    void test3(){
+        ItemDTO itemDTO = new ItemDTO("김치", 1200L, "URL");
+        //when
+        itemService.insertItem(itemDTO);
+        itemService.deleteItem(1L);
+        itemService.findItem(1L);
+        //then
+        assertThat(itemService.findItem(1L))
+            .isNull();
+    }
 }
