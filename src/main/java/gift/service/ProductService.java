562781070e5cdda72b99870b2349c4f34def1214
sunandrabbit <sunandrabbit@gamil.com>
2024-06-27 15:46:40 +0900

    refactor : refactor service class use repository class

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,56 +1,42 @@
 package gift.service;
 
 import gift.model.Product;
+import gift.repository.ProductRepository;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
 
-    private final Map<Long, Product> products = new HashMap<>();
-    private Long idCounter = 1L;
+    @Autowired
+    ProductRepository productRepository;
+
+    //전체 상품 조회 기능
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    //단일 상품 조회 기능
+    public Product getProductById(Long id) {
+        return productRepository.findById(id);
+    }
 
     //상품 추가 기능
-    public Product addProduct(Product product) {
-        product.setId(idCounter);
-        products.put(idCounter++, product);
-        return product;
+    public void addProduct(Product product) {
+        productRepository.save(product);
     }
 
-    //상품 전체 조회 기능
-    public List<Product> selectAllProduct() {
-        return new ArrayList<>(products.values());
-    }
-
-    //상품 단일 조회 기능
-    public Product selectProductById(Long id) {
-        return products.get(id);
+    //상품 수정 기능
+    public void updateProduct(Long id, Product product) {
+        productRepository.update(id, product);
     }
 
     //상품 삭제 기능
     public void deleteProduct(Long id) {
-        Product existingProduct = products.get(id);
-        if (existingProduct != null) {
-            products.remove(id);
-        } else {
-            throw new IllegalArgumentException("No Exists Product By Id");
-        }
-    }
-
-    //상품 수정 기능
-    public Product updateProduct(Long id, Product product) {
-        Product existingProduct = products.get(id);
-        if (existingProduct != null) {
-            existingProduct.setName(product.getName());
-            existingProduct.setPrice(product.getPrice());
-            existingProduct.setImageUrl(product.getImageUrl());
-            products.put(id, existingProduct);
-            return existingProduct;
-        } else {
-            throw new IllegalArgumentException("No Exists Product By Id");
-        }
+        productRepository.delete(id);
     }
 }
