1884bd6b961443db2148fef27428b80285124d10
Happeeee <cmg118@naver.com>
2024-06-27 16:53:19 +0900

    feat: 관리 페이지에서 상품 추가

--- src/main/java/gift/product/ProductWebController.java
+++ src/main/java/gift/product/ProductWebController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -22,5 +24,16 @@ public class ProductWebController {
         List<Product> products = productService.findAllProducts();
         model.addAttribute("products", products);
         return "products";
+    }
+
+    @PostMapping("/products")
+    public String postProduct(@RequestParam String name, @RequestParam int price, @RequestParam String imageUrl) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        product.setImageUrl(imageUrl);
+
+        productService.createProduct(product);
+        return "redirect:/products";
     }
 }
