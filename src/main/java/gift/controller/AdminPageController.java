73724fdcc7588916a7a3f769558264feb2cc545d
신예준 <dpwns1826@gmail.com>
2024-06-27 19:54:07 +0900

    feat: 2단계 - 관리자 화면

---
+++ src/main/java/gift/controller/AdminPageController.java
@@ -0,0 +1,25 @@
+package gift.controller;
+
+import gift.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminPageController {
+    private final ProductService productService;
+
+    public AdminPageController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String products(Model model) {
+        System.out.println(productService.readProduct());
+
+        model.addAttribute("products", productService.readProduct());
+        return "products";
+    }
+}
