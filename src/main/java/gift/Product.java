3f0f449913668b69e51f56c75f2d908f975835b7
hyyyh0x <hyyyh0x@gmail.com>
2024-06-27 19:01:39 +0900

    feat: ProductController written

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,16 +1,3 @@
 package gift;
 
-public record Product(Long id, String name, Integer price, String imageUrl) {
-    public long getId(){
-        return id;
-    }
-    public String getName(){
-        return name;
-    }
-    public Integer getPrice(){
-        return price;
-    }
-    public String getImageUrl(){
-        return imageUrl;
-    }
-}
+public record Product(Long id, String name, Integer price, String imageUrl) {}
--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,7 +1,5 @@
 package gift;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/api/products")
 public class ProductController {
-    private final Map<Long, Product> products = new HashMap<>();
+    private final ProductDao productDao;
+    public ProductController(ProductDao productDao){
+        this.productDao = productDao;
+    }
     private long productIdSequence = 1L;
     //조회
     @GetMapping()
     public String getAllProduct(Model model){
-        model.addAttribute("products",products.values());
+        model.addAttribute("products",this.productDao.findAll());
         return "products";
     }
     //추가
@@ -29,28 +30,28 @@ public class ProductController {
     //수정
     @GetMapping("/update/{id}")
     public String showPutProduct(@PathVariable("id") Long id,Model model) {
-        Product product = products.get(id);
+        Product product = productDao.selectProduct(id);
         model.addAttribute("product",product);
         return "update";
     }
     //삭제
     @GetMapping("/delete/{id}")
     public String deleteProduct(@PathVariable("id") Long id){
-        products.remove(id);
+        productDao.deleteProduct(id);
         return "redirect:/api/products";
     }
     @PostMapping()
     public String postProduct(Product product,Model model){
         long id = productIdSequence++;
-        Product tmpProduct= new Product(id,product.getName(),product.getPrice(),product.getImageUrl());
-        products.put(id,tmpProduct);
+        Product tmpProduct= new Product(id,product.name(),product.price(),product.imageUrl());
+        productDao.insertProduct(tmpProduct);
         model.addAttribute("product",tmpProduct);
         return "redirect:/api/products";
     }
     @PostMapping("/update/{id}")
     public String putProduct(@PathVariable("id") Long id,  @ModelAttribute Product product){
-        Product tmpProduct= new Product(id,product.getName(),product.getPrice(),product.getImageUrl());
-        products.put(id,tmpProduct);
+        Product tmpProduct= new Product(id,product.name(),product.price(),product.imageUrl());
+        productDao.updateProduct(tmpProduct);
         return "redirect:/api/products";
     }
 }
