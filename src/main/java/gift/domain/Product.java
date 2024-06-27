231c5d76eb50d479eef87648c41d687655c53e69
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-06-27 14:30:47 +0900

    데이터베이스에 넣을 Product 엔티티 생성

---
+++ src/main/java/gift/domain/Product.java
@@ -0,0 +1,53 @@
+package gift.domain;
+
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private String imageUrl;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, double price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
