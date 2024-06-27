10ec57e9a45b335ebccd6b928ce49b590a45a023
seoyoung-dev <seoyoung.dev@gmail.com>
2024-06-28 00:08:08 +0900

    feat: 상품 수정 기능

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -25,6 +25,17 @@ public class ProductController {
         return product;
     }
 
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product receivedData) {
+        Product product = products.get(id);
+        if (product != null) {
+            Product changed_product = new Product(id, receivedData.getName(), receivedData.getPrice(), receivedData.getImageUrl());
+            products.put(id, changed_product);
+            return changed_product;
+        }
+        return null; // 수정할 제품이 없는 경우
+    }
+
     @DeleteMapping("/{id}")
     public boolean deleteProduct(@PathVariable Long id) {
         return products.remove(id) != null;
