e3c5e043bad07dc3230a50931451acbf07246c64
noojmogstel <goaudgo@naver.com>
2024-06-27 14:50:36 +0900

    feat(controller): Admin페이지 Controller 구현

---
+++ src/main/java/gift/controller/AdminController.java
@@ -0,0 +1,36 @@
+package gift.controller;
+
+import gift.domain.Product;
+import gift.domain.Products;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    Products products = Products.getInstance();
+
+    @GetMapping
+    public String admin() {
+        return "admin";
+
+    }
+
+    @GetMapping("/product-management")
+    public String productManage() {
+
+        return "product-manage";
+
+    }
+
+    @GetMapping("/product-add")
+    public String productAdd() {
+
+        return "product-add";
+
+    }
+
+}
