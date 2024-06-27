ea2f446b3b5d04ca0504c51ab8a90f9c89546756
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-06-27 14:54:52 +0900

    feat: 관리자페이지 Controller 추가

---
+++ src/main/java/gift/WebProductController.java
@@ -0,0 +1,24 @@
+package gift;
+
+import java.util.Collection;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebProductController {
+
+    private final ProductController productController;
+
+    public WebProductController(ProductController productController) {
+        this.productController = productController;
+    }
+
+    @GetMapping("/products")
+    public String ViewHomepage(Model model) {
+        Collection<Product> products = productController.getAllProducts().getBody();
+        model.addAttribute("productsList", products);
+        return "index";
+    }
+
+}
