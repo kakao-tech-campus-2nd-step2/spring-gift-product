a845bc5af525b33cc1eac5e6428730dcbb1c0832
Happeeee <cmg118@naver.com>
2024-06-27 22:38:58 +0900

    feat: db 전체 상품 조회 기능

--- src/main/java/gift/product/repository/JdbcProductRepository.java
+++ src/main/java/gift/product/repository/JdbcProductRepository.java
@@ -1,8 +1,33 @@
 package gift.product.repository;
 
+import gift.product.Product;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class JdbcProductRepository implements ProductRepository {
+    private final JdbcTemplate jdbcTemplate;
 
+    public JdbcProductRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Product> findAllProducts() {
+        String sql = "select * from product";
+        return jdbcTemplate.query(sql, productRowMapper());
+    }
+
+    private RowMapper<Product> productRowMapper() {
+        return (rs, rowNum) -> {
+            Product product = new Product();
+            product.setId(rs.getLong("id"));
+            product.setName(rs.getString("name"));
+            product.setPrice(rs.getInt("price"));
+            product.setImageUrl(rs.getString("imageUrl"));
+            return product;
+        };
+    }
 }
