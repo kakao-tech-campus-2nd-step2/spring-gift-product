17f25ebc256b99d32b6547afc37854b8f2582e25
Yongjin An <yongjin5352@gmail.com>
2024-06-27 19:43:12 +0900

    refactor : reorder methods for better readability and understanding

--- src/main/java/gift/ProductDAO.java
+++ src/main/java/gift/ProductDAO.java
@@ -10,20 +10,12 @@ public class ProductDAO {
     private Map<Long, ProductRecord> records = new HashMap<Long, ProductRecord>();
     private long idTraker = 1;
 
-    public boolean isRecordExist(long id) {
-        if (records.containsKey(id)) {
-            return true;
-        }
-
-        return false;
+    public ProductRecord[] getAllRecords() {
+        return records.values().toArray(new ProductRecord[records.size()]);
     }
 
-    private long getNewId() {
-        if (isRecordExist(idTraker)) {
-            idTraker++;
-        }
-
-        return idTraker;
+    public ProductRecord addNewRecord(ProductRecord product) {
+        return addNewRecord(product, getNewId());
     }
 
     public ProductRecord addNewRecord(ProductRecord product, long id) throws DuplicateKeyException {
@@ -35,21 +27,6 @@ public class ProductDAO {
         records.put(id, record);
 
         return record;
-    }
-
-    public ProductRecord addNewRecord(ProductRecord product) {
-        return addNewRecord(product, getNewId());
-    }
-
-    public ProductRecord[] getAllRecords() {
-        return records.values().toArray(new ProductRecord[records.size()]);
-    }
-
-    public void deleteRecord(long id) throws NoSuchElementException {
-        if (!isRecordExist(id)) {
-            throw new NoSuchElementException("Record not found");
-        }
-        records.remove(id);
     }
 
     public ProductRecord replaceRecord(long id, ProductRecord product) throws NoSuchElementException {
@@ -72,5 +49,29 @@ public class ProductDAO {
         records.put(id, record);
 
         return record;
+    }
+
+    public void deleteRecord(long id) throws NoSuchElementException {
+        if (!isRecordExist(id)) {
+            throw new NoSuchElementException("Record not found");
+        }
+        records.remove(id);
+    }
+
+
+    public boolean isRecordExist(long id) {
+        if (records.containsKey(id)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private long getNewId() {
+        if (isRecordExist(idTraker)) {
+            idTraker++;
+        }
+
+        return idTraker;
     }
 }
