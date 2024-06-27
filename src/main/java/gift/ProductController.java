9f396a08b008b4889023b999218943b8c0fbd1e7
Lucerna00 <pjs3859811@naver.com>
2024-06-27 15:19:26 +0900

    feat(ProductController.java): add method to add product

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final Map<Long, Product> products = new HashMap<>();
+    private long sequence = 1;
 
     @GetMapping
     public Collection<Product> getProducts() {
@@ -25,6 +29,16 @@ public class ProductController {
         if (product == null) {
             throw new IllegalArgumentException("존재하지 않는 상품입니다.");
         }
+        return product;
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        if (products.containsValue(product)) {
+            throw new IllegalArgumentException("이미 존재하는 상품입니다.");
+        }
+        products.put(sequence, product);
+        sequence++;
         return product;
     }
 }
