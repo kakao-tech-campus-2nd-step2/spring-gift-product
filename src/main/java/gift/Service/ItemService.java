84b14731743eb6b244c23e7e7b20599dc55469cf
dkswoals <a4445656@gmail.com>
2024-06-27 19:41:37 +0900

    feat: insertItem, findItem 서비스 구현

---
+++ src/main/java/gift/Service/ItemService.java
@@ -0,0 +1,29 @@
+package gift.Service;
+
+import Model.Item;
+import Model.ItemDTO;
+import java.util.HashMap;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService {
+    private final HashMap<Long, Item> itemRepository = new HashMap<>();
+    private Long NumId = 1L;
+
+    //Item 저장 메서드
+    public void insertItem(ItemDTO itemDTO){
+        Item item = new Item(NumId,
+            itemDTO.getName(),itemDTO.getPrice(),itemDTO.getImgUrl());
+        itemRepository.put(NumId++,item);
+    }
+
+    //Item 조회 메서드
+    public ItemDTO findItem(Long id){
+        if(itemRepository.containsKey(id)) {
+            Item item = itemRepository.get(id);
+            return new ItemDTO( item.getName(),item.getPrice(), item.getImgUrl());
+        }
+        return null;
+    }
+
+}
