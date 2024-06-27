4e2a874df337211a4dd4dfd78cbfec5a89ba9b4d
KimSongMok <mok0618@naver.com>
2024-06-28 00:13:47 +0900

    fix: database 연동안되는 현상 고침

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,28 +1,9 @@
 package gift.repository;
 
 import gift.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
 @Repository
-public class ProductRepository {
-  private final Map<Long, Product> productMap = new HashMap<>();
-  private long counter = 0;
-
-  public synchronized List<Product> findAll(){
-    return new ArrayList<>((productMap.values()));
-  }
-  public synchronized Optional<Product> findById(Long id){
-    return Optional.ofNullable(productMap.get(id));
-  }
-  public synchronized Product save(Product product){
-    if(product.getId() == null){
-      product.setId(++counter);
-    }
-    productMap.put(product.getId(), product);
-    return product;
-  }
-  public synchronized void deleteById(Long id){
-    productMap.remove(id);
-  }
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
