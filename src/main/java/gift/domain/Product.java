3ab36cfb54717e70ebca1e766c1426210726c8a9
wvssm <neoramiregi@gmail.com>
2024-06-27 16:43:21 +0900

    feat(Product): Product 생성자 추가

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -8,6 +8,13 @@ public class Product {
     private int price;
     private String imageUrl;
 
+    public Product(Long id, String name, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
     public Product(String name, int price, String imageUrl){
         this.name = name;
         this.price = price;
