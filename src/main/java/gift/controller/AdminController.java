95c39b1d889438a05c37a5ff967b62a2e1d7cb7c
Lucerna00 <pjs3859811@naver.com>
2024-06-28 03:12:10 +0900

    feat(AdminController.java): add admin page to view all products

---
+++ src/main/java/gift/controller/AdminController.java
@@ -0,0 +1,26 @@
+package gift.controller;
+
+import gift.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/products")
+public class AdminController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public AdminController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String getProducts(Model model) {
+        model.addAttribute("products", productService.getProducts());
+        return "index";
+    }
+}
