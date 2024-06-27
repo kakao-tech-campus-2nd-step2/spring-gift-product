69b3155faa005d1682c1921da472d6e21b14d234
Hyewon Seok <hws2008@naver.com>
2024-06-28 07:37:28 +0900

    feat: add feature of returning all products

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,21 +1,24 @@
 package gift;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/products")
 public class ProductController {
 
-    private final Map<String, Object> products = new HashMap<>();
+    private final ProductDao productDao;
 
-    @GetMapping("/api/products")
-    public Map<String, Object> produce() {
-        products.put("id", 8146027);
-        products.put("name", "아이스 카페 아메리카노 T");
-        products.put("price", 4500);
-        products.put("imageUrl", "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg");
-        return products;
+    public ProductController(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Product>> view(Model model) {
+        return ResponseEntity.ok().body(productDao.getAllProducts());
     }
 }
