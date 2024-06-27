ba17ca349340c56f516837577e70dcd7e5d2d8a3
Kim Sanghae <pelikan@pusan.ac.kr>
2024-06-27 20:26:26 +0900

    feat(ProductController.java): add exception handling
    - 수정 및 삭제 시 예외 처리 추가

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,88 +1,150 @@
 package gift.controller;
 
 import gift.dto.Product;
-import java.util.Collection;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
+@Controller
 public class ProductController {
 
     private final Map<Long, Product> products = new HashMap<>();
-    private final AtomicLong orderId = new AtomicLong(1);
-
-    @PostMapping("/api/products")
-    public ResponseEntity<?> addProduct(@RequestBody Product product) {
-        var newProduct = new Product(product.id(), product.name(), product.price(),
-            product.imageUrl());
-        if (newProduct.id() == null || newProduct.name().isEmpty() || newProduct.price() < 0
-            || newProduct.imageUrl().isEmpty()) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Invalid Request");
-        }
-        products.put(orderId.getAndIncrement(), newProduct);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newProduct);
-    }
 
     @GetMapping("/api/products")
-    public ResponseEntity<Collection<Product>> getAllProducts() {
-        if (products.isEmpty()) {
-            return ResponseEntity.noContent().build();
+    public String getAllProducts(Model model) {
+        if (!products.isEmpty()) {
+            model.addAttribute("productDto", products.values());
         }
-        return ResponseEntity.ok().body(products.values());
+        return "getproducts";
     }
 
     @GetMapping("/api/products/{id}")
-    public ResponseEntity<?> getProduct(@PathVariable(name = "id") Long id) {
-        var productList = products.entrySet().stream().filter(x -> x.getValue().id().equals(id))
-            .toList();
-        if (productList.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body("There is No Product with id:" + id);
+    public String getProduct(@PathVariable(name = "id") Long id, Model model) {
+        if (products.isEmpty()) {
+            return "Empty Products";
         }
-        return ResponseEntity.ok().body(productList.getFirst().getValue());
+        if (!products.containsKey(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build().toString();
+        }
+        var product = products.get(id);
+        model.addAttribute("productDto", product);
+        return "getproducts";
     }
 
-    @PutMapping("/api/products/{id}")
-    public ResponseEntity<?> updateProductsName(@PathVariable(name = "id") Long id,
-        @RequestBody Product product) {
-        if (product.id() == null || product.name().isEmpty() || product.price() < 0
-            || product.imageUrl().isEmpty()) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Invalid Request");
+    @GetMapping("/api/products/add/form")
+    public String addProductForm(Model model) {
+        model.addAttribute("productDto", new Product());
+        return "addproductform";
+    }
+
+    @PostMapping("/api/products/add")
+    public String addProduct(@ModelAttribute("productDto") Product product,
+        HttpServletResponse response) {
+        var newProduct = new Product(product.getId(), product.getName(), product.getPrice(),
+            product.getImageUrl());
+        if (handleBadAttribute(response, product)) {
+            return null;
         }
-        var productList = products.entrySet().stream().filter(x -> x.getValue().id().equals(id))
-            .map(Entry::getKey).toList();
-        if (productList.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product is not Exist");
+        if (handleAlreadyExist(products.containsKey(product.getId()), response)) {
+            return null;
         }
-        products.remove(id);
-        products.put(id, product);
-        return ResponseEntity.ok().body(products.get(productList.getFirst()));
+        products.put(newProduct.getId(), newProduct);
+        return "redirect:/api/products";
+    }
+
+    @GetMapping("/api/products/update/form")
+    public String updateProductForm(Model model) {
+        model.addAttribute("productDto", new Product());
+        return "updateproductform";
     }
 
 
-    @DeleteMapping("/api/products/{id}")
-    public ResponseEntity<?> deleteProduct(@PathVariable(name = "id") Long productId) {
-        var keyList = products.entrySet().stream().filter(x -> x.getValue().id().equals(productId))
-            .map(Entry::getKey).toList();
-        var deletedProducts = keyList.stream()
-            .map(products::get)
-            .toList();
-        if (keyList.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product is not exist");
+    @PostMapping("/api/products/update")
+    public String updateProductsName(@ModelAttribute(name = "productDto") Product product,HttpServletResponse httpServletResponse
+        ) {
+        if (handleBadAttribute(httpServletResponse, product)) {
+            return null;
         }
-        keyList.forEach(products::remove);
-        return ResponseEntity.ok(deletedProducts);
+        if (handleNonExistProduct(product, httpServletResponse)) {
+            return null;
+        }
+        var updatedProduct = products.get(product.getId());
+        updatedProduct.setName(product.getName());
+        updatedProduct.setPrice(product.getPrice());
+        updatedProduct.setImageUrl(product.getImageUrl());
+        return "redirect:/api/products";
+    }
+
+
+    @GetMapping("/api/products/delete/form")
+    public String deleteProductForm(Model model) {
+        model.addAttribute("productDto", new Product());
+        return "deleteproduct";
+    }
+
+    @GetMapping("/api/products/delete")
+    public String deleteProduct(@ModelAttribute(name = "productDto") Product product,
+        HttpServletResponse httpServletResponse) {
+        if (handleNonExistProduct(product, httpServletResponse)) {
+            return null;
+        }
+        products.remove(product.getId());
+        return "redirect:/api/products";
+    }
+
+    private boolean handleNonExistProduct(Product product, HttpServletResponse httpServletResponse) {
+        if (!products.containsKey(product.getId())) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            System.out.println("HIHI");
+            try {
+                httpServletResponse.sendRedirect("/api/products");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private boolean handleAlreadyExist(boolean containsProductKey, HttpServletResponse response) {
+        if (containsProductKey) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            try {
+                response.sendRedirect("/api/products");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private boolean handleBadAttribute(HttpServletResponse response, Product newProduct) {
+        if (newProduct.getId() == null|| newProduct.getId() < 0 || newProduct.getName().isEmpty()
+            || newProduct.getPrice() < 0
+            || newProduct.getImageUrl().isEmpty()) {
+            // BAD_REQUEST임을 알리는 동시에 다시 리다이렉트 시킬 수 있다.
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            try {
+                response.sendRedirect("/api/products");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return true;
+        }
+        return false;
     }
 }
 
