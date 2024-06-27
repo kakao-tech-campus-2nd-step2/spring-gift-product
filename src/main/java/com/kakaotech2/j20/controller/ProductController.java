095d4480d4d556c962eae8b1b0a7cb9a7026c93d
mac <20j.code@gmail.com>
2024-06-28 03:21:06 +0900

    feat : Product 컨트롤러 추가

--- src/main/java/com/kakaotech2/j20/controller/ProductController.java
+++ src/main/java/com/kakaotech2/j20/controller/ProductController.java
@@ -3,9 +3,11 @@ package com.kakaotech2.j20.controller;
 import com.kakaotech2.j20.DTO.ProductDTO;
 import com.kakaotech2.j20.service.ProductService;
 import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +27,41 @@ public class ProductController {
 
     @GetMapping("/api/products")
     public List<ProductDTO> getList(){
-        return pm.readAll();
+        List<ProductDTO> dto = pm.readAll();
+        return dto;
+    }
+
+
+    @PostMapping("/api/products")
+    public void add(ProductDTO dto){
+        pm.create(dto);
+    }
+
+    @PutMapping("/api/products")
+    public void update(@RequestParam("id") Long id, @RequestBody ProductDTO dto){
+        if (dto.getId()==null){
+            //TODO : http 메소드로 반환
+            throw new IllegalArgumentException("id is null");
+        }
+        changeCheckAndUpdate(id, dto);
+    }
+
+    @DeleteMapping("/api/products")
+    public void delete(@RequestParam("id") Long id){
+        pm.delete(id);
+    }
+
+
+    private void changeCheckAndUpdate(Long id, ProductDTO dto) {
+        if (dto.getName()!=null){
+            pm.updateName(id, dto.getName());
+        }
+        if (dto.getPrice()!=null){
+            pm.updatePrice(id, dto.getPrice());
+        }
+        if (dto.getImageUrl()!=null){
+            pm.updateImageUrl(id, dto.getImageUrl());
+        }
     }
 
 
