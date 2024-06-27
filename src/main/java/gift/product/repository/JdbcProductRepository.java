8dc5734e7040d8f957dfa5100316d226e19947c0
Happeeee <cmg118@naver.com>
2024-06-27 22:51:22 +0900

    feat: db에 상품 추가 기능

--- src/main/java/gift/product/repository/JdbcProductRepository.java
+++ src/main/java/gift/product/repository/JdbcProductRepository.java
@@ -1,9 +1,12 @@
 package gift.product.repository;
 
 import gift.product.Product;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -24,6 +27,21 @@ public class JdbcProductRepository implements ProductRepository {
     public Product findProductById(Long id) {
         String sql = "select * from product where id = ?";
         return jdbcTemplate.queryForObject(sql, productRowMapper(), id);
+    }
+
+    @Override
+    public Product createProduct(Product product) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+            .withTableName("product")
+            .usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", product.getName());
+        parameters.put("price", product.getPrice());
+        parameters.put("imageUrl", product.getImageUrl());
+
+        Long id = jdbcInsert.executeAndReturnKey(parameters).longValue();
+        return findProductById(id);
     }
 
     private RowMapper<Product> productRowMapper() {
