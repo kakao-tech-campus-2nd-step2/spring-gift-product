b7286a7e3c7874487b1208086d78973c03711c04
sanghee0820 <lsh277604@gmail.com>
2024-06-28 03:16:16 +0900

    refactor: JDBCTemplateProductDao를 Bean으로 등록

--- src/main/java/gift/config/RepositoryConfig.java
+++ src/main/java/gift/config/RepositoryConfig.java
@@ -1,17 +1,20 @@
 package gift.config;
 
 import gift.model.dao.JDBCProductDao;
+import gift.model.dao.JDBCTemplateProductDao;
 import gift.model.repository.ProductRepository;
 import gift.model.dao.MapProductDao;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class RepositoryConfig {
 
     @Bean
-    public ProductRepository products(){
-        return new JDBCProductDao();
+    public ProductRepository products(JdbcTemplate jdbcTemplate) {
+        return new JDBCTemplateProductDao(jdbcTemplate);
     }
-
 }
