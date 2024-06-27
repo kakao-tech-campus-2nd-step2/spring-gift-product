9f02543f901f7b98fb085ba1002cf1591e658666
Geon Kim <kky8586696@naver.com>
2024-06-27 17:41:28 +0900

    feat : 기본 데이터 저장을 위해 data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO gift (id,name,price,imageUrl) values (0,'coffee',1500,'abcabc');
+INSERT INTO gift (id,name,price,imageUrl) values (1,'meat',15500,'adcadc');
+INSERT INTO gift (id,name,price,imageUrl) values (2,'egg',3000,'bvvbbv');
+INSERT INTO gift (id,name,price,imageUrl) values (4,'soup',40000,'adasads');
---
+++ src/test/java/gift/model/GiftDaoTest.java
@@ -0,0 +1,10 @@
+package gift.model;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class GiftDaoTest {
+
+}
