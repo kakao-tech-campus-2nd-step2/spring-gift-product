9f06ad19042dc880957c04bf6ac609c73da733a9
sanghee0820 <lsh277604@gmail.com>
2024-06-28 01:56:26 +0900

    feat: update 매서드 추가

--- src/main/java/gift/model/repository/RepositoryInterface.java
+++ src/main/java/gift/model/repository/RepositoryInterface.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public interface RepositoryInterface<T, ID> {
     void save(final T entity);
+    void update(final T entity);
     T find(final ID id);
     void delete(final T entity);
     List<T> findAll();
