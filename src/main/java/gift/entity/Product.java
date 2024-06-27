d3ebfbafbb46a33fdc94fc3167f69e9628cd0825
Shsin9797 <aaa8141@naver.com>
2024-06-28 00:50:45 +0900

    feat: 상품 객체 필드 설정 및 생성자 구현

--- src/main/java/gift/entity/Product.java
+++ src/main/java/gift/entity/Product.java
@@ -1,4 +1,12 @@
 package gift.entity;
 
 public class Product {
+    String name, url;
+    int price;
+
+    public Product(String name, int price, String url) {
+        this.name=name;
+        this.price = price;
+        this.url = url;
+    }
 }
