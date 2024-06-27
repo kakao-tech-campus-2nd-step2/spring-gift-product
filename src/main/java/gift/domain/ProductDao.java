4b6d844fd6ccea51c5f2a8dfe30c54c80181bba3
noojmogstel <goaudgo@naver.com>
2024-06-27 22:08:58 +0900

    feat(domain): Create ProductDaoImpl
    생성자 구현.
    Interface에 구현된 메서드 구현

--- src/main/java/gift/domain/ProductDao.java
+++ src/main/java/gift/domain/ProductDao.java
@@ -3,7 +3,7 @@ package gift.domain;
 import java.util.List;
 
 public interface ProductDao {
-    void Save(Product product);
+    void save(Product product);
     List<Product> findAll();
     Product findById(Long id);
     void update(Product product);
