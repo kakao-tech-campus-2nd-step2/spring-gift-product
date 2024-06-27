15391755e11b7f94edd8d18e48a1815f68fdf2ed
Yongjin An <yongjin5352@gmail.com>
2024-06-27 16:32:59 +0900

    feat: add feature to insert records with specified ID

--- src/main/java/gift/ProductDAO.java
+++ src/main/java/gift/ProductDAO.java
@@ -22,4 +22,16 @@ public class ProductDAO {
 
         return idTraker;
     }
+
+    public ProductRecord addNewRecord(ProductRecord product, long id) {
+        if (isRecordExist(id)) {
+            //이미 해당 id가 존재하면 null반환. @TODO 예외를 던지도록 리팩토링
+            return null;
+        }
+
+        ProductRecord record = new ProductRecord(id, product.name(), product.price(), product.imageUrl());
+        records.put(id, record);
+
+        return record;
+    }
 }
