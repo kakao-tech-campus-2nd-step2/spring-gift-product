807900e1e1c645530de13a6b8f66d16c312f265c
dong_yxxn <g0326kdy@gmail.com>
2024-06-28 02:17:34 +0900

    포기

--- src/main/java/gift/view/ViewController.java
+++ src/main/java/gift/view/ViewController.java
@@ -10,4 +10,8 @@ public class ViewController
     public String showAddProductForm() {
         return "api/add_product";
     }
+    @GetMapping("/api/modify_product")
+    public String showModifyProductForm() {
+        return "api/modify_product";
+    }
 }
