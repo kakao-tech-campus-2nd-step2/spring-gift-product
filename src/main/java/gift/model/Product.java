7a99c6c202a3841a48e111f6743a1fad4302a577
KimSongMok <mok0618@naver.com>
2024-06-27 23:49:21 +0900

    Docs: rewrite readme

--- src/main/java/gift/model/Product.java
+++ src/main/java/gift/model/Product.java
@@ -1,7 +1,18 @@
 package gift.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   private String name;
   private int price;
   private String imageUrl;
