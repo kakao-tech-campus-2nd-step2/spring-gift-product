c8ef933fd5bd3114afdc137e5ef45d84f87f4764
sanghee0820 <lsh277604@gmail.com>
2024-06-28 03:42:40 +0900

    refactor: Product Mapper 추가

--- src/main/java/gift/model/dao/JDBCTemplateProductDao.java
+++ src/main/java/gift/model/dao/JDBCTemplateProductDao.java
@@ -2,6 +2,8 @@ package gift.model.dao;
 
 import gift.model.Product;
 import gift.model.repository.ProductRepository;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -16,24 +18,24 @@ public class JDBCTemplateProductDao implements ProductRepository {
     @Override
     public void save(Product entity) {
         if (entity.isNew()) {
-            jdbcTemplate.update(ProductQuery.INSERT_PRODUCT.getQuery(), entity.getName(), entity.getPrice(), entity.getImgUrl(), entity.isDeleted());
+            jdbcTemplate.update(ProductQuery.INSERT_PRODUCT.getQuery(), entity.getName(), entity.getPrice(),
+                    entity.getImgUrl(), entity.isDeleted());
         } else {
             update(entity);
         }
     }
+
     @Override
     public void update(Product entity) {
-        jdbcTemplate.update(ProductQuery.UPDATE_PRODUCT.getQuery(), entity.getName(), entity.getPrice(), entity.getImgUrl(), entity.isDeleted(), entity.getId());
+        jdbcTemplate.update(ProductQuery.UPDATE_PRODUCT.getQuery(), entity.getName(), entity.getPrice(),
+                entity.getImgUrl(), entity.isDeleted(), entity.getId());
     }
 
     @Override
     public Product find(Long id) {
-        return jdbcTemplate.queryForObject(ProductQuery.SELECT_PRODUCT_BY_ID.getQuery(), new Object[]{id}, ((rs, rowNum) -> new Product(
-                rs.getLong("id"),
-                rs.getString("name"),
-                rs.getInt("price"),
-                rs.getString("image_url"),
-                rs.getBoolean("is_deleted"))));
+        return jdbcTemplate.queryForObject(ProductQuery.SELECT_PRODUCT_BY_ID.getQuery(), new Object[]{id},
+                ((rs, rowNum) -> ProductMapper(
+                        rs)));
     }
 
     @Override
@@ -43,16 +45,20 @@ public class JDBCTemplateProductDao implements ProductRepository {
 
     @Override
     public List<Product> findAll() {
-        return jdbcTemplate.query(ProductQuery.SELECT_ALL_PRODUCTS.getQuery(), (rs, rowNum) -> new Product(
-                rs.getLong("id"),
-                rs.getString("name"),
-                rs.getInt("price"),
-                rs.getString("image_url"),
-                rs.getBoolean("is_deleted")));
+        return jdbcTemplate.query(ProductQuery.SELECT_ALL_PRODUCTS.getQuery(), (rs, rowNum) -> ProductMapper(rs));
     }
 
     private void createTable() {
         jdbcTemplate.execute(ProductQuery.CREATE_PRODUCT_TABLE.getQuery());
         System.out.println("Table created");
+    }
+
+    private Product ProductMapper(ResultSet rs) throws SQLException {
+        return new Product(
+                rs.getLong("id"),
+                rs.getString("name"),
+                rs.getInt("price"),
+                rs.getString("image_url"),
+                rs.getBoolean("is_deleted"));
     }
 }
