678244e6101d8bd0e5e08e571d43f3186e0337fb
Gahyeon Lee <best11gh@naver.com>
2024-06-28 03:15:07 +0900

    feat : 필요없는 코드 정리  & 메인화면 생성

--- src/main/java/gift/entity/Product.java
+++ src/main/java/gift/entity/Product.java
@@ -1,17 +1,19 @@
-package gift;
+package gift.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
   private String name;
   private int price;
   private String imageUrl;
 
-  public Product(Long id, String name, int price, String imageUrl) {
-    this.id = id;
-    this.name = name;
-    this.price = price;
-    this.imageUrl = imageUrl;
-  }
 
   public Long getId() {
     return id;
