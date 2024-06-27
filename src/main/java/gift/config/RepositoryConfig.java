be16876f76eace22efa478bcf140245fc2789e15
sanghee0820 <lsh277604@gmail.com>
2024-06-28 04:10:52 +0900

    chore: 다양한 ProductRepository 추가 후 주석처리

--- src/main/java/gift/config/RepositoryConfig.java
+++ src/main/java/gift/config/RepositoryConfig.java
@@ -1,6 +1,8 @@
 package gift.config;
 
+import gift.model.dao.JDBCProductDao;
 import gift.model.dao.JDBCTemplateProductDao;
+import gift.model.dao.MapProductDao;
 import gift.model.repository.ProductRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,4 +15,12 @@ public class RepositoryConfig {
     public ProductRepository products(JdbcTemplate jdbcTemplate) {
         return new JDBCTemplateProductDao(jdbcTemplate);
     }
+//    @Bean
+//    public ProductRepository products() {
+//        return new JDBCProductDao();
+//    }
+//    @Bean
+//    public ProductRepository products() {
+//        return new MapProductDao();
+//    }
 }
