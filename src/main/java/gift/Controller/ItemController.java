f13cd9dfcf84d812576bbd010acfa669ffe69a2d
dkswoals <a4445656@gmail.com>
2024-06-27 20:53:31 +0900

    feat: 상품 수정 요청 헨들러 구현

--- src/main/java/gift/Controller/ItemController.java
+++ src/main/java/gift/Controller/ItemController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -43,5 +44,9 @@ public class ItemController {
         model.addAttribute("id",id);
         return "update";
     }
-
+    @PutMapping("/update/{id}")
+    public String updateItem(@PathVariable Long id,@ModelAttribute ItemDTO itemDTO){
+        itemService.updateItem(itemDTO,id);
+        return "redirect:/product/list";
+    }
 }
