65451a38631b4bd9367daddd5a278ddc90353d05
BaeJunho <bjh4498@knu.ac.kr>
2024-06-27 19:08:56 +0900

    feat : add DataBase Function
    데이터베이스에 데이터가 저장되도록 사양을 변경

--- src/main/java/gift/controller/FormController.java
+++ src/main/java/gift/controller/FormController.java
@@ -1,24 +1,44 @@
 package gift.controller;
 
+import gift.DTO.Product;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 public class FormController {
-    // Product를 추가하기 위해 정보 입력 HTML Form 열기
-    // 이전 화면 : 상품 목록 -> 현재 -> 다음 화면 : 상품 추가 폼
+    private JdbcTemplate jdbcTemplate;
+    public FormController(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    /*
+     * Product를 추가하기 위해 정보 입력 HTML Form 열기
+     * 이전 화면 : 상품 목록 -> 현재 -> 다음 화면 : 상품 추가 폼
+     */
     @GetMapping("/api/products/addForm")
     public String addForm(){
         return "productAddForm.html";
     }
-
-    // Product를 수정하기 위해 정보 입력 HTML Form 열기
-    // 이전 화면 : 상품 목록 -> 현재 -> 다음 화면 : 상품 수정 폼
+    /*
+     * Product를 수정하기 위해 정보 입력 HTML Form 열기
+     * 이전 화면 : 상품 목록 -> 현재 -> 다음 화면 : 상품 수정 폼
+     */
     @GetMapping("/api/products/modForm/{productId}")
     public String modForm(@PathVariable("productId") Long product_id, Model model){
-        model.addAttribute("id", product_id);
+        List<Product> product = jdbcTemplate.query(
+                "SELECT id, name, price, imageUrl FROM products WHERE id = ?", new Object[]{product_id},
+                (rs, rowNum) -> new Product(
+                        rs.getLong("id"),
+                        rs.getString("name"),
+                        rs.getInt("price"),
+                        rs.getString("imageUrl")
+                )
+        );
+        model.addAttribute("product", product.get(0));
         return "productModForm.html";
     }
 }
