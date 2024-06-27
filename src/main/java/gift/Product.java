f4930ec38706a9d01b448d4cab789de2ff9b3b28
seoyoung-dev <seoyoung.dev@gmail.com>
2024-06-27 16:30:54 +0900

    feat: id 타입 long으로 변경

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -3,12 +3,12 @@ package gift;
 import java.util.UUID;
 
 public class Product {
-    private UUID id;
+    private long id;
     private String name;
     private double price;
     private String imageUrl;
 
-    public Product(UUID id, String name, double price, String imageUrl) {
+    public Product(long id, String name, double price, String imageUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
