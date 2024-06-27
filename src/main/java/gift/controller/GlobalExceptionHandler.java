540c6bf795264e5df9c5ed3d06c0c676608f9575
kangwlgns <kangji0615@gmail.com>
2024-06-27 15:19:26 +0900

    feat(GlobalExceptionHandler): add the WrongParamException handler

--- src/main/java/gift/controller/GlobalExceptionHandler.java
+++ src/main/java/gift/controller/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package gift.controller;
 
 import gift.exception.ErrorResponse;
 import gift.exception.ValidationException;
+import gift.exception.WrongParamException;
 import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -38,5 +39,18 @@ public class GlobalExceptionHandler {
         var errorResponse = new ErrorResponse(message, details);
         // ErrorResponse객체를 JSON으로 바꾼 것을 반환하면서 NOT_FOUND(404)를 반환.
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    // WrongParamException 핸들러 함수
+    @ExceptionHandler(WrongParamException.class)
+    public ResponseEntity<ErrorResponse> handler(WrongParamException wrongParamException) {
+        // 어떤 에러가 발생했는지.
+        String message = "wrong parameter error";
+        // 예외의 message를 통해 자세한 내용을 가져옴.
+        String details = wrongParamException.getMessage();
+        // ErrorResponse 객체를 만듦
+        var errorResponse = new ErrorResponse(message, details);
+        // ErrorResponse객체를 JSON으로 바꾼 것을 반환하면서 BAD_REQUEST(400)를 반환.
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
