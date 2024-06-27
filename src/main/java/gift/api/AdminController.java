8059b225ce7bc0b37df4c81e54fc460317572e2d
HSHwan <tnghks2866@gmail.com>
2024-06-28 02:28:47 +0900

    feat: 관리자 페이지 상품 조회 기능 구현

---
+++ src/main/java/gift/api/AdminController.java
@@ -0,0 +1,46 @@
+package gift.api;
+
+import gift.application.ProductService;
+import gift.dto.ProductRequest;
+import gift.dto.ProductResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public AdminController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    // 상품 조회
+    @GetMapping
+    public String getAllProducts(Model model) {
+        model.addAttribute("productList", productService.getAllProducts());
+        return "product-list";
+    }
+
+    // 상품 추가
+    @PostMapping
+    public ProductResponse addProduct(@RequestBody ProductRequest request) {
+        return productService.createProduct(request);
+    }
+
+    // 상품 삭제
+    @DeleteMapping("/{id}")
+    public Long deleteProduct(@PathVariable("id") Long id) {
+        return productService.deleteProductById(id);
+    }
+
+    // 상품 수정
+    @PatchMapping("/{id}")
+    public Long updateProduct(@PathVariable("id") Long id, @RequestBody ProductRequest request) {
+        return productService.updateProduct(id, request);
+    }
+}
