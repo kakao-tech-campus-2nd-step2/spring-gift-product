20a92aa8bbe24cf823da31827437e4d7776e1a67
dkswoals <a4445656@gmail.com>
2024-06-27 19:57:55 +0900

    test: 상품 목록 페이지 요청 핸들러 구현

--- src/main/java/gift/Controller/ItemController.java
+++ src/main/java/gift/Controller/ItemController.java
@@ -1,7 +1,11 @@
 package gift.Controller;
 
 import Model.Item;
+import gift.Service.ItemService;
 import java.util.HashMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +15,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/product")
 public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("/list")
+    public String getItemList(Model model){
+        model.addAttribute("list",itemService.getList());
+        return "list";
+    }
 
 }
