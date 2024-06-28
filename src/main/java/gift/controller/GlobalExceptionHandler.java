b0e93cad2847d6e57c57f93a06029fbbc1afc40e
SeongHun <tjdgns5506@gmail.com>
2024-06-28 17:27:29 +0900

    chore: 주석으로 설명추가, 코드정리

--- src/main/java/gift/controller/GlobalExceptionHandler.java
+++ src/main/java/gift/controller/GlobalExceptionHandler.java
@@ -4,18 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    //MethodArgumentNotValidException이 Controller 메서드 단계에서 안잡혀서 @ControllerAdvice를 이용해보았습니다.
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleValidationExceptions(MethodArgumentNotValidException ex) {
-
         return "version-SSR/product-error";
     }
 }
