dbd9b8e08c54f37e8dd54172ad4803100c994586
daolove0323 <daolove0323@gmail.com>
2024-06-28 17:23:06 +0900

    refac: ProductService 클래스 리팩토링
    products 멤버 변수를 지우고, 비지니스 로직들은 레포지토리 객체를 통해 처리한다.

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,51 +1,47 @@
 package gift.service;
 
 import gift.domain.Product;
+import gift.repository.ProductRepository;
 import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
-    private final Map<Long, Product> products = new HashMap<>();
-    private long currentId = 1;
+    private final ProductRepository productRepository;
 
-    public ArrayList<Product> findAll() {
-        return new ArrayList<>(products.values());
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAll() {
+        return productRepository.selectAllProducts();
     }
 
     public boolean exists(Long id) {
-        return products.containsKey(id);
+        return productRepository.selectProductById(id) != null;
     }
 
     public Optional<Product> find(Long id) {
-        Product product = products.get(id);
-        return Optional.ofNullable(product);
+        return Optional.ofNullable(productRepository.selectProductById(id));
     }
 
     public Optional<Product> save(Product product) {
-        if (products.containsKey(product.id())) {
-            return Optional.empty();
-        }
-        product = new Product(currentId++, product.name(), product.price(), product.imageUrl());
-        products.put(product.id(), product);
-        return Optional.of(product);
+        Product newProduct = new Product(productRepository.getNextId(), product.name(), product.price(), product.imageUrl());
+        productRepository.insertProduct(newProduct);
+        return Optional.of(newProduct);
     }
 
     public Optional<Product> update(Long id, Product product) {
-        if (!products.containsKey(id)) {
-            return Optional.empty();
-        }
         product = new Product(id, product.name(), product.price(), product.imageUrl());
-        products.put(id, product);
+        productRepository.updateProduct(product);
         return Optional.of(product);
     }
 
     public Optional<Product> delete(Long id) {
-        if (!products.containsKey(id)) {
-            return Optional.empty();
-        }
-        Optional<Product> product = Optional.of(products.get(id));
-        products.remove(id);
+        Optional<Product> product = find(id);
+        productRepository.deleteProductById(id);
         return product;
     }
 }
