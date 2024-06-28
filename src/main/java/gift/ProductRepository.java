8e611133099fb3c99fb133552525d57b6d312a6f
안원모 <tommy45231@naver.com>
2024-06-28 15:23:46 +0900

    Feat : JdbcTemplate 사용한 repository 구현

---
+++ src/main/java/gift/ProductRepository.java
@@ -0,0 +1,30 @@
+package gift;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private static final class ProductRowMapper implements RowMapper<Product> {
+        @Override
+        public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Product product = new Product();
+            product.setId(rs.getLong("id"));
+            product.setName(rs.getString("name"));
+            product.setPrice(rs.getBigDecimal("price"));
+            product.setDescription(rs.getString("description"));
+            return product;
+        }
+    }
