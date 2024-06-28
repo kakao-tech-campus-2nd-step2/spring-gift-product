d3015045311e8e793c8bb90aae906158af288d61
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-06-28 10:31:09 +0900

    feat : JdbcTemplate 추가
    @Autowired를 통해 의존성을 주입합니다. JdbcTemplate 인스턴스를 자동으로 jdbcTemplate 필드에 주입하므로 수동으로 객체를 생성하거나 관리할 필요가 없습니다.

--- src/main/java/gift/Repository/ProductRepository.java
+++ src/main/java/gift/Repository/ProductRepository.java
@@ -1,8 +1,12 @@
 package gift.Repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductRepository {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 }
