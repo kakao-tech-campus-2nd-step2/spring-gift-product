a20e71429603c92e989c42230f6ca0f938172dde
Seunghyun Ko <chris00825@naver.com>
2024-06-28 15:10:32 +0900

    feat(ProductDao.java): init database

---
+++ src/main/java/gift/product/dao/ProductDao.java
@@ -0,0 +1,16 @@
+package gift.product.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ProductDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+}
