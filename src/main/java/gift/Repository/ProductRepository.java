86b4d4ec4ee15bb23e819ce6dd04095a57066d29
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-06-28 10:38:40 +0900

    feat : 모든 상품 조회
    products 테이블의 모든 레코드를 조회합니다. jdbcTemplate.query를 사용하여 sql 쿼리를 실행시키고 결과를 Product 객체로 매핑하여 List<Product>로 반환합니다.

--- src/main/java/gift/Repository/ProductRepository.java
+++ src/main/java/gift/Repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package gift.Repository;
 
+import gift.Model.Product;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,4 +12,16 @@ public class ProductRepository {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    public List<Product> checkAll() {
+        String sql = "SELECT * FROM products";
+        return jdbcTemplate.query(sql, (resultSet, rowNum) ->
+            new Product(
+                resultSet.getLong("id"),
+                resultSet.getString("name"),
+                resultSet.getLong("price"),
+                resultSet.getString("imageUrl")
+            )
+        );
+    }
 }
