6951842dafc6347e38e126c4bbecbce258ec51eb
5win <woozzoo7@gmail.com>
2024-06-28 15:24:55 +0900

    test: Refactoring ProductController.java test code

--- src/test/java/gift/ProductControllerTest.java
+++ src/test/java/gift/ProductControllerTest.java
@@ -3,19 +3,19 @@ package gift;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 class ProductControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    private @Autowired MockMvc mockMvc;
 
     @Test
     @DisplayName("상품 목록 가져오기 테스트")
@@ -25,7 +25,7 @@ class ProductControllerTest {
     }
 
     @Test
-    @DisplayName("상품 추가 폼 페이지 테스트")
+    @DisplayName("상품 추가 폼 페이지 가져오기 테스트")
     void addProductForm() throws Exception {
         mockMvc.perform(get("/api/products/add"))
                 .andExpect(status().isOk())
@@ -36,8 +36,8 @@ class ProductControllerTest {
     @DisplayName("상품 추가 테스트")
     void addProduct() throws Exception {
         mockMvc.perform(post("/api/products/add")
-                        .param("id", "1")
-                        .param("name", "sgoh")
+                        .param("id", "10")
+                        .param("name", "커피")
                         .param("price", "4500")
                         .param("imageUrl", "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"))
                 .andExpect(status().is3xxRedirection())
@@ -48,12 +48,12 @@ class ProductControllerTest {
     @DisplayName("상품 수정 폼 페이지 테스트")
     void editProductForm() throws Exception {
         mockMvc.perform(post("/api/products/add")
-                .param("id", "1")
-                .param("name", "sgoh")
+                .param("id", "10")
+                .param("name", "커피")
                 .param("price", "4500")
                 .param("imageUrl", "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
 
-        mockMvc.perform(get("/api/products/1"))
+        mockMvc.perform(get("/api/products/10"))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
@@ -62,14 +62,14 @@ class ProductControllerTest {
     @DisplayName("상품 수정 테스트")
     void editProduct() throws Exception {
         mockMvc.perform(post("/api/products/add")
-                .param("id", "1")
-                .param("name", "sgoh")
+                .param("id", "10")
+                .param("name", "커피")
                 .param("price", "4500")
                 .param("imageUrl", "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
 
          mockMvc.perform(post("/api/products/edit")
-                         .param("id", "1")
-                         .param("name", "another")
+                         .param("id", "10")
+                         .param("name", "식혜")
                          .param("price", "8900")
                          .param("imageUrl", "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"))
                  .andExpect(status().is3xxRedirection())
@@ -80,12 +80,12 @@ class ProductControllerTest {
     @DisplayName("상품 삭제 테스트")
     void deleteProduct() throws Exception {
         mockMvc.perform(post("/api/products/add")
-                .param("id", "1")
-                .param("name", "sgoh")
+                .param("id", "10")
+                .param("name", "커피")
                 .param("price", "4500")
                 .param("imageUrl", "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
 
-        mockMvc.perform(post("/api/products/1"))
+        mockMvc.perform(post("/api/products/10"))
                 .andExpect(status().is3xxRedirection())
                 .andDo(print());
     }
