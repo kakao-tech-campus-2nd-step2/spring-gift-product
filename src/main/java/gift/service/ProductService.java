19996306a1731b12f371facb5b1123aa014b034b
ariimo <159447466+ariimo@users.noreply.github.com>
2024-06-28 19:27:51 +0900

    feat: ProductService

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,10 +1,14 @@
 package gift.service;
 import gift.Product;
+import gift.controller.PageController;
+import gift.repositories.ProductRepository;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,14 +16,17 @@ public class ProductService {
     private final Map<Long, Product> products = new HashMap<>();
     private long currentId = 1;
 
+    @Autowired
+    ProductRepository productRepository;
+
     // 모든 제품 조회
     public List<Product> getAllProducts() {
-        return new ArrayList<>(products.values());
+        return productRepository.findAll();
     }
 
     // 특정 제품 조회
     public Product getProductById(Long id) {
-        Product product = products.get(id);
+        Product product = productRepository.find(id);
         if (product == null) {
             throw new NoSuchElementException("Product not found with id " + id);
         }
@@ -27,29 +34,20 @@ public class ProductService {
     }
 
     // 제품 추가
-    public Product addProduct(Product product) {
-        product.setId(currentId++);
-        products.put(product.getId(), product);
-        return product;
+    public void addProduct(Product product) {
+        if (product.getId() == null) {
+            product.setId(currentId++);
+        }
+        productRepository.insert(product);
     }
 
     // 제품 수정
-    public Product updateProduct(Long id, Product productDetails) {
-        Product product = products.get(id);
-        if (product == null) {
-            throw new NoSuchElementException("Product not found with id " + id);
-        }
-        product.setName(productDetails.getName());
-        product.setPrice(productDetails.getPrice());
-        products.put(id, product);
-        return product;
+    public void updateProduct(Product product) {
+        productRepository.update(product);
     }
 
     // 제품 삭제
     public void deleteProduct(Long id) {
-        if (!products.containsKey(id)) {
-            throw new NoSuchElementException("Product not found with id " + id);
-        }
-        products.remove(id);
+        productRepository.remove(id);
     }
 }
