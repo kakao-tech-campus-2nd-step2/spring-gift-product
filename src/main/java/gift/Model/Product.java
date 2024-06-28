11fd165667735ab79544c6119e6cfa5536ad0243
Woojae Jeong <woo6388@naver.com>
2024-06-28 17:14:44 +0900

    refactor : 데이터베이스를 활용하도록 바뀌면서 생성자들을 추가하고 idCount값을 삭제.

--- src/main/java/gift/Model/Product.java
+++ src/main/java/gift/Model/Product.java
@@ -1,15 +1,25 @@
 package gift.Model;
 
 public class Product {
-    private static Long idCount = 0L;
-
     private Long id;
     private String name;
     private int price;
     private String imageUrl;
 
+    public Product() {
+        // 기본 생성자
+    }
+
+    // 데이터베이스에서 받아올 때 사용할 생성자
+    public Product(Long id, String name, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    // 새 제품을 생성할 때 사용할 생성자
     public Product(String name, int price, String imageUrl) {
-        this.id = ++idCount;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
@@ -29,6 +39,10 @@ public class Product {
 
     public String getImageUrl() {
         return imageUrl;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setName(String name) {
