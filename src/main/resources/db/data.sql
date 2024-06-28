b3b7e9f200c51cffcc6458e06a24ecdab02b1c77
wvssm <neoramiregi@gmail.com>
2024-06-28 14:27:35 +0900

    fix(data.sql): data 초기화 sql 변경

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,1 +1,2 @@
-INSERT INTO products (name, price, imageUrl) VALUES ('아이스 카페 아메리카노 T', 4500, 'https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg');
+INSERT INTO products (name, price, imageUrl) VALUES ('Product1', 100, 'http://example.com/image1');
+INSERT INTO products (name, price, imageUrl) VALUES ('Product2', 150, 'http://example.com/image2');
