8fefc2a70f70b2abb2d7c44c99fa92c7e824dc0e
Yongjin An <yongjin5352@gmail.com>
2024-06-28 09:32:44 +0900

    feat: create necessary files for server side rendering

---
+++ src/main/java/gift/controller/AdminController.java
@@ -0,0 +1,12 @@
+package gift.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @GetMapping("/")
+    public String admin() {
+        return "admin";
+    }
+}
