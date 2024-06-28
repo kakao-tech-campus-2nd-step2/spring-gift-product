f1dd7f854803a6813c7bac56d40b7570cb6f23a2
bokyeong <wntjdals0412@gmail.com>
2024-06-28 16:03:18 +0900

    Feat: 사용자 Controller 생성

--- src/main/java/gift/controller/WebController.java
+++ src/main/java/gift/controller/WebController.java
@@ -1,9 +1,15 @@
 package gift.controller;
 
 import gift.ProductService;
+import gift.dto.ProductRequestDto;
+import gift.dto.ProductResponseDto;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class WebController {
@@ -11,7 +17,13 @@ public class WebController {
 
     @GetMapping("/lists")
     public String itemList(Model model){
-        model.addAttribute("products",productService.findAll());
+        List<ProductResponseDto> products = productService.findAll();
+        model.addAttribute("products",products);
         return "items";
+    }
+
+    @GetMapping("/add")
+    public String add(){
+        return "addForm";
     }
 }
