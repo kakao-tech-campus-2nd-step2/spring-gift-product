1427d44ec1339e002ab2a8a519ac19cf7d3292b1
Kangboom <jkl6147@naver.com>
2024-06-28 15:15:56 +0900

    refactor: 데이터베이스 적용

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,8 +1,9 @@
-package gift;
+package gift.controller;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import gift.Product;
+import gift.ProductRepository;
+import gift.ProductDto;
+import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,41 +14,41 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-    private final Map<Long, Product> products = new HashMap<>();
+    private final ProductRepository productRepository;
 
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
     // 모든 상품 Read
     @GetMapping("/api/products")
-    public Collection<Product> readAll(){
-        return products.values();
+    public List<Product> readAll(){
+        return productRepository.findAll();
     }
 
     // 특정 상품 Read
     @GetMapping("/api/products/{id}")
     public Product read(@PathVariable("id") Long id){
-        return products.get(id);
+        return productRepository.findById(id);
     }
+
     // 새 상품 Create
     @PostMapping("/api/products")
     public Product create(@RequestBody ProductDto productDto){
-        Product product = new Product(productDto.getName(), productDto.getPrice(), productDto.getImageUrl());
-        products.put(product.getId(), product);
-        return product;
+        productRepository.save(productDto);
+        return productRepository.findByName(productDto.getName());
     }
 
     @PutMapping("/api/products/{id}")
     public Product update(@PathVariable("id") Long id, @RequestBody ProductDto productDto){
-        Product product = products.get(id);
-        product.setName(productDto.getName());
-        product.setPrice(productDto.getPrice());
-        product.setImageUrl(productDto.getImageUrl());
+        productRepository.update(id, productDto);
 
-        products.replace(product.getId(), product);
-        return product;
+        return productRepository.findById(id);
     }
 
     @DeleteMapping("/api/products/{id}")
     public Product delete(@PathVariable("id") Long id){
-        products.remove(id);
+        productRepository.delete(id);
+
         return null;
     }
 }
