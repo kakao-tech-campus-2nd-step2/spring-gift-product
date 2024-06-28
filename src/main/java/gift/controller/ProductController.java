93838f10d0f5413872955cbfeda13b5e58b02f28
sanghee0820 <lsh277604@gmail.com>
2024-06-28 16:24:00 +0900

    refactor: 공통 URI RequestMapping으로 변경

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/products")
 public class ProductController {
     private final ProductRepository productRepository;
 
@@ -25,7 +27,7 @@ public class ProductController {
 
     }
 
-    @GetMapping("/api/products")
+    @GetMapping("")
     public List<ProductResponse> productList(){
 
         return productRepository.findAll().stream()
@@ -33,26 +35,26 @@ public class ProductController {
                 .toList();
     }
 
-    @GetMapping("/api/products/{id}")
+    @GetMapping("/{id}")
     public ProductResponse productDetails(@PathVariable("id") Long id){
         Product findedProduct = productRepository.find(id).orElseThrow(IllegalArgumentException::new);
 
         return ProductResponse.fromModel(findedProduct);
     }
 
-    @PostMapping("/api/products")
+    @PostMapping("")
     public void productSave(@RequestBody ProductRequest newProduct){
         productRepository.save(newProduct.toModel());
     }
 
-    @PatchMapping("/api/products/{id}")
+    @PatchMapping("/{id}")
     public void productModify(@PathVariable("id") Long id, @RequestBody ProductRequest modifyProduct){
         Product findedProduct = productRepository.find(id).orElseThrow(IllegalArgumentException::new);
 
         productRepository.save(modifyProduct.toModel(id));
     }
 
-    @DeleteMapping("/api/products/{id}")
+    @DeleteMapping("/{id}")
     public void productDelete(@PathVariable("id") Long id){
         Product findedProduct = productRepository.find(id).orElseThrow(IllegalArgumentException::new);
 
