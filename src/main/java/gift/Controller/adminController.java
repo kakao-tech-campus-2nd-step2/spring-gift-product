f374e96d89503d5cf6ba525e587e1239f52f2825
suhyeon <nucc1186@gamil.com>
2024-06-28 09:14:23 +0900

    feat: admin 모든 상품 조회

--- src/main/java/gift/Controller/adminController.java
+++ src/main/java/gift/Controller/adminController.java
@@ -1,2 +1,34 @@
-package gift.Controller;public class adminController {
+package gift.controller;
+
+import gift.dao.ProductDao;
+import gift.model.Product;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/list")
+public class AdminController {
+    private ProductDao productDao;
+
+    @Autowired
+    public void setProductDao(ProductDao productDao){
+        this.productDao = productDao;
+    }
+
+    @GetMapping
+    public String getAllProducts(Model model){
+        List<Product> productList = productDao.selectAllProduct();
+        model.addAttribute("productList", productList);
+        return "list";
+    }
 }
