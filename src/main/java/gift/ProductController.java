06e534cce74281422476c714902f54a80533cec8
amm0124 <amm012450@gmail.com>
2024-06-28 17:33:20 +0900

    FIX : Controller collection 제거 및 JDBC 생성자 구현

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -4,6 +4,12 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 
@@ -11,61 +17,10 @@ import java.util.*;
 @RequestMapping("/api")
 public class ProductController {
 
-    private final Map<Long, Product> products = new HashMap<>();
-    private long sequence = 0L;
+    private JdbcTemplate jdbcTemplate;
 
-    ProductController(){
-        products.put(++sequence, new Product(3, "name" , 14000, "url"));
-    }
-
-    @GetMapping("/products")
-    public Collection<Product> getProducts() {
-        return products.values();
-    }
-
-    @GetMapping("/products/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
-        for (Product product : products.values()) {
-            if (product.id() == id) {
-                return ResponseEntity.ok(product);
-            }
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-    }
-
-    @PostMapping("/products")
-    public ResponseEntity<String> addProduct(@RequestBody Product addProduct) {
-        long id = addProduct.id();
-        for (Product product : products.values()) {
-            if (product.id() == id) {
-                return ResponseEntity.status(HttpStatus.CONFLICT).body("중복된 id입니다.");
-            }
-        }
-        products.put(++sequence, addProduct);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Product added successfully.");
-    }
-
-    @DeleteMapping("/product/{id}")
-    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
-        for (Product product : products.values()) {
-            if (product.id() == id) {
-                products.values().remove(product);
-                return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Product deleted successfully.");
-            }
-        }
-        return ResponseEntity.status(HttpStatus.CONFLICT).body("존재하지 않는 id입니다.");
-    }
-
-    @PutMapping("/product/{id}")
-    public ResponseEntity<String> updateProduct(@PathVariable Long id, @RequestBody Product updateProduct) {
-        for (Product product : products.values()) {
-            if (product.id() == id) {
-                products.values().remove(product);
-                products.put(++sequence, updateProduct);
-                return ResponseEntity.status(HttpStatus.CREATED).body("Product updated successfully.");
-            }
-        }
-        return ResponseEntity.status(HttpStatus.CONFLICT).body("존재하지 않는 id입니다.");
+    public ProductController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
 
