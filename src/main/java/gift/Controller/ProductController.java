3e94cb73814373cc7abb4c957f7565d97f6d0076
Woojae Jeong <woo6388@naver.com>
2024-06-28 14:01:49 +0900

    sytle : Controller로 변경
    @RestController 에서 Controller로 변경. 관리자 화면 구현을 위해서

--- src/main/java/gift/Controller/ProductController.java
+++ src/main/java/gift/Controller/ProductController.java
@@ -4,11 +4,12 @@ import gift.Model.Product;
 import gift.Model.RequestProduct;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class ProductController {
     private final Map<Long, Product> products = new HashMap<Long, Product>();
