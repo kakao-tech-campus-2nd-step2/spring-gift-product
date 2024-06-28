192007e672d33847d413ca2479287f84c5af0575
lja3723 <lja3723@gmail.com>
2024-06-28 21:49:13 +0900

    refactor: modify GlobalExceptionHandler behavior
    글로벌 예외 핸들러가 사용자 정의 템플릿 페이지를 반환하도록 변경

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -2,13 +2,14 @@ package gift.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * 서버에서 발생하는 예외를 종합적으로 처리하는 클래스
  */
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ProductNotFoundException.class)
@@ -17,7 +18,8 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ProductAlreadyExistsException.class)
-    public ResponseEntity<Void> handleProductAlreadyExistsException(ProductAlreadyExistsException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+    public String handleProductAlreadyExistsException(ProductAlreadyExistsException e, Model model) {
+        model.addAttribute("errorMessage", "Product already exists with the same name, price, and imageUrl.");
+        return "productDuplicate";
     }
 }
