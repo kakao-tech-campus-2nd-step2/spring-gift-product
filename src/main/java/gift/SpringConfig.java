c0ecf7554f0b2500adb15f795aaf2f24b66c0bec
Lee-dong-won <140253404+cocharm@users.noreply.github.com>
2024-06-28 19:25:27 +0900

    feat: 애플리케이션이 실행될 때 테이블 새로 구축되도록 수정

--- src/main/java/gift/SpringConfig.java
+++ src/main/java/gift/SpringConfig.java
@@ -3,32 +3,41 @@ import gift.controller.AdminController;
 import gift.repository.JdbcProductRepository;
 import gift.repository.MemoryProductRepository;
 import gift.repository.ProductRepository;
+import jakarta.annotation.PostConstruct;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
+
+
+@Configuration
 public class SpringConfig {
 
+    private final DataSource dataSource;
 
-    @Configuration
-    public class SpringConfig {
-
-        private final DataSource dataSource;
-
-        @Autowired
-        public SpringConfig(DataSource dataSource) {
-            this.dataSource = dataSource;
-        }
-
-        @Bean
-        public AdminController adminController() {
-            return new AdminController(productRepository());
-        }
-
-        @Bean
-        public ProductRepository productRepository() {
-            //        return new MemoryProductRepository();
-            return new JdbcProductRepository(dataSource);
-        }
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
+
+    @Bean
+    public AdminController adminController() {
+        return new AdminController(productRepository());
+    }
+
+    @Bean
+    public ProductRepository productRepository() {
+        //        return new MemoryProductRepository();
+        return new JdbcProductRepository(dataSource);
+    }
+    @PostConstruct
+    private void initializeDB() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("sql/ddl.sql"));
+        DatabasePopulatorUtils.execute(populator, dataSource);
+    }
+}
