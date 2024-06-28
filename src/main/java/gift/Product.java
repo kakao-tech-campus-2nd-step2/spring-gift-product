3d41a71373a4c405509477c744647fce8b2c7734
kuma4188 <psb4188@gmail.com>
2024-06-28 19:15:49 +0900

    fix : 스네이크케이스를 카멜케이스로 변경

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,17 +1,21 @@
 package gift;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private int price;
+    @Column(name = "image_url", nullable = false)
     private String imageUrl;
 
     public  Product() {
--- src/main/resources/model/schema.sql
+++ src/main/resources/model/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE product (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price INT NOT NULL,
-    imageUrl VARCHAR(1000)
+    image_url VARCHAR(1000)
 );
 
 
