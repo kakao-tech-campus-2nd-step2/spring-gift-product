4bab8e914bb24fdb73f519c7ee0abe49ebf23e4b
anacnoda77 <sm1108shin@naver.com>
2024-06-28 12:12:28 +0900

    feat: ProductDao 구현

--- src/main/java/gift/repository/ProductDao.java
+++ src/main/java/gift/repository/ProductDao.java
@@ -1,7 +1,12 @@
 package gift.repository;
 
+import gift.model.Product;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,5 +15,36 @@ public class ProductDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    public void insertProduct(Product product) {
+        var sql = "INSERT INTO products(name,price,image_url) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl());
+    }
+
+    public void updateProduct(Product product) {
+        var sql = "UPDATE products SET name = ?, price = ?, image_url = ? WHERE id = ?";
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl(), product.getId());
+    }
+
+    public void deleteProduct(Product product) {
+        var sql = "DELETE FROM products WHERE id = ?";
+        jdbcTemplate.update(sql, product.getId());
+    }
+
+    public List<Product> getAllProducts() {
+        var sql = "SELECT * FROM products";
+        return jdbcTemplate.query(sql, productRowMapper);
+    }
+    
+    public Product getProductById(Long id) {
+        var sql = "SELECT * FROM products WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, productRowMapper);
+    }
+
+    private final RowMapper<Product> productRowMapper = (rs, rowNum) -> new Product(
+        rs.getLong("id"),
+        rs.getString("name"),
+        rs.getInt("price"),
+        rs.getString("image_url")
+    );
 
 }
