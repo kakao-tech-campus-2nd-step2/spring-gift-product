f2d0b3885bd52ae4e8e96646d984cc1dd70fb35a
ez23re <ez23re@gmail.com>
2024-06-28 17:13:49 +0900

    feat : product.java 생성

---
+++ src/main/java/gift/Product.java
@@ -0,0 +1,42 @@
+package gift;
+
+public class Product {
+
+    private Long id;
+    private String category;
+    private String name;
+    private Long price;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+}
