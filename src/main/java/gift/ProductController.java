e99e19a0cb31e48a5d6b7f639b0943d48d8bf6d3
dong_yxxn <g0326kdy@gmail.com>
2024-06-28 15:18:14 +0900

    refactor(ProductController): repository 이용하도록 코드 수정

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,39 +1,41 @@
 package gift;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
-    private final Map<Long, Product> products = new HashMap<>();
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @GetMapping("")
-    public Map<Long, Product> getAllProducts() {
-        return products;
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Product findById(@PathVariable Long id) {
-        return products.get(id);
+        return productRepository.findById(id);
     }
 
     @PostMapping("")
     public void createProduct(@RequestBody Product product) {
-        products.put(product.id(), product);
+        productRepository.save(product);
     }
 
     @PutMapping("/{id}")
     public void updateProduct(@PathVariable Long id, @RequestBody Product product) {
-        if (products.containsKey(id)) {
-            products.put(id, product);
-        }
+        productRepository.update(id, product);
     }
 
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable Long id) {
-        products.remove(id);
+        productRepository.deleteById(id);
     }
 }
