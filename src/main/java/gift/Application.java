2c2e147f6157af3fa7e06dc314ca0861a1fc4a91
suhyeon <nucc1186@gamil.com>
2024-06-28 09:49:54 +0900

    feat(Application.java):initializing table when spring application start

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,11 +1,40 @@
 package gift;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... strings) throws Exception{
+        dropProductTable();
+        createProductTable();
+    }
+    private void dropProductTable(){
+        var sql="DROP TABLE products IF EXISTS";
+        jdbcTemplate.execute(sql);
+    }
+
+    private void createProductTable(){
+        var sql= """
+            create table products(
+                id bigint,
+                name varchar(255),
+                price int,
+                image_url varchar(255)
+            )
+            """;
+        jdbcTemplate.execute(sql);
     }
 }
