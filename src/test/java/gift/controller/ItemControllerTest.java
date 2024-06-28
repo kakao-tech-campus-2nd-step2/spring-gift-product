5c0d35414b657543116d2901da1c3695bc6935f9
dkswoals <a4445656@gmail.com>
2024-06-28 12:11:34 +0900

    test: 수정된 ItemController test 코드 작성

--- src/test/java/gift/controller/ItemControllerTest.java
+++ src/test/java/gift/controller/ItemControllerTest.java
@@ -9,9 +9,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import gift.Model.Item;
 import gift.Model.ItemDTO;
 import gift.service.ItemService;
+import java.beans.Transient;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +23,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 @WebMvcTest(ItemController.class)
 public class ItemControllerTest {
@@ -29,12 +34,13 @@ public class ItemControllerTest {
     private ItemService itemService;
 
     ItemDTO itemDTO = new ItemDTO("커피",2000L,"url");
-    Long testId = 1L;
+    Long testId = 3L;
 
     @Test
     @DisplayName("리스트 요청 핸들러 테스트")
     public void test1() throws Exception{
-        given(itemService.getList()).willReturn(new HashMap<>());
+        List<Item> list = new ArrayList<>();
+        given(itemService.getList()).willReturn(list);
         mockMvc.perform(get("/product/list"))
             .andExpect(status().isOk())
             .andExpect(model().attributeExists("list"))
@@ -44,9 +50,9 @@ public class ItemControllerTest {
     @DisplayName("상품 추가 요청 핸들러 테스트")
     void test2() throws Exception{
         mockMvc.perform(MockMvcRequestBuilders.post("/product/create")
-                .param("price",String.valueOf(itemDTO.getPrice()))
-                .param("name",itemDTO.getName())
-                .param("imgUrl",itemDTO.getImgUrl()))
+                .param("price",String.valueOf(itemDTO.price()))
+                .param("name",itemDTO.name())
+                .param("imgUrl",itemDTO.imgUrl()))
             .andExpect(status().is3xxRedirection())
             .andExpect(redirectedUrl("/product/list"))
             .andDo(print());
@@ -54,11 +60,13 @@ public class ItemControllerTest {
     @Test
     @DisplayName("상품 수정 페이지 요청 헨들러 테스트")
     void test3() throws Exception{
-        given(itemService.findItem(testId)).willReturn(itemDTO);
+        Item item = new Item(testId,"김치",2000L,"url");
+        ItemDTO updated = new ItemDTO("김치",2000L,"url");
+        given(itemService.findItem(testId)).willReturn(item);
 
         mockMvc.perform(get("/product/update/{id}",testId))
             .andExpect(status().isOk())
-            .andExpect(model().attribute("item",itemDTO))
+            .andExpect(model().attribute("item",updated))
             .andExpect(model().attribute("id",testId))
             .andExpect(view().name("update"))
             .andDo(print());
