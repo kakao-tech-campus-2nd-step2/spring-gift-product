bd4066e821311e1193adce2d3c2136980cd03a28
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-06-28 12:16:49 +0900

    feat : data.sql 수정
    sql에서 문자열 데이터를 입력할 때는 작은 따옴표를 사용한다. 큰 따옴표는 식별자를 감싸는 데 사용된다.

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO products (id, name, price, imageUrl) VALUES
-                                                     (1, "예시1", 100, "예시1 Image Url"),
-                                                     (2, "예시2", 200, "예시2 Image Url");
+                                                     (1, '예시1', 100, '예시1 Image Url'),
+                                                     (2, '예시2', 200, '예시2 Image Url');
