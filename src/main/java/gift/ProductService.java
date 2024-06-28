0568c04fc8f4f6d262669964937b065dcefa889f
hyunseo <wndlthsk@naver.com>
2024-06-28 17:17:54 +0900

    refactor : HashMap에서 DAO로 변경

--- src/main/java/gift/ProductService.java
+++ src/main/java/gift/ProductService.java
@@ -1,33 +1,32 @@
 package gift;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.springframework.stereotype.Service;
 @Service
 public class ProductService {
-    private final Map<Long, Product> products = new HashMap<>();
+    private final ProductDao productDao;
 
-    public List<Product> getAllProducts(){
-        return new ArrayList<>(products.values());
+    public ProductService(ProductDao productDao) {
+        this.productDao = productDao;
     }
 
-    public Product getProductById(Long id) {
-        return products.get(id);
+    public List<Product> getAllProducts(){
+        return productDao.findAll();
+    }
+
+    public Product getProductById(long id) {
+        return productDao.findById(id);
     }
 
     public void addProduct(Product newProduct){
-        products.put(newProduct.id(), newProduct);
+        productDao.insertProduct(newProduct);
     }
 
     public void updateProduct(long id, Product updatedProduct){
-        products.put(id, updatedProduct);
+        productDao.updateProduct(id, updatedProduct);
     }
 
     public void deleteProduct(long id){
-        products.remove(id);
+        productDao.deleteById(id);
     }
-
-
 }
