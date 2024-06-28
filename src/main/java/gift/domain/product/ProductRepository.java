a014e29d58071c037ad89a905cd81a14c9e267be
bokyeong <wntjdals0412@gmail.com>
2024-06-28 18:45:07 +0900

    Feat: repository 생성 및 수정

--- src/main/java/gift/domain/product/ProductRepository.java
+++ src/main/java/gift/domain/product/ProductRepository.java
@@ -1,36 +1,10 @@
 package gift.domain.product;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
-public class ProductRepository {
-    private final Map<Long,Product> storage = new HashMap<>();
-
-    private static long sequence = 0L;
-
-    public static final ProductRepository instance = new ProductRepository();
-
-    public static ProductRepository getInstance() {
-        return instance;
-    }
-
-    private ProductRepository(){}
-
-    public void save(Product product){
-        product.setId(++sequence);
-        storage.put(product.getId(),product);
-    }
-
-    public ArrayList<Product> findAll(){
-        return new ArrayList<>(storage.values());
-    }
-
-    public Product findById(Long id){
-        return storage.get(id);
-    }
-
-    public Product deleteById(Long id){
-        return storage.remove(id);
-    }
+public interface ProductRepository {
+    void save(Product product);
+    ArrayList<Product> findAll();
+    Product findById(Long id);
+    void deleteById(Long id);
 }
