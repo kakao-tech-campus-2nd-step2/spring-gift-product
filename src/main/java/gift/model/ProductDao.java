d21c31d6289c9ec5489924637c6f7e6a8c211137
suhyeon <nucc1186@gamil.com>
2024-06-28 15:45:42 +0900

    refactor: query to enum

--- src/main/java/gift/model/ProductDao.java
+++ src/main/java/gift/model/ProductDao.java
@@ -13,9 +13,8 @@ public class ProductDao {
     }
 
     public List<Product> selectAllProduct(){
-        var sql = "select id, name, price, image_url from products";
         return jdbcTemplate.query(
-            sql,
+            ProductQuery.SELECT_ALL_PRODUCT.getQuery(),
             (resultSet, rowNum)->new Product(
                 resultSet.getLong("id"),
                 resultSet.getString("name"),
@@ -26,9 +25,8 @@ public class ProductDao {
     }
 
     public Product selectProductById(Long id){
-        var sql = "select id, name, price, image_url from products where id=?";
         return jdbcTemplate.queryForObject(
-            sql,
+            ProductQuery.SELECT_PRODUCT_BY_ID.getQuery(),
             (resultSet, rowNum) -> new Product(
                 resultSet.getLong("id"),
                 resultSet.getString("name"),
@@ -40,17 +38,14 @@ public class ProductDao {
     }
 
     public void insertProduct(Product product){
-        var sql = "insert into products (id, name, price, image_url) values (?, ?, ?, ?)";
-        jdbcTemplate.update(sql, product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
+        jdbcTemplate.update(ProductQuery.INSERT_PRODUCT.getQuery(), product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
     }
 
     public void updateProductById(Long id, Product product){
-        var sql = "update products set name=?, price=?, image_url=? where id=?";
-        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl(), id);
+        jdbcTemplate.update(ProductQuery.UPDATE_PRODUCT_BY_ID.getQuery(), product.getName(), product.getPrice(), product.getImageUrl(), id);
     }
 
     public void deleteProductById(Long id){
-        var sql = "DELETE FROM products where id=?";
-        jdbcTemplate.update(sql, id);
+        jdbcTemplate.update(ProductQuery.DELETE_PRODUCT_BY_ID.getQuery(), id);
     }
 }
