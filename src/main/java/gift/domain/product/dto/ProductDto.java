ada8fc1b39fa82d03751cbaf94bae0e29a9c8e9c
hyunseo Jeong <wjdgustj2877@naver.com>
2024-06-28 16:02:33 +0900

    refactor: ProductDto 필드 수정

--- src/main/java/gift/domain/product/dto/ProductDto.java
+++ src/main/java/gift/domain/product/dto/ProductDto.java
@@ -2,7 +2,7 @@ package gift.domain.product.dto;
 
 import gift.domain.product.entity.Product;
 
-public record ProductDto(Long id, String name, String description, int price, String imageUrl) {
+public record ProductDto(Long id, String name, int price, String imageUrl) {
 
     public Product toProduct() {
         return new Product(null, name, price, imageUrl);
