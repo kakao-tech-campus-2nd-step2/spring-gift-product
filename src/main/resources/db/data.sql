f4c713672545c6804d264a6fd6d4fb0403d71fa0
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-06-28 10:09:05 +0900

    feat : data.sql 추가
    데이터베이스 테이블에 자동으로 더미 데이터를 생성합니다.

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO products (id, name, price, imageUrl) VALUES
+                                                     (1, "예시1", 100, "예시1 Image Url"),
+                                                     (2, "예시2", 200, "예시2 Image Url");
