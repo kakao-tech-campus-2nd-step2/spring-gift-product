e79ff6bebd56396d7a57edcf2b32df51ec1e9c0e
bokyeong <wntjdals0412@gmail.com>
2024-06-28 17:41:03 +0900

    Chore: H2 환경 구축, 테이블 생성

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,11 +1,22 @@
 package gift;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        jdbcTemplate.execute("DROP TABLE product IF EXISTS");
     }
 }
