fdf5d3d5447d7946de741b968a2af2fc77d704c4
peacefullyquietly <pjh73015@gmail.com>
2024-06-28 16:27:47 +0900

    feat : JDBC 연결 설정 및 구현
    - JdbcTemplate을 이용한 DB 연결 설정

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,38 +1,42 @@
-// ProductDAO.class
 package gift.repository;
 
 import gift.model.Product;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Repository
 public class ProductRepository {
-    private final Map<Long, Product> products = new HashMap<>();
+    private final JdbcTemplate jdbcTemplate;
 
-    public Collection<Product> getAllProducts() {
-        return products.values();
+    public ProductRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Product> getAllProducts() {
+        String sql = "SELECT * FROM products";
+        return jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Product.class));
     }
 
     public Product getProduct(Long id) {
-        return products.get(id);
+        String sql = "SELECT * FROM products WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, BeanPropertyRowMapper.newInstance(Product.class), id);
     }
 
     public void addProduct(Product product) {
-        products.put(product.getId(), product);
+        String sql = "INSERT INTO products (name, price, imageUrl) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl());
     }
 
     public void updateProduct(Long id, Product product) {
-        products.put(id, product);
+        String sql = "UPDATE products SET name = ?, price = ?, imageUrl = ? WHERE id = ?";
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl(), id);
     }
 
-    public boolean containsProduct(Long id) {
-        return products.containsKey(id);
-    }
-
-    public void removeProduct(Long id) {
-        products.remove(id);
+    public String removeProduct(Long id) {
+        String sql = "DELETE FROM products WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+        return "상품 삭제";
     }
 }
