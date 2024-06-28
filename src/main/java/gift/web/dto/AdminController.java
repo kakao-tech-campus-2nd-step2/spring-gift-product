1643f6ec6ca80968aae9e2772567c32200d80d24
yoong-saks <wjdghtjd06@gmail.com>
2024-06-28 13:29:20 +0900

    feat : [step2] getProducts 구현

--- src/main/java/gift/web/dto/AdminController.java
+++ src/main/java/gift/web/dto/AdminController.java
@@ -1,8 +1,21 @@
 package gift.web.dto;
 
+import gift.web.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+    private final ProductService productService;
 
+    public AdminController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String getProducts(Model model) {
+        model.getAttribute("products", productService.getProducts());
+        return "products";
+    }
 }
