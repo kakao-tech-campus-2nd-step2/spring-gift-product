fa3cb26816c9753ac031d55e690c40ace2f763df
donghyun kim <gimdonghyun5434@gmail.com>
2024-06-28 15:31:20 +0900

    feat : data.sql

---
+++ src/main/resources/script/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO product (name, price, image_url) VALUES ('example A', 100, 'urlA');
+INSERT INTO product (name, price, image_url) VALUES ('example B', 200, 'urlB');
+INSERT INTO product (name, price, image_url) VALUES ('example C', 300, 'urlC');
