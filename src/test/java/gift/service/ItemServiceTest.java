526a16e8c9be6fe6ce21a2f4613f0c77dc3d1417
dkswoals <a4445656@gmail.com>
2024-06-28 12:11:51 +0900

    test: 수정된 ItemService test 코드 작성

--- src/test/java/gift/service/ItemServiceTest.java
+++ src/test/java/gift/service/ItemServiceTest.java
@@ -2,11 +2,13 @@ package gift.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import gift.Model.Item;
 import gift.Model.ItemDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 public class ItemServiceTest {
@@ -14,29 +16,30 @@ public class ItemServiceTest {
     @Autowired
     private ItemService itemService;
 
+    private final ItemDTO itemDTO = new ItemDTO("김치", 1200L, "URL");
+    Long testId = 3L;
+
     @Test
     @DisplayName("상품 추가 기능 검사")
     void Test1() {
-        //given
-        ItemDTO itemDTO = new ItemDTO("김치", 1200L, "URL");
         //when
         itemService.insertItem(itemDTO);
-        ItemDTO item = itemService.findItem(1L);
+        Item item = itemService.findItem(testId);
         //then
         assertThat(item)
             .isNotNull()
             .extracting("name", "price", "imgUrl")
-            .containsExactly(itemDTO.getName(), itemDTO.getPrice(), itemDTO.getImgUrl());
+            .containsExactly(itemDTO.name(), itemDTO.price(), itemDTO.imgUrl());
     }
 
     @Test
+    @Transactional
     @DisplayName("상품 수정 기능 검사")
     void test2(){
-        ItemDTO itemDTO = new ItemDTO("김치", 1200L, "URL");
         //when
         itemService.insertItem(itemDTO);
-        itemService.updateItem(new ItemDTO("커피", 2000L, "nnn"),1L);
-        ItemDTO item = itemService.findItem(1L);
+        itemService.updateItem(new ItemDTO("커피", 2000L, "nnn"),testId);
+        Item item = itemService.findItem(testId);
         //then
         assertThat(item)
             .isNotNull()
@@ -45,15 +48,15 @@ public class ItemServiceTest {
     }
 
     @Test
+    @Transactional
     @DisplayName("상품 삭제 테스트")
     void test3(){
-        ItemDTO itemDTO = new ItemDTO("김치", 1200L, "URL");
         //when
         itemService.insertItem(itemDTO);
-        itemService.deleteItem(1L);
-        itemService.findItem(1L);
+        itemService.deleteItem(testId);
+        itemService.findItem(testId);
         //then
-        assertThat(itemService.findItem(1L))
+        assertThat(itemService.findItem(testId))
             .isNull();
     }
 }
