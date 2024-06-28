8d55c5dd0eb941684d241788e599d75265f5f455
us4c0d3 <jwseok312@gmail.com>
2024-06-28 11:09:56 +0900

    feat(Product): thymeleaf에 빈 객체 전달을 위한 id가 nullable하도록 수정
    질문 예정

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -2,8 +2,9 @@ package gift;
 
 public record Product(Long id, String name, Integer price, String imageUrl) {
     public Product {
-        if(id == null) {
-            throw new IllegalArgumentException("[ERROR] ID는 비워둘 수 없습니다.");
-        }
+        // 빈 객체 전달을 위한 검증 임시 주석처리
+//        if(id == null) {
+//            throw new IllegalArgumentException("[ERROR] ID는 비워둘 수 없습니다.");
+//        }
     }
 }
