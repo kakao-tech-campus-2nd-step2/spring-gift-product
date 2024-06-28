273c5d6480d073322f2e1dc8bcceb7fa9ea992c6
us4c0d3 <jwseok312@gmail.com>
2024-06-28 16:50:17 +0900

    refactor(ProductService): 임시 저장 로직 삭제

--- src/main/java/gift/ProductService.java
+++ src/main/java/gift/ProductService.java
@@ -8,21 +8,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
-    private final Map<Long, Product> products = new HashMap<>();
-
     public List<Product> getAllProducts() {
-        // product가 존재하지 않으면 null 반환
-        if(products.isEmpty()) {
-            return null;
-        }
-        return new ArrayList<>(products.values());
+//        select 로직으로 변경
+//        // product가 존재하지 않으면 null 반환
+//        if(products.isEmpty()) {
+//            return null;
+//        }
+//        return new ArrayList<>(products.values());
     }
 
     public Product getProductById(long id) {
-        if(products.containsKey(id)) {
-            return products.get(id);
-        }
-        throw new IllegalArgumentException("[ERROR] ID가 " + id + "인 상품을 찾을 수 없습니다.");
+//        select 로직으로 변경
+//        if(products.containsKey(id)) {
+//            return products.get(id);
+//        }
+//        throw new IllegalArgumentException("[ERROR] ID가 " + id + "인 상품을 찾을 수 없습니다.");
     }
 
     public void addProduct(Product product) {
@@ -30,28 +30,32 @@ public class ProductService {
     }
 
     public void updateProduct(Long id, Product product) {
-        if(!id.equals(product.id())) {
-            throw new IllegalArgumentException(
-                "[ERROR] 요청한 상품의 ID와 상품 정보의 ID가 동일하지 않습니다."
-            );
-        }
-
-        if(!products.containsKey(id)) {
-            throw new IllegalArgumentException(
-                "[ERROR] ID가 " + product.id() + "인 상품을 찾을 수 없어 수정할 수 없습니다."
-            );
-        }
-
-        products.put(product.id(), product);
+//        TODO
+//        update DB 로직으로 수정
+//        if(!id.equals(product.id())) {
+//            throw new IllegalArgumentException(
+//                "[ERROR] 요청한 상품의 ID와 상품 정보의 ID가 동일하지 않습니다."
+//            );
+//        }
+//
+//        if(!products.containsKey(id)) {
+//            throw new IllegalArgumentException(
+//                "[ERROR] ID가 " + product.id() + "인 상품을 찾을 수 없어 수정할 수 없습니다."
+//            );
+//        }
+//
+//        products.put(product.id(), product);
     }
 
     public void deleteProduct(Long id) {
-        if(!products.containsKey(id)) {
-            throw new IllegalArgumentException(
-                "[ERROR] ID가 " + id + "인 상품을 찾을 수 없어 삭제할 수 없습니다."
-            );
-        }
-
-        products.remove(id);
+//        TODO
+//        삭제 DB 로직으로 변경
+//        쿼리를 통해 검증/삭제 한번에 처리 가능
+//        if(!products.containsKey(id)) {
+//            throw new IllegalArgumentException(
+//                "[ERROR] ID가 " + id + "인 상품을 찾을 수 없어 삭제할 수 없습니다."
+//            );
+//        }
+//        products.remove(id);
     }
 }
