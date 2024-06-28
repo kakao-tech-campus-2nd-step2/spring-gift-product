50bb6a21b63d9e4498c05452420520b71bd32f05
안원모 <tommy45231@naver.com>
2024-06-28 15:19:23 +0900

    Docs : 데이터베이스 초기화

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO product (name, price, description) VALUES ('Product1', 10.00, 'Description for Product1');
+INSERT INTO product (name, price, description) VALUES ('Product2', 20.00, 'Description for Product2');
