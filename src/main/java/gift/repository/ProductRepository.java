73ff04fbf88a2e8ab8f74ae531b6bca4cdcd7e86
lja3723 <lja3723@gmail.com>
2024-06-28 16:30:17 +0900

    refactor: modify repository logic
    레포지토리의 작동방식 변경 및 상품 순수 정보를 표현하는 Dto 추가

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,13 +1,20 @@
 package gift.repository;
 
 import gift.model.Product;
+import gift.model.ProductDto;
 import java.util.*;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductRepository {
 
-    private final Map<Long, Product> products = new HashMap<>();
+    private final Map<Long, Product> products;
+    private Long nextId;
+
+    public ProductRepository() {
+        this.products = new HashMap<>();
+        this.nextId = 1L;
+    }
 
     public List<Product> findAll() {
         return new ArrayList<>(products.values());
@@ -17,17 +24,23 @@ public class ProductRepository {
         return Optional.ofNullable(products.get(id));
     }
 
-    public Optional<Product> findByContents(String name, Long price, String imageUrl) {
+    public Optional<Product> findByContents(ProductDto dto) {
         return products.values().stream()
-            .filter(product -> product.name().equals(name) &&
-                product.price().equals(price) &&
-                product.imageUrl().equals(imageUrl))
+            .filter(product -> product.name().equals(dto.name()) &&
+                product.price().equals(dto.price()) &&
+                product.imageUrl().equals(dto.imageUrl()))
             .findFirst();
     }
 
-    public Product save(Product product) {
-        products.put(product.id(), product);
-        return product;
+    public Product update(Long id, ProductDto product) {
+        return products.put(id, new Product(id, product.name(), product.price(), product.imageUrl()));
+    }
+
+    public Product save(ProductDto product) {
+        Product ret = new Product(nextId, product.name(), product.price(), product.imageUrl());
+        products.put(nextId, ret);
+        nextId++;
+        return ret;
     }
 
     public void deleteById(Long id) {
--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -3,6 +3,7 @@ package gift.service;
 import gift.exception.ProductAlreadyExistsException;
 import gift.exception.ProductNotFoundException;
 import gift.model.Product;
+import gift.model.ProductDto;
 import gift.repository.ProductRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,21 +23,20 @@ public class ProductService {
         return productRepository.findAll();
     }
 
-    public Product addProduct(Product product) {
+    public Product addProduct(ProductDto product) {
         //이미 존재하는 상품 등록 시도시 예외 발생
-        productRepository.findByContents(product.name(), product.price(), product.imageUrl())
-            .orElseThrow(ProductAlreadyExistsException::new);
+        productRepository.findByContents(product).orElseThrow(ProductAlreadyExistsException::new);
 
         //상품 등록
         return productRepository.save(product);
     }
 
-    public Product updateProduct(Long id, Product product) {
+    public Product updateProduct(Long id, ProductDto product) {
         //존재하지 않는 상품 업데이트 시도시 예외 발생
         productRepository.findById(id).orElseThrow(ProductNotFoundException::new);
 
         //상품 업데이트
-        return productRepository.save(new Product(id, product.name(), product.price(), product.imageUrl()));
+        return productRepository.update(id, product);
     }
 
     public void deleteProduct(Long id) {
