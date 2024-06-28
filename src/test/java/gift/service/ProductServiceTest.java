ee0c6b84878d607a6e86284adea6a1d7abbeac38
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-06-28 14:50:43 +0900

    fix: 테스트 코드 중복 최소화

--- src/test/java/gift/service/ProductServiceTest.java
+++ src/test/java/gift/service/ProductServiceTest.java
@@ -2,6 +2,7 @@ package gift.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import gift.DTO.ProductDTO;
 import gift.domain.Product;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,7 @@ class ProductServiceTest {
     @Autowired
     private ProductService productService;
     private List<Product.CreateProduct> list = new ArrayList<>();
+    private ProductDTO productDTO;
 
     @BeforeEach
     public void setUp() {
@@ -32,38 +34,38 @@ class ProductServiceTest {
     @Test
     @DisplayName("상품 생성")
     public void CreateProduct() {
-        //    가장 마지막 상품 가져오기
-        productService.getProduct((long) productService.getProductList().size() - 1);
         //  how
         Product.CreateProduct temp = new Product.CreateProduct("test4", 100, "test4");
         //  when
         productService.createProduct(temp);
+
+        //    가장 마지막 상품 가져오기
+        productDTO = productService.getProduct((long) productService.getProductList().size() - 1);
+
         //  then
         //    추가한 상품의 이름과 마지막에 위치한 상품의 이름 비교
-        assertEquals(temp.getName(),
-            productService.getProduct((long) productService.getProductList().size() - 1).getName());
+        assertEquals(temp.getName(), productDTO.getName());
     }
 
     @Test
     @DisplayName("상품 수정")
     public void UpdateProduct() {
-        //    가장 마지막 상품 가져오기
-        productService.getProduct((long) productService.getProductList().size() - 1);
         //  how
         Product.UpdateProduct temp = new Product.UpdateProduct("test4-1", 100, "test4-1");
         //  when
         productService.updateProduct(temp, (long) productService.getProductList().size() - 1);
+
+        //    가장 마지막 상품 가져오기
+        productDTO = productService.getProduct((long) productService.getProductList().size() - 1);
+
         //  then
         //    수정한 상품의 이름과 마지막에 위치한 상품의 이름 비교
-        assertEquals(temp.getName(),
-            productService.getProduct((long) productService.getProductList().size() - 1).getName());
+        assertEquals(temp.getName(), productDTO.getName());
     }
 
     @Test
     @DisplayName("상품 삭제")
     public void removeProduct() {
-        //    가장 마지막 상품 가져오기
-        productService.getProduct((long) productService.getProductList().size() - 1);
         //  how
         int before = productService.getProductList().size();
         Long target = (long) before - 1;
