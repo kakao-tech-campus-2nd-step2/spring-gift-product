adc89e4228fe77890e9ffb1eaa1d7213fd481486
안원모 <tommy45231@naver.com>
2024-06-28 15:40:14 +0900

    Feat : 서비스 및 컨트롤러 생성

---
+++ src/main/java/gift/ProductService.java
@@ -0,0 +1,16 @@
+package gift;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+   
+}
