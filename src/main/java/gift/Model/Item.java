e22a4dea41bf6e14ded88ef51c08a7c5fe834a17
dkswoals <a4445656@gmail.com>
2024-06-28 11:47:34 +0900

    feat: ItemDTO 및 Item 객체 수정
    ItemDTO의 역할에 맞게 불변성을 가지도록 수정
    Item객체는 값 자체를 표현하도록 수정(vo)

--- src/main/java/gift/Model/Item.java
+++ src/main/java/gift/Model/Item.java
@@ -1,10 +1,30 @@
 package gift.Model;
 
+import java.util.Objects;
+
 public class Item {
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
     Long id;
     String name;
     Long price;
     String imgUrl;
+    public Item(){}
 
     public Item(Long id, String name, Long price, String imgUrl) {
         this.id = id;
@@ -24,5 +44,24 @@ public class Item {
 
     public String getImgUrl() {
         return imgUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return Objects.equals(id, item.id) && Objects.equals(name, item.name)
+            && Objects.equals(price, item.price) && Objects.equals(imgUrl,
+            item.imgUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, imgUrl);
     }
 }
--- src/main/java/gift/Model/ItemDTO.java
+++ src/main/java/gift/Model/ItemDTO.java
@@ -3,67 +3,21 @@ package gift.Model;
 import java.util.Objects;
 
 public class ItemDTO {
-
     private String name;
     private Long price;
     private String imgUrl;
-
-    public  ItemDTO(){}
-
     public ItemDTO(String name, Long price, String imgUrl) {
         this.name = name;
         this.price = price;
         this.imgUrl = imgUrl;
     }
-
-    @Override
-    public String toString() {
-        return "ItemDTO{" +
-            "name='" + name + '\'' +
-            ", price=" + price +
-            ", imgUrl='" + imgUrl + '\'' +
-            '}';
-    }
-
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Long getPrice() {
         return price;
     }
-
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-
     public String getImgUrl() {
         return imgUrl;
-    }
-
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        ItemDTO itemDTO = (ItemDTO) o;
-        return Objects.equals(name, itemDTO.name) && Objects.equals(price,
-            itemDTO.price) && Objects.equals(imgUrl, itemDTO.imgUrl);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, price, imgUrl);
     }
 }
