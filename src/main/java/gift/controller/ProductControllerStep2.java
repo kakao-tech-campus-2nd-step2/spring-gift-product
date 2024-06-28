fcb9fd4c82ef17975454f0fe0378d5f41b44c2af
pinkbeenyeti <rbgusdn12@gmail.com>
2024-06-28 14:19:29 +0900

    feat: 웹 페이지의 요청을 받아들이는 controller 생성 step1 컨트롤러와 경로를 다르게 지정하고 보내는 요청을 받아들여 상품의 웹페이지 표시, 추가, 수정, 삭제를 할 수 있도록 하였다.

--- src/main/java/gift/controller/ProductControllerStep2.java
+++ src/main/java/gift/controller/ProductControllerStep2.java
@@ -1,2 +1,62 @@
-package gift.controller;public class ProductControllerStep2 {
+package gift.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import gift.entity.Product;
+import gift.dto.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Objects;
+
+@Controller
+@RequestMapping("/")
+public class ProductControllerStep2 {
+    private final Map<Long, Product> products = new HashMap<>();
+
+    @GetMapping("pinkbeen/products")
+    public String getAllProducts(Model model) {
+        model.addAttribute("products", products);
+        return "index";
+    }
+
+    @PostMapping("pinkbeen/products")
+    public String addProduct(@ModelAttribute ProductDTO productDTO) {
+        Product newProduct = new Product(productDTO.id(), productDTO.name(), productDTO.price(), productDTO.imageUrl());
+        products.put(newProduct.getId(), newProduct);
+
+        return "redirect:/pinkbeen/products";
+    }
+
+    @PostMapping("pinkbeen/products/{id}")
+    public ResponseEntity<ProductDTO> modifyProduct(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
+        Product updatedProduct = products.get(id);
+
+        if (!Objects.equals(id, productDTO.id())) {
+            products.remove(id);
+            updatedProduct.setId(productDTO.id());
+            products.put(productDTO.id(), updatedProduct);
+        }
+
+        updatedProduct.setName(productDTO.name());
+        updatedProduct.setPrice(productDTO.price());
+        updatedProduct.setImageUrl(productDTO.imageUrl());
+
+        ProductDTO updatedProductDTO = new ProductDTO(
+                updatedProduct.getId(),
+                updatedProduct.getName(),
+                updatedProduct.getPrice(),
+                updatedProduct.getImageUrl()
+        );
+
+        return ResponseEntity.ok(updatedProductDTO);
+    }
+
+    @DeleteMapping("pinkbeen/products/{id}")
+    public String deleteProduct(@PathVariable("id") Long id) {
+        products.remove(id);
+        return "redirect:/pinkbeen/products";
+    }
 }
