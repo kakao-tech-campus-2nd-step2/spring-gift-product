02010f06a54eb53cc7c966fa515409d1b4778330
hyunseo Jeong <wjdgustj2877@naver.com>
2024-06-28 15:41:05 +0900

    test: DB 상품 추가 기능 테스트

---
+++ src/test/java/gift/domain/product/dao/ProductDaoTest.java
@@ -0,0 +1,33 @@
+package gift.domain.product.dao;
+
+import static org.assertj.core.api.Assertions.*;
+
+import gift.domain.product.entity.Product;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDaoTest {
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Test
+    @DisplayName("DB 상품 추가")
+    void insert() {
+        // given
+        Product product = new Product(null, "탕종 블루베리 베이글", 3500, "https://image.istarbucks.co.kr/upload/store/skuimg/2023/09/[9300000004823]_20230911131337469.jpg");
+
+        // when
+        Product savedProduct = productDao.insert(product);
+
+        // then
+        assertThat(savedProduct).isNotNull();
+        assertThat(savedProduct.getId()).isNotNull();
+        assertThat(savedProduct.getName()).isEqualTo(product.getName());
+        assertThat(savedProduct.getPrice()).isEqualTo(product.getPrice());
+        assertThat(savedProduct.getImageUrl()).isEqualTo(product.getImageUrl());
+    }
+}
