ea9b36b860b138ef410bf96917fd5238ee38a410
us4c0d3 <jwseok312@gmail.com>
2024-06-28 15:59:23 +0900

    refactor(ViewController): 정보 수정 후 메인 페이지로 리디렉션되도록 수정

--- src/main/java/gift/ViewController.java
+++ src/main/java/gift/ViewController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
@@ -38,10 +39,16 @@ public class ViewController {
         return "redirect:/products";
     }
 
-    @RequestMapping(value = "edit/{id}")
-    public String editProduct(Model model, @PathVariable Long id) {
+    @GetMapping("edit/{id}")
+    public String editProductPage(Model model, @PathVariable Long id) {
         Product product = productService.getProductById(id);
         model.addAttribute("product", product);
         return "product_edit_form";
+    }
+
+    @PutMapping("edit/{id}")
+    public String editProduct(@ModelAttribute Product product, @PathVariable Long id) {
+        productService.updateProduct(id, product);
+        return "redirect:/products";
     }
 }
