34e3349ee0b5eb0edd058e9a189e41df9d6df40b
pinkbeenyeti <rbgusdn12@gmail.com>
2024-06-28 14:54:49 +0900

    feat: DB를 사용하도록 변경한 컨트롤러 DB를 사용하는 경로로 요청을 받고 DB에 요청에 따른 쿼리를 통해 작업을 하는 컨트롤러를 생성하였다.

--- src/main/java/gift/controller/ProductControllerStep3.java
+++ src/main/java/gift/controller/ProductControllerStep3.java
@@ -1,2 +1,71 @@
-package gift.controller;public class ProductControllerStep3 {
+package gift.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import gift.entity.Product;
+import gift.dto.*;
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/")
+public class ProductControllerStep3 {
+    private JdbcTemplate jdbcTemplate;
+
+    public ProductControllerStep3(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @GetMapping("yeti/products")
+    public String getAllProducts(Model model) {
+        String sql = "select id, name, price, imageurl from products";
+        List<Product> products = jdbcTemplate.query(
+                sql, (resultSet, rowNum) -> {
+                    Product product = new Product(
+                            resultSet.getLong("id"),
+                            resultSet.getString("name"),
+                            resultSet.getInt("price"),
+                            resultSet.getString("imageurl")
+                    );
+                    return product;
+                });
+        model.addAttribute("products", products);
+        return "index";
+    }
+
+    @PostMapping("yeti/products")
+    public String addProduct(@ModelAttribute ProductDTO productDTO) {
+        String sql = "INSERT INTO products(id, name, price, imageurl) VALUES (?,?,?,?)";
+        jdbcTemplate.update(sql, productDTO.id(), productDTO.name(), productDTO.price(), productDTO.imageUrl());
+
+        return "redirect:/yeti/products";
+    }
+
+    @PostMapping("/yeti/products/{id}")
+    public ResponseEntity<ProductDTO> modifyProduct(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
+        if (!id.equals(productDTO.id())) {
+            String deleteSql = "DELETE FROM products WHERE id = ?";
+            jdbcTemplate.update(deleteSql, id);
+
+            String insertSql = "INSERT INTO products (id, name, price, imageurl) VALUES (?, ?, ?, ?)";
+            jdbcTemplate.update(insertSql, productDTO.id(), productDTO.name(), productDTO.price(), productDTO.imageUrl());
+        }
+
+        String updateSql = "UPDATE products SET id = ?, name = ?, price = ?, imageurl = ? WHERE id = ?";
+        jdbcTemplate.update(updateSql, productDTO.id(), productDTO.name(), productDTO.price(), productDTO.imageUrl(), id);
+
+        return ResponseEntity.ok(productDTO);
+    }
+
+    @DeleteMapping("yeti/products/{id}")
+    public String DeleteProduct(@PathVariable("id") Long id) {
+        String sql = "DELETE FROM products WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+
+        return "redirect:/yeti/products";
+    }
 }
