4170412c64ab6f554ecc5f2cf7308d792f92a9bd
Hyewon Seok <hws2008@naver.com>
2024-06-28 15:17:10 +0900

    fix: transfer products to thymeleaf view

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,7 +1,7 @@
 package gift;
 
-import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/api/products")
 public class ProductController {
 
@@ -23,8 +22,9 @@ public class ProductController {
     }
 
     @GetMapping()
-    public ResponseEntity<List<Product>> view(Model model) {
-        return ResponseEntity.ok().body(productDao.getAllProducts());
+    public String view(Model model) {
+        model.addAttribute("products", productDao.getAllProducts());
+        return "administrator";
     }
 
     @PostMapping("/new")
