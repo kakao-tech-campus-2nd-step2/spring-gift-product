248f66c5eed05b3f6a2a8d51204352f07cfe3419
Lee-dong-won <140253404+cocharm@users.noreply.github.com>
2024-06-28 17:38:50 +0900

    add: add ProductForm for post and put

--- src/main/java/gift/model/ProductForm.java
+++ src/main/java/gift/model/ProductForm.java
@@ -1,4 +1,36 @@
 package gift.model;
 public class ProductForm {
 
+    
+    private String name;
+    private int price;
+    private String imageUrl;
+
+    public ProductForm(String name, int price, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductForm{" +
+            "name='" + name + '\'' +
+            ", price=" + price +
+            ", imageUrl='" + imageUrl + '\'' +
+            '}';
+    }
+
 }
