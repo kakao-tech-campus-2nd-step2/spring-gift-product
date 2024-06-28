97237edf4ba73167f657b98436550942631722ce
wvssm <neoramiregi@gmail.com>
2024-06-28 15:47:11 +0900

    feat(ProductService): save, deleteById, updateById 트랜잭션 추가

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -7,11 +7,13 @@ import gift.exception.ProductNotFoundException;
 import gift.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 
 @Service
+@Transactional(readOnly = true)
 public class ProductService {
     private final ProductRepository productRepository;
 
@@ -20,6 +22,7 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    @Transactional
     public ProductResponseDto save(ProductRequestDto productDto){
         Long id = productRepository.save(productDto.toEntity());
         return new ProductResponseDto(id,productDto.getName(),productDto.getPrice(),productDto.getImageUrl());
@@ -39,12 +42,14 @@ public class ProductService {
                 .toList();
     }
 
+    @Transactional
     public Long deleteById(Long id){
         productRepository.findById(id)
                 .orElseThrow(()-> new ProductNotFoundException("ID가 " + id + "인 상품이 존재하지 않습니다."));
         return productRepository.delete(id);
     }
 
+    @Transactional
     public ProductResponseDto updateById(Long id, ProductRequestDto productDto){
         Product product = productRepository.findById(id)
                 .orElseThrow(()-> new ProductNotFoundException("ID가 " + id + "인 상품이 존재하지 않습니다."));
