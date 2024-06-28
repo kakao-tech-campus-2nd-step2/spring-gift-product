542f3ec15bcc9990d7edd6a389558e396aa34b53
안원모 <tommy45231@naver.com>
2024-06-28 12:20:35 +0900

    Feat : product 상품 초기값 설정

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -15,6 +15,13 @@ public class ProductController {
     private final Map<Long, Product> products = new HashMap<>();
     private long nextId = 1; // 상품 ID 생성을 위한 변수
 
+    //상품 초기값 설정
+    @PostConstruct
+    private void init() {
+        // 초기 상품 데이터 로드
+        addProduct(new Product(nextId, "아이스 카페 아메리카노 T", 4500, "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
+    }
+
     // 상품 추가
     @PostMapping
     public ResponseEntity<Product> addProduct(@RequestBody Product product) {
