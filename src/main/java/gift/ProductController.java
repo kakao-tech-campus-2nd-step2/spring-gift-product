0ba4c7f356fcd4f64c331b03712de1e0cf0db0d1
doyooon <ian661087@gmail.com>
2024-06-28 17:44:14 +0900

    feat ProductController : repository를 통해서 DB에 요청을 보내도록 api 수정

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,62 +1,44 @@
 package gift;
 
-import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @Controller
 public class ProductController {
-    private Map<Long, Product> products = new HashMap<>();
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @GetMapping("/")
     public String listProducts(Model model) {
-        Collection<Product> productCollection = products.values();
-        model.addAttribute("products", productCollection);
+        model.addAttribute("products", productRepository.findAll());
         model.addAttribute("newProduct", new Product()); // 새 상품 객체
         model.addAttribute("product", new Product()); // 편집을 위한 빈 객체*/
         return "home"; // Thymeleaf 템플릿 이름
     }
 
-    /*@GetMapping("/")
-    @ResponseBody
-    public Product getProduct(@RequestParam(value = "id") Long id) {
-        return products.get(id);
-    }
-*/
     @PostMapping("/post")
     public String createProduct(@ModelAttribute Product newProduct){
-        var product = new Product(newProduct.id, newProduct.name, newProduct.price, newProduct.imageUrl);
-        products.put(product.id, product);
-        System.out.println(products.get(product.id).name);
+        productRepository.save(newProduct);
         return "redirect:/";
     }
 
     @PostMapping ("/update")
     public String updateProduct(@ModelAttribute Product changeProduct){
-        Product changedProduct = products.get(changeProduct.id);
-        changedProduct.setProduct(changeProduct);
+        productRepository.update(changeProduct);
 
         return "redirect:/";
     }
 
     @GetMapping ("/delete/{id}")
     public String deleteProduct(@PathVariable Long id){
-        products.remove(id);
+        productRepository.deleteById(id);
 
         return "redirect:/";
     }
