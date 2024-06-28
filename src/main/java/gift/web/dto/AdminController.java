dc50ce15dc0f7d5e2c546c6472d4f7d1ddc49afb
yoong-saks <wjdghtjd06@gmail.com>
2024-06-28 13:32:39 +0900

    feat : [step2] getProductById 구현

--- src/main/java/gift/web/dto/AdminController.java
+++ src/main/java/gift/web/dto/AdminController.java
@@ -4,8 +4,11 @@ import gift.web.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin/products")
 public class AdminController {
     private final ProductService productService;
 
@@ -16,6 +19,15 @@ public class AdminController {
     @GetMapping
     public String getProducts(Model model) {
         model.getAttribute("products", productService.getProducts());
+        return "products";
+    }
+
+    @GetMapping("/{id}")
+    public String getProductById(@PathVariable Long id, Model model) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            model.addAttribute("product", product);
+        }
         return "products";
     }
 }
