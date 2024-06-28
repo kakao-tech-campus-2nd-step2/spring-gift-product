902203eb946a02a26ba3909e72de2bc8f5a7c114
Seunghyun Ko <chris00825@naver.com>
2024-06-28 15:58:31 +0900

    feat(ProductVo.java): add constructors

--- src/main/java/gift/product/model/ProductVo.java
+++ src/main/java/gift/product/model/ProductVo.java
@@ -7,6 +7,15 @@ public class ProductVo {
     private int price;
     private String imageUrl;
 
+    public ProductVo() {}
+
+    public ProductVo(long id, String name, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
     public Long getId() {
         return id;
     }
