e9790603988266b6a3f5cb9205b6759b951b736e
Lucerna00 <pjs3859811@naver.com>
2024-06-28 16:47:48 +0900

    feat(H2Repository.java): replace with h2 database

---
+++ src/main/java/gift/repository/H2Repository.java
@@ -0,0 +1,66 @@
+package gift.repository;
+
+import gift.dto.ProductDTO;
+import jakarta.annotation.PostConstruct;
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class H2Repository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public H2Repository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @PostConstruct
+    public void createProductTable() {
+        var sql = """
+            CREATE TABLE PRODUCT (
+                id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(255),
+                price INT,
+                imageUrl VARCHAR(255)
+            )
+            """;
+        jdbcTemplate.execute(sql);
+    }
+
+    public Collection<ProductDTO> getProducts() {
+        var sql = "SELECT * FROM PRODUCT";
+        return jdbcTemplate.query(sql, productRowMapper());
+    }
+
+    public ProductDTO getProduct(Long id) {
+        var sql = "SELECT * FROM PRODUCT WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, productRowMapper(), id);
+    }
+
+    public void addProduct(ProductDTO productDTO) {
+        var sql = "INSERT INTO PRODUCT (name, price, imageUrl) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, productDTO.name(), productDTO.price(), productDTO.imageUrl());
+    }
+
+    public void updateProduct(long id, ProductDTO productDTO) {
+        var sql = "UPDATE PRODUCT SET name = ?, price = ?, imageUrl = ? WHERE id = ?";
+        jdbcTemplate.update(sql, productDTO.name(), productDTO.price(), productDTO.imageUrl(), id);
+    }
+
+    public void deleteProduct(long id) {
+        var sql = "DELETE FROM PRODUCT WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+
+    private RowMapper<ProductDTO> productRowMapper() {
+        return (resultSet, rowNum) -> new ProductDTO(
+            resultSet.getLong("id"),
+            resultSet.getString("name"),
+            resultSet.getInt("price"),
+            resultSet.getString("imageUrl"));
+    }
+}
--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,6 +1,7 @@
 package gift.service;
 
 import gift.dto.ProductDTO;
+import gift.repository.H2Repository;
 import gift.repository.MemoryRepository;
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,42 +10,49 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductService {
 
-    private final MemoryRepository memoryRepository;
+//    private final MemoryRepository repository;
+//
+//    @Autowired
+//    public ProductService(MemoryRepository memoryRepository) {
+//        this.repository = memoryRepository;
+//    }
+
+    private final H2Repository repository;
 
     @Autowired
-    public ProductService(MemoryRepository memoryRepository) {
-        this.memoryRepository = memoryRepository;
+    public ProductService(H2Repository h2Repository) {
+        this.repository = h2Repository;
     }
 
     public Collection<ProductDTO> getProducts() {
-        return memoryRepository.getProducts();
+        return repository.getProducts();
     }
 
     public ProductDTO getProduct(Long id) {
-        return memoryRepository.getProduct(id);
+        return repository.getProduct(id);
     }
 
     public boolean addProduct(ProductDTO productDTO) {
-        if (memoryRepository.getProducts().contains(productDTO)) {
+        if (repository.getProducts().contains(productDTO)) {
             return false;
         }
-        memoryRepository.addProduct(productDTO);
+        repository.addProduct(productDTO);
         return true;
     }
 
     public boolean updateProduct(long id, ProductDTO productDTO) {
-        if (memoryRepository.getProduct(id) == null) {
+        if (repository.getProduct(id) == null) {
             return false;
         }
-        memoryRepository.updateProduct(id, productDTO);
+        repository.updateProduct(id, productDTO);
         return true;
     }
 
     public boolean deleteProduct(long id) {
-        if (memoryRepository.getProduct(id) == null) {
+        if (repository.getProduct(id) == null) {
             return false;
         }
-        memoryRepository.deleteProduct(id);
+        repository.deleteProduct(id);
         return true;
     }
 }
