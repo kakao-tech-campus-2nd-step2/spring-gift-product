31884fda32795759e43f616d7051655040adb5d1
HSHwan <tnghks2866@gmail.com>
2024-06-28 15:27:11 +0900

    feat: 상품 DAO 데이터 추가 및 조회 기능 구현

---
+++ src/main/java/gift/dao/ProductDao.java
@@ -0,0 +1,70 @@
+package gift.dao;
+
+import gift.domain.Product;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductDao implements ProductRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Product save(Product product) {
+        String sql = "INSERT INTO product (name, price, image_url) VALUES(?, ?, ?)";
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl());
+        return product;
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        String sql = "SELECT * FROM product WHERE id = ?";
+        return Optional.ofNullable(
+                jdbcTemplate.queryForObject(
+                        sql,
+                        (rs, rowNum) -> new Product(
+                                rs.getLong("id"),
+                                rs.getString("name"),
+                                rs.getInt("price"),
+                                rs.getString("image_url")
+                        ),
+                        id
+                )
+        );
+    }
+
+    @Override
+    public List<Product> findAll() {
+        String sql = "SELECT * FROM product";
+        return jdbcTemplate.query(sql,
+                (rs, rowNum) -> new Product(
+                        rs.getLong("id"),
+                        rs.getString("name"),
+                        rs.getInt("price"),
+                        rs.getString("image_url")
+                )
+        );
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    public int update() {
+        return 1;
+    }
+}
