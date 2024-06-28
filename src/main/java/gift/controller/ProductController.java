062a1da0d2ae5690c5c84c49435b16d881ee774d
lja3723 <lja3723@gmail.com>
2024-06-28 17:05:24 +0900

    feat: add Product Update API

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -5,7 +5,12 @@ import gift.model.ProductDto;
 import gift.service.ProductService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +26,19 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<Product> getProducts() {
-        return productService.getAllProducts();
+    public ResponseEntity<List<Product>> getProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addProduct(@RequestBody ProductDto dto) {
+        productService.addProduct(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Void> updateProduct(@PathVariable("id") Long id, @RequestBody ProductDto dto) {
+        productService.updateProduct(id, dto);
+        return ResponseEntity.ok().body(null);
     }
 }
