845da7a93e3bdd4ba6dca06dcb338ebdce393c66
dong_yxxn <g0326kdy@gmail.com>
2024-06-28 15:20:28 +0900

    feat(DatabaseInitializer): table 생성을 위해 클래스 선성ㄴ

--- src/main/java/gift/DatabaseInitializer.java
+++ src/main/java/gift/DatabaseInitializer.java
@@ -1,2 +1,19 @@
-package gift;public class DatabaseInitializer {
+package gift;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseInitializer implements CommandLineRunner {
+
+    private final ProductRepository productRepository;
+
+    public DatabaseInitializer(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        productRepository.createTable();
+    }
 }
