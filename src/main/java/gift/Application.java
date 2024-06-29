5674c88f01f421dd80b4e46d9329058bafb5f16e
Kiwoong Kim <gw150311@naver.com>
2024-06-29 21:44:04 +0900

    refactor: Application에서 테이블 생성 쿼리 분리 - schema.sql 생성

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,27 +1,12 @@
 package gift;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-    @Autowired
-    JdbcTemplate jdbcTemplate;
+public class Application  {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS product (" +
-            "id BIGINT PRIMARY KEY check (id >= 1) ," +
-            "name VARCHAR(255) NOT NULL," +
-            "price INT NOT NULL," +
-            "image_url VARCHAR(255))");
     }
 }
