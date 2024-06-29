592d4163f2cea50e5fca29fc92b5471aaf6bfa97
sanghee0820 <lsh277604@gmail.com>
2024-06-29 10:58:54 +0900

    refactor: PatchMapping -> PutMapping으로 변경

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -11,9 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,7 +54,7 @@ public class ProductController {
                 .body("Product created");
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> productModify(@PathVariable("id") Long id,
                                                 @RequestBody ProductRequest modifyProduct) {
         Product findedProduct = productRepository.find(id)
