28abe7fd782aa96e262348db12e96478f5377e2f
Hyewon Seok <hws2008@naver.com>
2024-06-30 03:07:46 +0900

    fix: modify delete feature to use thymeleaf view and remove checkbox

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,9 +1,7 @@
 package gift;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,9 +37,9 @@ public class ProductController {
         return view(model);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> delete(@PathVariable("id") long id) {
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable("id") long id, Model model) {
         productDao.delete(id);
-        return ResponseEntity.ok().build();
+        return view(model);
     }
 }
