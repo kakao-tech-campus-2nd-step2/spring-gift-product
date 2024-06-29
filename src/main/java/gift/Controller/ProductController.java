547599b521e376376f4028944fd7c5d40454f17e
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-06-29 22:39:46 +0900

    feat : 메모리에서 DB 사용하게 변경

--- src/main/java/gift/Controller/ProductController.java
+++ src/main/java/gift/Controller/ProductController.java
@@ -1,43 +1,43 @@
 package gift.Controller;
 
 import gift.Model.Product;
-import java.util.ArrayList;
-import java.util.HashMap;
+
+import gift.Repository.ProductRepository;
 import java.util.List;
-import java.util.Map;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
 
-    private final Map<Long, Product> products = new HashMap<>();
+    private final ProductRepository productRepository;
 
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    
     @PostMapping
     public Product addProduct(@RequestBody Product newProduct) {
-        products.put(newProduct.id(), newProduct);
-        return newProduct;
+        return productRepository.saveProduct(newProduct);
     }
 
     @GetMapping("/{id}")
     public Product getProduct(@PathVariable(value = "id") long id) {
-        return products.get(id);
+        return productRepository.checkProductsById(id);
     }
 
     @GetMapping("/all")
     public List<Product> getAllProduct() {
-        return new ArrayList<>(products.values());
+        return productRepository.checkProductsAll();
     }
 
     @DeleteMapping("/{id}")
     public Product deleteProduct(@PathVariable(value = "id") long id) {
-        return products.remove(id);
+        productRepository.deleteProduct(id);
     }
 
     @PutMapping("/{id}")
     public Product updateProduct(@PathVariable("id") long id, @RequestBody Product updatedProduct) {
-        products.remove(id);
-        products.put(updatedProduct.id(), updatedProduct);
-        return updatedProduct;
+        return productRepository.checkProductsById(id);
     }
 }
