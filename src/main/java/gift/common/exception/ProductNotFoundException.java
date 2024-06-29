0c126a6e861bb442664fad0f2cd492bb5eacf4ed
sanghee0820 <lsh277604@gmail.com>
2024-06-29 11:06:20 +0900

    refactor: Exception의 HttpStatus와 Message를 static final 로 변경

--- src/main/java/gift/common/exception/ProductNotFoundException.java
+++ src/main/java/gift/common/exception/ProductNotFoundException.java
@@ -3,8 +3,12 @@ package gift.common.exception;
 import org.springframework.http.HttpStatus;
 
 public class ProductNotFoundException extends ProductException {
+
+    private static final HttpStatus HTTP_STATUS = HttpStatus.NO_CONTENT;
+    private static final String MESSAGE = "Can't find Product with id %s";
+
     private ProductNotFoundException(final Long id) {
-        super(String.format("Can't find Product with id %s", id.toString()), HttpStatus.NO_CONTENT);
+        super(String.format(MESSAGE, id.toString()), HTTP_STATUS);
     }
 
     public static ProductNotFoundException of(final Long id) {
