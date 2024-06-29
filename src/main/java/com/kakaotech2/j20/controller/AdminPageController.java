aff0b6343eba2f94a597d4d40bf257627fd8fd60
mac <20j.code@gmail.com>
2024-06-30 01:36:26 +0900

    feat : 관리자페이지 상품 추가 구현

--- src/main/java/com/kakaotech2/j20/controller/AdminPageController.java
+++ src/main/java/com/kakaotech2/j20/controller/AdminPageController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,6 +22,14 @@ public class AdminPageController {
     @GetMapping("/admin")
     public String adminPage(Model model) {
         model.addAttribute("products",pm.readAll());
+        System.out.println(pm.readAll().stream().count());
+        model.addAttribute("productDTO",new ProductDTO());
         return "admin/index";//렌더링하는 html 이름
+    }
+
+    @PostMapping("/admin") //admin으로 오는 post에 대해서 submit
+    public String adminPageSubmit(@ModelAttribute("productDTO") ProductDTO productDTO) {
+        pm.create(productDTO); //서비스에 접근해서 해당 부분을 추가해주도록 한다.
+        return "redirect:/admin";
     }
 }
