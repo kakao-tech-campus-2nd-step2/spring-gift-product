5f2d006b5e4d7f6c6d9c3024bfd645c1631500e8
suhyeon <nucc1186@gamil.com>
2024-06-30 14:29:06 +0900

    feat(ProductResponseDto):Product Entity를 ProductResponsedto로 쪼갬

--- src/main/java/gift/controller/DTO/ProductResponseDto.java
+++ src/main/java/gift/controller/DTO/ProductResponseDto.java
@@ -1,2 +1,24 @@
-package gift.controller.DTO;public class ProductResponseDto {
+package gift.controller.DTO;
+
+public class ProductResponseDto {
+    private Long id;
+    private String name;
+    private int price;
+    private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
 }
