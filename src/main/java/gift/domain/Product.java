32562c04558081b0e413d951ea7c29125a121320
김민성 <166998868+castlekimdev@users.noreply.github.com>
2024-06-30 22:52:44 +0900

    refactor: 도메인 객체 `record` 대신 `class`로 변경

--- src/main/java/gift/domain/Product.java
+++ src/main/java/gift/domain/Product.java
@@ -1,10 +1,80 @@
 package gift.domain;
 
-public record Product(
-    Long id,
-    String name,
-    Integer price,
-    String imageUrl
-) {
+import java.util.Objects;
 
+public class Product {
+
+    private Long id;
+    private String name;
+    private Integer price;
+    private String imageUrl;
+
+    public Product(Long id, String name, Integer price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", price=" + price +
+            ", imageUrl='" + imageUrl + '\'' +
+            '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) &&
+            Objects.equals(name, product.name) &&
+            Objects.equals(price, product.price) &&
+            Objects.equals(imageUrl, product.imageUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, imageUrl);
+    }
 }
