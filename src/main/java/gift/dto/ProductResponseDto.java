82a1d2b9d21e70bae82be432e381c0ba06651c03
wvssm <neoramiregi@gmail.com>
2024-07-01 14:44:51 +0900

    refactor(ProductResponseDto): 사용하지 않는 메서드 삭제

--- src/main/java/gift/dto/ProductResponseDto.java
+++ src/main/java/gift/dto/ProductResponseDto.java
@@ -41,16 +41,4 @@ public class ProductResponseDto {
     public void setId(Long id) {
         this.id = id;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
 }
