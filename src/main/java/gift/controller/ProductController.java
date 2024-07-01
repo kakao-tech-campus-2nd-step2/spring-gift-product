83c8325299e5dd7fa8aa3d735d586c1d9268c6a1
김민성 <166998868+castlekimdev@users.noreply.github.com>
2024-07-01 12:14:34 +0900

    refactor: controller가 domain service에 의존하도록 리팩토링

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -3,10 +3,8 @@ package gift.controller;
 import gift.controller.dto.ProductCreateRequestDto;
 import gift.controller.dto.ProductResponseDto;
 import gift.controller.dto.ProductUpdateRequestDto;
-import gift.domain.Product;
-import gift.repository.ProductRepository;
+import gift.service.ProductService;
 import java.util.List;
-import java.util.NoSuchElementException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,79 +18,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductController {
 
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
-    public ProductController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
     @GetMapping
     public List<ProductResponseDto> getAllProducts() {
-        return productRepository
-            .findAll()
-            .stream()
-            .map(ProductResponseDto::from)
-            .toList();
+        return productService.getAllProducts();
     }
 
     @GetMapping("/{id}")
     public ProductResponseDto getProduct(@PathVariable Long id) {
-        Product product = productRepository.findById(id)
-            .orElseThrow(() -> new NoSuchElementException("Product not found. productId: " + id));
-        return ProductResponseDto.from(product);
+        return productService.getProduct(id);
     }
 
-
     @PostMapping
-    public void addProduct(
-        @RequestBody ProductCreateRequestDto productCreateRequest
-    ) {
-        Product product = productOf(productCreateRequest);
-        productRepository.save(product);
+    public void addProduct(@RequestBody ProductCreateRequestDto productCreateRequest) {
+        productService.addProduct(productCreateRequest);
     }
 
     @PutMapping("/{id}")
-    public void updateProduct(
-        @PathVariable Long id,
-        @RequestBody ProductUpdateRequestDto productUpdateRequest
-    ) {
-        Product originalProduct = productRepository.findById(id)
-            .orElseThrow(() -> new NoSuchElementException("Product not found. productId: " + id));
-
-        Product updatedProduct = applyUpdate(originalProduct, productUpdateRequest);
-        productRepository.save(updatedProduct);
+    public void updateProduct(@PathVariable Long id,
+        @RequestBody ProductUpdateRequestDto productUpdateRequest) {
+        productService.updateProduct(id, productUpdateRequest);
     }
 
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable Long id) {
-        productRepository.remove(id);
-    }
-
-    private Product productOf(ProductCreateRequestDto productCreateRequest) {
-        return new Product(
-            0L,
-            productCreateRequest.name(),
-            productCreateRequest.price(),
-            productCreateRequest.imageUrl()
-        );
-    }
-
-    private Product applyUpdate(Product originalProduct,
-        ProductUpdateRequestDto productUpdateRequest) {
-        String name = originalProduct.getName();
-        if (productUpdateRequest.name() != null) {
-            name = productUpdateRequest.name();
-        }
-
-        Integer price = originalProduct.getPrice();
-        if (productUpdateRequest.price() != null) {
-            price = productUpdateRequest.price();
-        }
-
-        String imageUrl = originalProduct.getImageUrl();
-        if (productUpdateRequest.imageUrl() != null) {
-            imageUrl = productUpdateRequest.imageUrl();
-        }
-        return new Product(originalProduct.getId(), name, price, imageUrl);
+        productService.deleteProduct(id);
     }
 }
