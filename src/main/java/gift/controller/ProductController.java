7f3a8932812ce63675ce0f68382cd4824a605b97
wvssm <neoramiregi@gmail.com>
2024-07-01 16:07:16 +0900

    refactor(Controller): Autowired 삭제 - 현재 스프링 버전에서는 필요 없다

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
     private final ProductService productService;
 
-    @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
--- src/main/java/gift/controller/ProductRestController.java
+++ src/main/java/gift/controller/ProductRestController.java
@@ -18,7 +18,6 @@ import java.util.List;
 public class ProductRestController {
     private final ProductService productService;
 
-    @Autowired
     public ProductRestController(ProductService productService) {
         this.productService = productService;
     }
