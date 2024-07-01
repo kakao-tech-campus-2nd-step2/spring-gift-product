aac61682a15bd8b5251374324b9b9eabad03944f
seoyoung-dev <seoyoung.dev@gmail.com>
2024-07-01 19:17:59 +0900

    refactor: 사용하지 않는 기능 import문 삭제

--- src/main/java/gift/ProductViewController.java
+++ src/main/java/gift/ProductViewController.java
@@ -1,16 +1,13 @@
 package gift;
 
-import java.util.List;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ProductViewController {
-    private List<Product> products; // 상품 데이터
 
     @GetMapping("/products")
-    public String showProductList(Model model) {
+    public String showProductList() {
         return "products";
     }
 
