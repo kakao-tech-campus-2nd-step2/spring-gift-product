af1586c93553ea4d3832b121f9d4aaf7c3c4d460
mac <20j.code@gmail.com>
2024-07-01 14:30:58 +0900

    chore : Product type 변경
    null을 위한 Long, Integer로 타입 변경

--- src/main/java/com/kakaotech2/j20/Product.java
+++ src/main/java/com/kakaotech2/j20/Product.java
@@ -2,18 +2,21 @@ package com.kakaotech2.j20;
 
 public class Product {
 
-    private final long id;
+    private Long id;
     private String name;
 
-    private int price;
+    private Integer price;
 
     private String imageUrl;
 
-    public Product(long id, String name, int price, String imageUrl) {
+    public Product(Long id, String name, Integer price, String imageUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
+    }
+    public void setId(Long id) {
+        this.id=id;
     }
 
     public void setName(String name) {
@@ -28,7 +31,7 @@ public class Product {
         this.imageUrl = imageUrl;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
