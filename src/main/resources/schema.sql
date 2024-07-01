0fb7ef87c0a8a0f4c0573fd1b2742595e8547518
geonny <kky8586696@naver.com>
2024-07-01 10:43:06 +0900

    refactor : 스키마 not null 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 drop table gift if exists;
 create table gift(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255),
-    price INT,
-    imageUrl varchar(255)
+    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `name` varchar(255) not null,
+    `price` INT not null,
+    `imageUrl` varchar(255)
 
 );
