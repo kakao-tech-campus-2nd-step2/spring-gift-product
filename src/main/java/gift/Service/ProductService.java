09d44bab044ebf1fcbde2b5fdbba018ad300d0b0
BOMIN LYU <83059096+rbm0524@users.noreply.github.com>
2024-07-01 21:14:25 +0900

    전남대 BE_유보민_1주차 과제(1단계, 2단계, 3단계) (#108)
    * Docs(Readme.md) : 요구사항 정리
    
    * feat: Controller 클래스 구현
    
    * feat: Product 클래스 구현
    
    * feat: ProductService 클래스 구현
    
    * fix: deleteProduct에서 id가 존재할 경우에만 삭제하도록 수정
    
    * fix: 구현 기능 수정
    
    * fix: saveProduct에서 이미 존재하는 상품이면 null 반환하도록 수정
    
    * fix : 이미 존재하는 상품이라면 null을 반환받았으므로 BAD_REQUEST 반환하도록 수정
    
    * fix: README.md 2단계 내용 추가
    
    * fix : Thymeleaf 사용으로 인해 어노테이션과 세부 기능 수정, Controller 이름 ProductController로 수정
    
    * fix : 상품 삭제 시 idCounter 1 감소하도록 수정
    
    * feat : 상품 조회 화면 구현
    
    * feat : 상품 update 화면 구현
    
    * feat : 상품 추가 화면 구현
    
    * fix : 주석 삭제
    
    * fix : README.md 3단계 요구사항 추가
    
    * fix : Google Code Style 적용
    
    * fix : h2 db를 위해 properties 수정
    
    * fix : updateProduct의 기능을 Service에서 수행하도록 변경
    
    * fix : updateProduct의 기능을 Service에서 수행하도록 변경, init() 삭제, deleteProduct 수정
    
    * feat: DBConnection 클래스 추가
    
    * feat: ProductRepository 클래스 추가
    
    * fix : 사용하지 않는 Map 삭제
    
    * fix : 기존 product_form과 update_form 통합
    
    * fix : Get, Post로 나눠져있던 메서드 RequestMapping으로 통합
    
    * fix : update_form 삭제

---
+++ src/main/java/gift/Service/ProductService.java
@@ -0,0 +1,43 @@
+package gift.Service;
+
+import gift.Model.Product;
+import gift.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final AtomicLong idCounter = new AtomicLong(); //thread-safe
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAllProduct();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product saveProduct(Product product) {
+        product.setId(idCounter.incrementAndGet());
+        return productRepository.saveProduct(product);
+    }
+
+    public void updateProduct(Long id, Product productDetails) {
+        productRepository.updateProduct(id, productDetails);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteProductById(id);
+        idCounter.decrementAndGet();
+    }
+}
