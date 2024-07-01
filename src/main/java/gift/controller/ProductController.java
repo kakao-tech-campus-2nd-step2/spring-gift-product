f82c98dbf3341822ca4e73800774d18bcd14ccdc
sanghee0820 <lsh277604@gmail.com>
2024-07-01 14:42:18 +0900

    refactor: PostMapping 시 Status만 반환하도록 변경

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -7,6 +7,7 @@ import gift.model.Product;
 import gift.model.repository.ProductRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -51,11 +53,9 @@ public class ProductController {
     }
 
     @PostMapping()
-    public ResponseEntity<String> productSave(@RequestBody ProductRequest newProduct) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void productSave(@RequestBody ProductRequest newProduct) {
         productRepository.save(newProduct.toModel());
-
-        return ResponseEntity.created(null) // ORM의 Dirty Checking을 사용할 수 없어 쿼리를 한번 더 날려야해서 null로 설정
-                .body("Product created");
     }
 
     @PutMapping("/{id}")
