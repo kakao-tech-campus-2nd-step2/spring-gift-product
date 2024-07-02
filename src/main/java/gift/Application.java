5fa109811cdc2e6a861be0b6f74b814eaaf0f3c6
배준호 <114082026+BaeJunH0@users.noreply.github.com>
2024-07-02 22:54:24 +0900

    경북대 BE_배준호_1주차 과제(3단계) (#185)
    * feat : make Product DTO Class
    
    상품의 정보를 저장하기 위한 DTO 클래스 작성
    
    * feat : make ProductController Class
    
    /api/products 에 대한 GET요청을 받으면 Json 형식의 Product 객체를 반환하는 컨트롤러 클래스 구현
    
    * doc : Write Readme file
    
    Readme 파일 커밋을 잊었다, 늦은 커밋
    
    * chore : Write Product DTO Class's Constructor
    
    생성자가 없어도 될 것 같았는데, 필요할 것 같아서 추가
    
    * chore : 공백 삭제
    
    문서 내 공백 문자로 인한 수정 사항 발생, 간단한 수정
    
    * chore : make default constructor
    
    매개변수 없이 인스턴스를 생성하는 기본 생성자 작성
    
    * Test : Write Product DTO Class Test
    
    Product DTO 클래스는 Clean Area 이므로 normal case 하나에 대해서만 테스팅을 진행하였다
    
    * test : Write Empty ProductControllerTest
    
    * docs : 요구 사항 수정
    
    기존 요구 사항에 대한 잘못된 이해로 인해 구현하지 않은 사항에 대한 내용 추가
    
    * feat : CRUD API Implementation
    
    GET, POST, PUT, DELETE HTTP 메서드를 이용한 CRUD 기능 구현
    
    * feat : Form Controller for Create and Update
    
    Create와 Update를 위한 Form으로의 접근을 관리하는 Controller 구현
    
    * feat : index page implementation
    
    기본 페이지 구현
    
    * feat : Form for Create and Update
    
    Create와 Update에 사용할 데이터를 받기 위한 HTML Form 구현
    
    * feat : Main Page implementation
    
    상품 목록을 출력해주는 메인 페이지 구현
    
    * feat : add Hidden method finder
    
    hidden type input의 method 속성을 읽기 위해 빈 추가
    
    * chore : add commentary
    
    간단한 주석을 추가했다
    
    * docs : add properties
    
    데이터베이스 사용을 위한 설정
    
    * docs : add step 3's requirements
    
    스텝 3에서 요구하는 부분을 추가하여 Readme 파일 수정
    
    * feat : add DataBase Function
    
    데이터베이스에 데이터가 저장되도록 사양을 변경
    
    * chore : change ModForm's function
    
    수정 폼에 기존 정보가 모두 채워진 상태로 시작하고, id는 바꿀 수 없도록 기능을 수정하였다
    
    * fix : primary key duplicate error fix
    
    Primary 키, 즉 상품의 id 가 중복되면 데이터베이스 에러가 발생하던 부분을 수정하였다, 기존 상품과 같은 id가 입력되면 해당 입력을 무시한다
    
    ---------
    
    Co-authored-by: BaeJunho <bjh4498@knu.ac.kr>

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,11 +1,40 @@
 package gift;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    // HttpRequestMethodNotSupportedException: Request method 'POST' is not supported <- 발생에 대한 대응
+    // HTML에서 input 태그의 속성중 type = hidden, name = _method, value = put or delete를 전달하기 위한 빈
+    // 이를 추가하지 않으면 put, delete가 아닌 모두 post나 get으로 전달되게 되어 mapping 할 수 없다
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
+
+    // 필드 의존성 주입
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    // CommandLineRunner의 메서드를 오버라이딩, 실행 시 아래 sql을 실행 시킨다 ( 데이터베이스 초기화 )
+    @Override
+    public void run(String... strings) throws Exception {
+
+        jdbcTemplate.execute("DROP TABLE products IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE products(" +
+                             "id bigint, " +
+                             "name VARCHAR(255), " +
+                             "price bigint, " +
+                             "imageUrl VARCHAR(255), "+
+                             "primary key(id))");
     }
 }
