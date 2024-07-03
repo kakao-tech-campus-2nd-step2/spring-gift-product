a41ecefbd1f340a59d688c6e25f9af5f7c2ebf0c
pinkbeenyeti <rbgusdn12@gmail.com>
2024-07-03 12:56:02 +0900

    fix: 컨트롤러 수정 컨트롤러에서 product의 추가를 기존에느 ModelAttribute를 통해 받았으나 현재는 ResponseBody로 전송하므로 이에 맞게 수정하였다. 또한 Id를 현재 수정하지 않으므로 id를 수정하는 부분을 삭제하였다.

--- src/main/java/gift/controller/ProductControllerStep2.java
+++ src/main/java/gift/controller/ProductControllerStep2.java
@@ -1,6 +1,5 @@
 package gift.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,54 +8,40 @@ import gift.entity.Product;
 import gift.dto.*;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Objects;
 
 @Controller
 @RequestMapping("/")
 public class ProductControllerStep2 {
     private final Map<Long, Product> products = new HashMap<>();
 
-    @GetMapping("pinkbeen/products")
+    @GetMapping("v2/products")
     public String getAllProducts(Model model) {
         model.addAttribute("products", products);
         return "index";
     }
 
-    @PostMapping("pinkbeen/products")
-    public String addProduct(@ModelAttribute ProductDTO productDTO) {
+    @PostMapping("v2/products")
+    public String addProduct(@RequestBody ProductDTO productDTO) {
         Product newProduct = new Product(productDTO.id(), productDTO.name(), productDTO.price(), productDTO.imageUrl());
         products.put(newProduct.getId(), newProduct);
 
-        return "redirect:/pinkbeen/products";
+        return "redirect:/v2/products";
     }
 
-    @PostMapping("pinkbeen/products/{id}")
-    public ResponseEntity<ProductDTO> modifyProduct(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
+    @PostMapping("v2/products/{id}")
+    public String modifyProduct(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
         Product updatedProduct = products.get(id);
-
-        if (!Objects.equals(id, productDTO.id())) {
-            products.remove(id);
-            updatedProduct.setId(productDTO.id());
-            products.put(productDTO.id(), updatedProduct);
-        }
 
         updatedProduct.setName(productDTO.name());
         updatedProduct.setPrice(productDTO.price());
         updatedProduct.setImageUrl(productDTO.imageUrl());
 
-        ProductDTO updatedProductDTO = new ProductDTO(
-                updatedProduct.getId(),
-                updatedProduct.getName(),
-                updatedProduct.getPrice(),
-                updatedProduct.getImageUrl()
-        );
-
-        return ResponseEntity.ok(updatedProductDTO);
+        return "redirect:/v2/products";
     }
 
-    @DeleteMapping("pinkbeen/products/{id}")
+    @DeleteMapping("v2/products/{id}")
     public String deleteProduct(@PathVariable("id") Long id) {
         products.remove(id);
-        return "redirect:/pinkbeen/products";
+        return "redirect:/v2/products";
     }
 }
