57968eeaf1b3f83e07fc2d8218c1b86ac08c65a7
sseooh <sseooh@gmail.com>
2024-07-04 19:33:15 +0900

    refactor: 의존성 주입 방식 생성자 주입

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -2,7 +2,6 @@ package gift.controller;
 
 import gift.dto.Product;
 import gift.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
@@ -14,7 +13,6 @@ import java.util.*;
 public class ProductController {
     private final ProductService productService;
 
-    @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
